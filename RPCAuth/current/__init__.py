b8e4fe59a1e5b3a0ba3aa22b14d7149c1439a8c8
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2008-02-08 09:46:38 +0000

    Load RPCAuth-0.4/ into RPCAuth/current.

---
+++ RPCAuth/current/__init__.py
@@ -0,0 +1,10 @@
+import RPCAuth 
+
+def initialize(context):
+    context.registerClass(
+        RPCAuth.RPCAuth,
+        constructors=(RPCAuth.manage_addRAForm,
+                      RPCAuth.manage_addRPCAuth),
+        icon = 'RPCAuth.gif'
+        )
+

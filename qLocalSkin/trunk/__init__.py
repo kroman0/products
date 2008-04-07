8b7f73bff44b6e187dd9bde1ac7df14cc1de9609
piv <piv@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2008-04-07 14:37:16 +0000

    patched portal_url's getPhysicalPath

--- qLocalSkin/trunk/__init__.py
+++ qLocalSkin/trunk/__init__.py
@@ -6,7 +6,7 @@ def urltool_call(self, relative=0, *args, **kw):
     """ Get by default the absolute URL of the portal. If request is annonated then add suffix to portal_url
     """
 
-    # print '################################ Called pathed portal_url call: ' + self.REQUEST.URL
+    # print '################################ Called patched portal_url __call__: ' + self.REQUEST.URL
     url_suffix = ''
     if self.REQUEST:
         annotator = IRequestPortalUrlAnnotator(self.REQUEST, None)
@@ -16,4 +16,18 @@ def urltool_call(self, relative=0, *args, **kw):
 
     return self.getPortalObject().absolute_url(relative=relative) +  url_suffix
 
+def urltool_getPortalPath(self):
+    """ Get the portal object's URL without the server URL component.
+    """
+
+    url_suffix = ''
+    if self.REQUEST:
+        annotator = IRequestPortalUrlAnnotator(self.REQUEST, None)
+        if annotator is not None:
+            url_suffix = annotator.getPortalUrlSuffix()
+            # print '############ Added sufix to portal_url: ' + url_suffix
+
+    return '/'.join(self.getPortalObject().getPhysicalPath()) + url_suffix
+
 URLTool.__call__ = urltool_call
+URLTool.getPortalPath = urltool_getPortalPath

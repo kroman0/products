9cb3c8efda16c4c326f765dd855c300d42f2c779
deo <deo@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2008-06-27 17:12:53 +0000

    Moved the portal_properties registration to the GS profile. Removed the remaining skin code (un)registration.

--- qPloneComments/branches/plone-3.0/Extensions/Install.py
+++ qPloneComments/branches/plone-3.0/Extensions/Install.py
@@ -1,6 +1,5 @@
 from StringIO import StringIO
 from Products.CMFCore.utils import getToolByName
-from Products.CMFCore.DirectoryView import addDirectoryViews
 from Products.CMFCore.permissions import ManagePortal, ReplyToItem
 
 from Products.qPloneComments.config import *
@@ -12,17 +11,8 @@ def install(self):
     portal_setup = getToolByName(self, 'portal_setup')
     portal_setup.runAllImportStepsFromProfile('profile-Products.qPloneComments:default')
 
-    # Add Property sheet to portal_properies
-    pp = getToolByName(self, 'portal_properties')
-    if not 'qPloneComments' in pp.objectIds():
-        pp.addPropertySheet(id='qPloneComments', title='qPloneComments Properties')
-        out.write("Adding %s property sheet to portal_properies\n" % 'qPloneComments')
-    props_sheet = pp['qPloneComments']
-    updateProperties(props_sheet, out, PROPERTIES)
     # Tern on Anonymous commenting
     self.manage_permission(ReplyToItem, ['Anonymous','Manager','Member'], 1)
-
-    out.write("Updating properties of %s property sheet\n" % 'qPloneComments' )
 
     # Add Configlet. Delete old version before adding, if exist one.
     controlpanel_tool = getToolByName(self, 'portal_controlpanel')
@@ -39,6 +29,7 @@ def install(self):
         out.write("Added DiscussionManager role top portal.\n")
 
     self.manage_permission('Moderate Discussion', ['DiscussionManager', 'Manager'])
+
     # Add 'DiscussionManagers' group
     gtool = getToolByName(self, 'portal_groups')
     existing = gtool.listGroupIds()
@@ -54,29 +45,8 @@ def install(self):
     out.write('Installation successfully completed.\n')
     return out.getvalue()
 
-def updateProperties(pp_ps, out, *args):
-    for prop in args:
-        for prop_id, prop_value, prop_type in prop:
-            if not pp_ps.hasProperty(prop_id):
-                pp_ps.manage_addProperty(prop_id, prop_value, prop_type)
-                out.write("Adding %s property to %s property sheet\n" % (prop_id, 'qPloneComments'))
-
 def uninstall(self):
-    skinstool = getToolByName(self, 'portal_skins')
-    # Remove skin
-    for skinName in skinstool.getSkinSelections():
-        old_path = skinstool.getSkinPath(skinName)
-        old_path = [i.strip() for i in  old_path.split(',')]
-        path = []
-        for p in old_path:
-            if not p.startswith(SKIN_NAME):
-                path.append(p)
-        path = ','.join(path)
-        skinstool.addSkinSelection(skinName, path)
 
     # Remove configlet
     controlpanel_tool = getToolByName(self, 'portal_controlpanel')
     controlpanel_tool.unregisterConfiglet(CONFIGLET_ID)
-
-    # Remove Product's property sheet from portal_properties
-    pp = getToolByName(self, 'portal_properties')
--- qPloneComments/branches/plone-3.0/config.py
+++ qPloneComments/branches/plone-3.0/config.py
@@ -1,19 +1,4 @@
 PROJECTNAME = "qPloneComments"
 
-GLOBALS = globals()
-SKINS_DIR = "skins"
-SKIN_NAME = "qplonecomments"
-
 CONFIGLET_ID = "prefs_comments_setup_form"
 CONFIGLET_NAME = "qPloneComments setup"
-
-PROPERTIES = (('enable_approve_user_notification', 'True', 'boolean'),
-              ('enable_reply_user_notification', 'True', 'boolean'),
-              ('enable_rejected_user_notification', 'True', 'boolean'),
-              ('enable_moderation', 'True', 'boolean'),
-              ('require_email', 'False', 'boolean'),
-              ('enable_anonymous_commenting', 'True', 'boolean'),
-              ('enable_published_notification', 'True', 'boolean'),
-              ('enable_approve_notification', 'True', 'boolean'),
-              ('email_discussion_manager', '', 'string'),
-              ('email_subject_prefix', '', 'string'))

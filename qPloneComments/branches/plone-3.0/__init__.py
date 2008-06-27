bcdf16ae1ba8ba1416fb4219bbb3e9db431ebc69
deo <deo@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2008-06-27 17:11:32 +0000

    Removed the (now) unnecessary directory registration code. Cleaned up.

--- qPloneComments/branches/plone-3.0/__init__.py
+++ qPloneComments/branches/plone-3.0/__init__.py
@@ -1,12 +1,9 @@
-from Products.CMFCore.DirectoryView import registerDirectory
-from AccessControl import allow_module, Permission
+from AccessControl import allow_module
+from AccessControl.Permission import registerPermissions
 
-from  config import *
-import patch
+# Feed our monkeys :-)
+from Products.qPloneComments import patch
 
-registerDirectory(SKINS_DIR, GLOBALS)
-
-allow_module('Products.qPloneComments.config')
 allow_module('Products.qPloneComments.utils')
 
-Permission.registerPermissions((('Moderate Discussion', (), ('Manager',)),))
+registerPermissions((('Moderate Discussion', (), ('Manager',)),))

8197f8f559c728400c002d75ae4f2aa6474adf7a
piv <piv@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2008-07-24 11:21:33 +0000

    fully refactored action adding and validation

--- qPloneTabs/branches/quintagroup.plonetabs/trunk/quintagroup/plonetabs/plugins/plugin.js
+++ qPloneTabs/branches/quintagroup.plonetabs/trunk/quintagroup/plonetabs/plugins/plugin.js
@@ -6,17 +6,30 @@ kukit.plonetabs = {};
 kukit.actionsGlobalRegistry.register("plonetabs-redirectTo", function(oper) {
 ;;; oper.componentName = "[plonetabs-redirectTo] action";
     var wl = window.location;
-    oper.evaluateParameters([], {"protocol" : wl.protocol,
-                                 "host"     : wl.host,
-                                 "pathname" : wl.pathname,
-                                 "search"   : wl.search,
-                                 "hash"     : wl.hash});
+    oper.evaluateParameters([], {"protocol"     : wl.protocol,
+                                 "host"         : wl.host,
+                                 "pathname"     : wl.pathname,
+                                 "search"       : wl.search,
+                                 "searchparams" : "",
+                                 "searchvalues" : "",
+                                 "hash"         : wl.hash});
 
     // normalize parameters
     var protocol = oper.parms.protocol + (oper.parms.protocol[oper.parms.protocol.length - 1] == ":") ?  "" : ":";
     var host = oper.parms.host;
     var pathname = oper.parms.pathname;
-    var search = (oper.parms.search.substr(0, 1) == "?") ? "" : "?" + oper.parms.search;
+    if ((params = oper.parms.searchparams) && (values = oper.parms.searchvalues)) {
+        search = "?";
+        params = params.split(",");
+        values = values.split(",");
+        for (var i = 0; i < params.length; i++) {
+            search += params[i] + '=' + values[i] + '&';
+        }
+        search = (search.slice(search.length - 1) == '&') ? search.slice(0, search.length - 1) : search;
+    } else {
+        search = oper.parms.search;
+        search = (search && search.substr(0, 1) == "?") ? search : (search ? "?" + search : "");
+    }
     var hash = (oper.parms.hash.length > 1) ? ((oper.parms.hash.substr(0, 1) == "#" ? "" : "#") + oper.parms.hash) : "";
 
     url = protocol + "//" + host + pathname + search + hash;

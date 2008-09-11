ff0ac756cac03798a45f1982cc60598462d6f88b
liebster <liebster@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2008-09-11 08:53:33 +0000

    Add tests

---
+++ qPingTool/branches/plone-3.1/tests/testRegister.py
@@ -0,0 +1,32 @@
+#
+# Register skins
+#
+
+import os, sys
+if __name__ == '__main__':
+    execfile(os.path.join(sys.path[0], 'framework.py'))
+
+from base import *
+
+class TestSetup(TestCase):
+
+    def afterSetUp(self):
+        self.loginAsPortalOwner()
+
+    def testRegisterSkins(self):
+        test_content = ['tool.gif']
+        self.failUnless(not getattr(self.portal.portal_skins, 'qpingtool', None)==None)
+        content = [i.id for i in self.portal.portal_skins.qpingtool.objectValues()]
+        content.sort()
+        test_content.sort()
+        self.failUnless(content==test_content)
+
+
+def test_suite():
+    from unittest import TestSuite, makeSuite
+    suite = TestSuite()
+    suite.addTest(makeSuite(TestSetup))
+    return suite
+
+if __name__ == '__main__':
+    framework()

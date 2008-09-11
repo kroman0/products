65dd1663cbb8f165253ff6c549f0104df6ff6627
liebster <liebster@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2008-09-11 08:26:27 +0000

    Add test properies setup

---
+++ qPingTool/branches/plone-3.1/tests/config.py
@@ -0,0 +1,6 @@
+# for testPropertiesToolConf
+nt_properties = (("metaTypesNotToList", "lines", ["PingTool", "PingInfo"]), )
+
+s_properties = (("use_folder_tabs", "lines", ["PingTool"]),
+                ("typesLinkToFolderContentsInFC", "lines", ["PingTool"]),
+                ("typesUseViewActionInListings", "lines", ["PingTool"]),)

3e6ff8929d5e30498ae2af76427de4f1d803795e
liebster <liebster@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2008-09-12 12:52:53 +0000

    Add tests installed.

--- qPingTool/branches/plone-3.1/tests/config.py
+++ qPingTool/branches/plone-3.1/tests/config.py
@@ -4,3 +4,24 @@ nt_properties = (("metaTypesNotToList", "lines", ["PingTool", "PingInfo"]), )
 s_properties = (("use_folder_tabs", "lines", ["PingTool"]),
                 ("typesLinkToFolderContentsInFC", "lines", ["PingTool"]),
                 ("typesUseViewActionInListings", "lines", ["PingTool"]),)
+
+# for test_skin_installed
+skins_content = ['tool.gif']
+
+# for test_installedAllTypes
+istalled_types = ['PingTool', 'PingInfo']
+
+# for test_added_action
+types_actions = ({'type':'PingTool',
+                  'actions':(('view', 'View', 'string:${object_url}/view', ('Manage portal', ), 'object', True),
+                             ('edit', 'Edit', 'string:${object_url}/edit', ('Modify portal content', ), 'object', True),
+                            ),
+                 },
+                 {'type':'PingInfo',
+                  'actions':(('view', 'View', 'string:${object_url}/view', ('View',), 'object', True),
+                             ('edit', 'Edit', 'string:${object_url}/edit', ('Modify portal content', ), 'object', True),
+                             ('metadata', 'Properties', 'string:${object_url}/properties', ('Modify portal content', ), 'object', True),
+                             ('references', 'References', 'string:${object_url}/reference_graph', ('Modify portal content', 'Review portal content'), 'object', True),
+                            ),
+                 },
+                )

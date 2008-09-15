88858ce3e439d8141ef402e6429e4377e276704a
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2008-09-15 20:05:15 +0000

    Register viewlet localcommand in setup.py

--- qtheme.template/trunk/setup.py
+++ qtheme.template/trunk/setup.py
@@ -30,6 +30,7 @@ setup(name='qtheme.template',
           skin_sublayer = qthemetemplate.localcommands.subtemplates:SkinSublayerSubTemplate
           css_resource = qthemetemplate.localcommands.subtemplates:CSSSubTemplate
           js_resource = qthemetemplate.localcommands.subtemplates:JSSubTemplate
+          viewlet = qthemetemplate.localcommands.subtemplates:ViewletSubTemplate
 
       # -*- Entry points: -*-
       """,

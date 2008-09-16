02d2cea02b7ce297cb2bafee55535a8bafa2af61
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2008-09-16 11:05:35 +0000

    Combine skinlayer and skinsublayer into one skinlayer template

--- qtheme.template/trunk/qthemetemplate/localcommands/subtemplates.py
+++ qtheme.template/trunk/qthemetemplate/localcommands/subtemplates.py
@@ -15,29 +15,18 @@ class SkinLayerSubTemplate(QThemeSubTemplate):
     """
     _template_dir = 'templates/skinlayer'
     summary = "A Plone 3 Skin Layer"
-    
+
+    compo_template_markers = [
+        ('layer4Skin',   'extra layer stuff goes here'),
+    ]
 
     vars = [
       var('layer_name', 'Skin Layer name (should not contain spaces)',
            default="skin_layer"),
-           ]
-
-
-class SkinSublayerSubTemplate(QThemeSubTemplate):
-    """ Server for add Skin Layer to skin-path
-        in profiles/default/skins.xml
-    """
-
-    _template_dir = 'templates/skinsublayer'
-    summary = "A Plone 3 Skin SubLayer registration in GS' skins.xml"
-    marker_name = "extra sublayer stuff goes here"
-    
-    vars = [
-        var('sublayername', 'Skin Layer name (should not contain spaces)', default="skin_layer"),
-        var('insert_type', 'Where insert the layer ("after" or "before")', default="after"),
-        var('insert_control_layer',
-            'Layer after or before which your layer will be inserted, "*" accepted, which mean all',
-            default='custom'),
+      var('insert_type', 'Where insert the layer ("after" or "before")', default="after"),
+      var('insert_control_layer',
+          'Layer after or before which your layer will be inserted, "*" accepted, which mean all',
+          default='custom'),
            ]
 
 

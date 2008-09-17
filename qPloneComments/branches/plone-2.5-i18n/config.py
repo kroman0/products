0b8966d95f369105a4dcab5290436d26ead44447
liebster <liebster@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2008-09-17 14:28:54 +0000

    Cleanup branches

--- qPloneComments/branches/plone-2.5-i18n/config.py
+++
@@ -1,9 +0,0 @@
-PROJECTNAME = "qPloneComments"
-
-GLOBALS = globals()
-SKINS_DIR = "skins"
-SKIN_NAME = "qplonecomments"
-
-CONFIGLET_ID = "prefs_comments_setup_form"
-CONFIGLET_NAME = "qPloneComments setup"
-

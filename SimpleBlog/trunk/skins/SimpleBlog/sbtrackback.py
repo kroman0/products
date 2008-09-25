7a9994ffc71717fcf061821fb49be17378df8427
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2008-09-25 14:36:21 +0000

    Block trackingback script for stop spam

--- SimpleBlog/trunk/skins/SimpleBlog/sbtrackback.py
+++ SimpleBlog/trunk/skins/SimpleBlog/sbtrackback.py
@@ -1,4 +1,4 @@
-## Script (Python) "trackback"   
+## Script (Python) "sbtrackback"
 ##bind container=container
 ##bind context=context
 ##bind namespace=
@@ -7,42 +7,4 @@
 ##parameters=url='',title='',excerpt='',blog_name=''
 ##title=Provide trackback functionality
 ##
-from DateTime import DateTime
-from Products.SimpleBlog.util import addTrackBack
-
-request = context.REQUEST
-if not hasattr(request, 'url'):
-    print context.sbtrackbackResponse(code=0,msg="URL is missing")
-    return printed
-
-   
-#url = request.get('url','')
-#title = request.get('title', '')
-#excerpt = request.get('excerpt', '') 
-#blog_name = request.get('blog_name', '')
-
-url = request.get('url', url)
-title = request.get('title', title)
-excerpt = request.get('excerpt', excerpt) 
-blog_name = request.get('blog_name', blog_name)
-
-
-
-ids=context.objectIds()
-id=0
-
-now=DateTime()
-id=now.strftime('%Y%m%d')+now.strftime('%M%S')
-
-
-try:
-	context.invokeFactory(id=id, type_name='TrackBack')
-	trback = getattr(context, id, None)
-	trback.setUrl(url)
-	trback.setTitle(title)
-	trback.setBlog_name(blog_name)
-	trback.setExcerpt(excerpt)
-except:
-	print context.sbtrackbackResponse(code=0, msg="") 
-	return printed
-return context.sbtrackbackResponse(code=1, msg="ok") 
+return ""

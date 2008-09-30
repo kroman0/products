979aba9ce3cedec47d8db4dc4e252917e2a2d6a3
fenix <fenix@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2008-09-30 11:22:49 +0000

    Now id for captcha field is created automatically, also added captcha field content type to factory types

--- qPloneCaptchaField/trunk/content/CaptchaField.py
+++ qPloneCaptchaField/trunk/content/CaptchaField.py
@@ -1,21 +1,50 @@
+from zope.event import notify
+from zope.lifecycleevent import ObjectCreatedEvent, ObjectModifiedEvent
 from Products.CMFCore.permissions import View
 
 from Products.Archetypes.atapi import *
 from Products.ATContentTypes.content.base import registerATCT
 
-from Products.PloneFormGen.content.fieldsBase import BaseFormField, BaseFieldSchemaStringDefault
+from Products.PloneFormGen.content.fieldsBase import BareFieldSchema, BaseFormField, BaseFieldSchemaStringDefault
 from Products.PloneFormGen.content.fields import FGStringField
 
 from Products.qPloneCaptchaField.config import PROJECTNAME
 from Products.qPloneCaptchaField.widgets.CaptchaWidget import CaptchaWidget
 
+CAPTCHA_ID = 'key'
+
+def finalizeCaptchaFieldSchema( schema ):
+    schema['title'].default= 'key'
+    for field in ('title', 
+                  'description',
+                  'required', 
+                  'hidden', 
+                  'fgTDefault', 
+                  'fgTEnabled', 
+                  'fgDefault',
+                  'fgTValidator'):
+        schema[field].widget.visible = {'view':'invisible','edit':'invisible'}
+
+CaptchaFieldSchema = BaseFieldSchemaStringDefault.copy()
+finalizeCaptchaFieldSchema( CaptchaFieldSchema )
+
+def addCaptchaField(self, id, **kwargs):
+    id = CAPTCHA_ID 
+    obj = CaptchaField(id)
+    notify(ObjectCreatedEvent(obj))
+    self._setObject(id, obj)
+    obj = self._getOb(id)
+    obj.initializeArchetype(**kwargs)
+    notify(ObjectModifiedEvent(obj))
+    return obj.getId()
+
 class CaptchaField(FGStringField):
 
-    schema = BaseFieldSchemaStringDefault
+    _at_rename_after_creation = False
+    schema = CaptchaFieldSchema
 
     def __init__(self, oid, **kwargs):
         """ initialize class """
-
         BaseFormField.__init__(self, oid, **kwargs)
 
         # set a preconfigured field as an instance attribute

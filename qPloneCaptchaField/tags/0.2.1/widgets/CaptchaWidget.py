1f3004b56b99585a449c17226c9b6a6b8497a632
fenix <fenix@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2008-10-03 09:36:02 +0000

    tagging 0.2.1 version of product

---
+++ qPloneCaptchaField/tags/0.2.1/widgets/CaptchaWidget.py
@@ -0,0 +1,13 @@
+from Products.Archetypes.Widget import StringWidget
+from Products.Archetypes.Registry import registerWidget
+
+class CaptchaWidget(StringWidget):
+    _properties = StringWidget._properties.copy()
+    _properties.update( {'macro' : "captchaField_widget"} )
+
+
+registerWidget(CaptchaWidget,
+               title = 'Captcha widget',
+               description= ('Renders captcha image and string input',),
+               used_for = ('Products.qPloneCaptchaField.content.CaptchaField',)
+              )

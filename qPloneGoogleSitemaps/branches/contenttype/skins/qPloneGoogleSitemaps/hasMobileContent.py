093ed9076a5bbbea55c6bea093329b774b355fa2
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2008-11-21 12:16:47 +0000

    Add mobile sitemap dependencies: catalog update, add script for new index. Fix skins registration

---
+++ qPloneGoogleSitemaps/branches/contenttype/skins/qPloneGoogleSitemaps/hasMobileContent.py
@@ -0,0 +1,9 @@
+## Script (Python) "searchForGoogleSitemapObjects"
+##bind container=
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=path=None
+##
+return len(getattr(context,'mobile_content',''))>0

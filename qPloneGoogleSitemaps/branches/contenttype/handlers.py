9351636b240b40d365922608a7e67a7df61e6f7d
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2008-11-27 15:11:58 +0000

    Refactor pingin for ping ONLY default Sitemap object

--- qPloneGoogleSitemaps/branches/contenttype/handlers.py
+++ qPloneGoogleSitemaps/branches/contenttype/handlers.py
@@ -9,19 +9,27 @@ from Products.qPloneGoogleSitemaps.utils import ping_google
 
 def pingByType(event, smtype):
     object = event.object
-    catalog = getToolByName(object, 'portal_catalog')
-
-    csm = None
-    for brain in catalog(portal_type='Sitemap'):
-        smobj = brain.getObject()
-        if smobj.getSitemapType() == smtype:
-            csm = smobj
-            break
-    if csm:
+    portal = getToolByName(object, 'portal_url').getPortalObject()
+    pp = getToolByName(object, 'portal_properties')
+    props = getattr(pp, 'googlesitemap_properties', None)
+    
+    pname = "%s_default" % smtype
+    csm_path = props and props.getProperty(pname, '') or ''
+    
+    print "csm_path: %s" % csm_path
+    
+    if csm_path:
+        csm = portal.unrestrictedTraverse(csm_path, default=None)
         wftrans_name = "%s#%s" % (event.workflow.id, event.transition.id)
-        if wftrans_name in csm.getPingTransitions():
+        
+        print "wftrans_name: %s, csm: %s" % (wftrans_name, repr(csm))
+        
+        if csm and wftrans_name in csm.getPingTransitions():
             plone_home = getToolByName(object, 'portal_url').getPortalObject().absolute_url()
-            ping_google(plone_home, csm.id)
+            #ping_google(plone_home, csm.id)
+            print "Pinged (%s/%s) " % (plone_home, csm.id)
+            
+            
             print "Pinged %s sitemap to google" % smtype
 
 

42dad47711b1d233854be63967a97d0c839cb0b5
koval <koval@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2008-11-28 10:47:29 +0000

    Merged revisions 1454-1456,1467,1469 via svnmerge from http://svn.quintagroup.com/products/quintagroup.transmogrifier/trunk
    ........
      r1454 | koval | 2008-11-24 16:58:58 +0200 (Mon, 24 Nov 2008) | 1 line
      
      extended logger section, added monitoring support in source sections
    ........
      r1455 | koval | 2008-11-24 17:24:44 +0200 (Mon, 24 Nov 2008) | 1 line
      
      fixed bugs in catalogsource section
    ........
      r1456 | koval | 2008-11-24 18:21:07 +0200 (Mon, 24 Nov 2008) | 1 line
      
      catalogsource section now yields items that have comments, which was returned by catalog
    ........
      r1467 | koval | 2008-11-26 15:39:17 +0200 (Wed, 26 Nov 2008) | 1 line
      
      fixed bug on event firing in demarshaller section, catalogsource section now use proper portal_type attribute on brain
    ........
      r1469 | koval | 2008-11-26 17:45:12 +0200 (Wed, 26 Nov 2008) | 1 line
      
      manifest importer section became source section
    ........

--- quintagroup.transmogrifier/branches/plone-2.1/quintagroup.transmogrifier/quintagroup/transmogrifier/catalogsource.py
+++ quintagroup.transmogrifier/branches/plone-2.1/quintagroup.transmogrifier/quintagroup/transmogrifier/catalogsource.py
@@ -1,8 +1,11 @@
 from zope.interface import classProvides, implements
+from zope.app.annotation.interfaces import IAnnotations
 
 from collective.transmogrifier.interfaces import ISection, ISectionBlueprint
 
 from Products.CMFCore import utils
+
+from quintagroup.transmogrifier.logger import VALIDATIONKEY
 
 class CatalogSourceSection(object):
     classProvides(ISectionBlueprint)
@@ -11,6 +14,10 @@ class CatalogSourceSection(object):
     def __init__(self, transmogrifier, name, options, previous):
         self.previous = previous
         self.context = transmogrifier.context
+
+        # next is for communication with 'logger' section
+        self.anno = IAnnotations(transmogrifier)
+        self.storage = self.anno.setdefault(VALIDATIONKEY, [])
 
         self.pathkey = options.pop('path-key', '_path')
         self.entrieskey = options.pop('entries-key', '_entries')
@@ -39,12 +46,15 @@ class CatalogSourceSection(object):
         results = list(self.catalog(**self.query))
         results.sort(key=lambda x: x.getPath())
         for brain in results:
-            # discussion items are get catalogued too
+            # discussion items are indexed and they must be replaced to
+            # content objects to which they correspond
             # we need to skip them
-            if brain.Type == 'Discussion Item':
-                continue
-
-            path = brain.getPath()
+            if brain.portal_type == 'Discussion Item':
+                path =  '/'.join(brain.getPath().split('/')[:-2])
+                cp, id_ = path.rsplit('/', 1)
+                brain = self.catalog(path=cp, id=id_)[0]
+            else:
+                path = brain.getPath()
 
             # folderish objects are tried to export twice:
             # when their contained items are exported and when they are
@@ -53,14 +63,27 @@ class CatalogSourceSection(object):
                 continue
             exported.append(path)
 
+            # export also all parents of current object
+            containers = []
             container_path = path.rsplit('/', 1)[0]
-            contained = self.getContained(container_path)
-            if contained and container_path not in exported:
-                exported.append(container_path)
-                yield {
-                    self.pathkey: '/'.join(path.split('/')[2:-1]),
-                    self.entrieskey: contained,
-                }
+            while container_path:
+                if container_path in exported:
+                    container_path = container_path.rsplit('/', 1)[0]
+                    continue
+                contained = self.getContained(container_path)
+                if contained:
+                    exported.append(container_path)
+                    containers.append({
+                        self.pathkey: '/'.join(container_path.split('/')[2:]),
+                        self.entrieskey: contained,
+                    })
+                container_path = container_path.rsplit('/', 1)[0]
+            containers.reverse()
+            # order metter for us
+            for i in containers:
+                self.storage.append(i[self.pathkey])
+                yield i
+
             item = {
                 self.pathkey: '/'.join(path.split('/')[2:]),
             }
@@ -69,20 +92,40 @@ class CatalogSourceSection(object):
                 if contained:
                     item[self.entrieskey] = contained
 
+            self.storage.append(item[self.pathkey])
             yield item
+
+        # cleanup
+        if VALIDATIONKEY in self.anno:
+            del self.anno[VALIDATIONKEY]
 
     def getContained(self, path):
         """ Return list of (object_id, portal_type) for objects that are returned by catalog
             and contained in folder with given 'path'.
         """
         results = []
+        seen = []
         raw_results = self.catalog(path=path, **self.query)
         for brain in raw_results:
             current = brain.getPath()
             relative = current[len(path):]
             relative = relative.strip('/')
-            if not relative or '/' in relative:
+            if not relative:
+                # it's object with path that was given in catalog query
                 continue
-            results.append(brain)
-        contained = [(i.getId, str(i.Type)) for i in results]
+            elif '/' in relative:
+                # object stored in subfolders, we need append to results their parent folder
+                parent_path = '/'.join([path, relative.split('/', 1)[0]])
+                if parent_path not in seen:                    
+                    res = self.catalog(path=path) #, meta_type='Folder')
+                    for i in res:
+                        if i.getPath() == parent_path:
+                            results.append(i)
+                            seen.append(parent_path)
+                            break
+            elif current not in seen:
+                # object is directly stored in folder, that has path given in query
+                seen.append(current)
+                results.append(brain)
+        contained = [(i.getId, str(i.portal_type)) for i in results]
         return tuple(contained)

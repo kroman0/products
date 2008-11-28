06685368e67d21006c5911bf102059e2edd3771b
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2008-11-28 18:14:11 +0000

    Update setup view with size & entries information

--- qPloneGoogleSitemaps/branches/contenttype/browser/configletview.py
+++ qPloneGoogleSitemaps/branches/contenttype/browser/configletview.py
@@ -1,9 +1,21 @@
+from zope.component import queryMultiAdapter
 from zope.interface import implements, Interface, Attribute
 
 from Products.Five import BrowserView
 from Products.CMFCore.utils import getToolByName
 
 from Products.qPloneGoogleSitemaps import qPloneGoogleSitemapsMessageFactory as _
+from Products.qPloneGoogleSitemaps.interfaces import ISitemap
+
+def splitNum(num):
+    res = []
+    prefn = 3
+    for c in str(num)[::-1]:
+        res.insert(0,c)
+        if not len(res)%prefn:
+            res.insert(0,',')
+            prefn += 4
+    return "".join(res[0]==',' and res[1:] or res)
 
 class IConfigletSettingsView(Interface):
     """
@@ -56,6 +68,21 @@ class ConfigletSettingsView(BrowserView):
     def sitemapsKeys(self):
         return self.sitemaps.keys()
 
+    def getSitemapData(self, ob):
+        size, entries = (0, 0)
+        view = ob and ob.defaultView() or None
+        if view:
+            resp = self.request.RESPONSE
+            bview = queryMultiAdapter((ob,self.request), name=view)
+            if bview:
+                try:
+                    size = len(bview())
+                    entries = bview.numEntries
+                    self.request.RESPONSE.setHeader('Content-Type', 'text/html')
+                except:
+                    pass
+        return (size, entries)
+
     def getSMDataByType(self, smtype):
         data = self.sitemaps[smtype]
         lpp = len(self.portal.getPhysicalPath())
@@ -65,11 +92,12 @@ class ConfigletSettingsView(BrowserView):
         if def_path:
             default = [sm for sm in data if '/'.join(sm.getPhysicalPath()[lpp:])==def_path]
             default = default and default[0] or None
-
+        def_size, def_entries = self.getSitemapData(default)
         return {'smtype_title'      : smtype.capitalize(),
                 'sm_list'           : [(sm.id, '/'.join(sm.getPhysicalPath()[lpp:]), 
                                         def_path=='/'.join(sm.getPhysicalPath()[lpp:])) for sm in data],
                 'sm_def_id'         : default and default.id or '',
-                'sm_def_editurl'    : default and '%s/edit' % default.absolute_url() or ''
+                'sm_def_editurl'    : default and '%s/edit' % default.absolute_url() or '',
+                'sm_def_size'       : def_size and splitNum(def_size) or '',
+                'sm_def_entries'    : def_entries and splitNum(def_entries) or '',
                }
-

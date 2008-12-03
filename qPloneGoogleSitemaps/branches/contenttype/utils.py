c93fb740947137f1bd00bc50bc2355b325437030
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2008-12-03 16:34:27 +0000

    Add Pinging tests. Fix bug in pinging

--- qPloneGoogleSitemaps/branches/contenttype/utils.py
+++ qPloneGoogleSitemaps/branches/contenttype/utils.py
@@ -3,10 +3,15 @@ from urllib  import quote as urlquote
 
 from OFS.ObjectManager import BadRequestException
 
+from Products.qPloneGoogleSitemaps.config import testing
+
 def ping_google(url, sitemap_id):
     """Ping sitemap to Google"""
-    sitemap_url = urlquote(url + "/" + sitemap_id)
+    resurl = url + "/" + sitemap_id
+    sitemap_url = urlquote(resurl)
     g = urlopen('http://www.google.com/webmasters/sitemaps/ping?sitemap='+sitemap_url)
     result = g.read()
     g.close()
+    if testing:
+        print "Pinged %s sitemap to google" % resurl
     return 0

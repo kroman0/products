75dd56c9dc96ef5e2caa5e93b096b070a9351c0a
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2008-12-10 11:20:22 +0000

    Tag 0.8 version with addition mobile and news Sitemaps type support.

---
+++ qPloneGoogleSitemaps/tags/0.8/handlers.py
@@ -0,0 +1,21 @@
+from zope.component import adapter
+from Products.CMFCore.utils import getToolByName
+#from Products.qPloneGoogleSitemaps.events import AfterTransitionEvent
+from Products.qPloneGoogleSitemaps.interfaces import IAfterTransitionEvent
+from Products.qPloneGoogleSitemaps.utils import ping_google
+
+@adapter(IAfterTransitionEvent)
+def pingGoogle(event):
+    object = event.object
+    catalog = getToolByName(object, 'portal_catalog')
+
+    sitemaps = [b.getObject() for b in catalog(portal_type='Sitemap')]
+    if sitemaps:
+        plone_home = getToolByName(object, 'portal_url').getPortalObject().absolute_url()
+        wftrans_name = "%s#%s" % (event.workflow.id, event.transition.id)
+        obj_ptype = object.portal_type
+        for sm in sitemaps:
+            if wftrans_name in sm.getPingTransitions() \
+               and obj_ptype in sm.getPortalTypes():
+                ping_google(plone_home, sm.id)
+    return 0

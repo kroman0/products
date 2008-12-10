2dd410541f9e997acc0dbcb3bd6d356b97062624
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2008-12-10 11:19:18 +0000

    Delete due to bug in namespace

--- qPloneGoogleSitemaps/tags/0.8/Extensions/ping_googlesitemap.py
+++
@@ -1,9 +0,0 @@
-from zope.event import notify
-from Products.qPloneGoogleSitemaps.events import AfterTransitionEvent
-
-def pingGoogleSitemap(stch):
-    notify(AfterTransitionEvent(stch.object,
-           stch.workflow, stch.old_state, stch.new_state,
-           stch.transition, stch.status, stch.kwargs))
-    return 0
-
--- qPloneGoogleSitemaps/tags/0.8/skins/qPloneGoogleSitemaps/hasMobileContent.py
+++
@@ -1,9 +0,0 @@
-## Script (Python) "searchForGoogleSitemapObjects"
-##bind container=
-##bind context=context
-##bind namespace=
-##bind script=script
-##bind subpath=traverse_subpath
-##parameters=path=None
-##
-return len(getattr(context,'mobile_content',''))>0

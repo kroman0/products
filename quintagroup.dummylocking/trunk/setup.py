45e4b45698e17c315fdf5bec132c6ea9dbf5ed37
koval <koval@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2008-12-24 16:34:26 +0000

    initial import

---
+++ quintagroup.dummylocking/trunk/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='quintagroup.dummylocking',
+      version=version,
+      description="This package turns off standard Plone locking",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone locking',
+      author='Bohdan Koval',
+      author_email='koval@quintagroup.com',
+      url='http://svn.quintagroup.com/products/quintagroup.dummylocking/trunk',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['quintagroup'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )

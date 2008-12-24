7fd3d2f2c9b94cedb9ea643abb9d7c76774d6041
chervol <chervol@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2008-12-24 20:56:58 +0000

    absolute_url for subsites fixed

--- qg.canonicalurl/trunk/qg/canonicalurl/__init__.py
+++ qg.canonicalurl/trunk/qg/canonicalurl/__init__.py
@@ -1,9 +1,10 @@
 #
 from config import DOES_PATCH_TRAVERSABLE
+from OFS.Application import Application 
 
 if DOES_PATCH_TRAVERSABLE:
     from OFS.Traversable import Traversable
-    from Acquisition import aq_inner, aq_parent
+    from Acquisition import aq_inner, aq_parent, aq_base
     from interfaces import ICanonicalURL, ICanonicalURLRoot
 
     def getCanonicalURL(self):
@@ -18,27 +19,28 @@ if DOES_PATCH_TRAVERSABLE:
         rel_path = self.absolute_url(1)[len(ob.absolute_url(1)):]
         return curl + rel_path
 
-    #Traversable.security.declarePublic('getCanonicalURL')
-    #Traversable.getCanonicalURL = getCanonicalURL
 
     def getCURL(self, relative=0):
         if relative:
             return self.original_absolute_url(relative)
         ob = self
         while not ICanonicalURLRoot.providedBy(ob):
+            if type(aq_base(ob)) == Application:
+                return ob.original_absolute_url()
             try:
-                ob = aq_parent(aq_inner(ob))
+                ob = aq_parent(aq_inner(ob))                
             except:
                 return self.original_absolute_url()
-
+       
         curl = ICanonicalURL(ob).getCanonicalURL()
         rel_path = self.original_absolute_url(1)[len(ob.original_absolute_url(1)):]
-        return curl + rel_path
+        if rel_path:
+            return '/'.join((curl, rel_path))
+        else: 
+            return curl
 
-    #Traversable.security.declarePublic('getCURL')
-    origianl_absolute_url = Traversable.absolute_url
+    Traversable.original_absolute_url = Traversable.absolute_url
     Traversable.absolute_url = getCURL
-    Traversable.original_absolute_url = origianl_absolute_url
     Traversable.getCURL = getCURL
 
     print "%"*20, "APPLY CANONICAL_URL PATCH"

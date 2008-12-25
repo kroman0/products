408b416f58332f3a8402ce5eb735f29f130ec2dd
Volodymyr Cherepanyak <chervol@users.noreply.github.com>
2008-12-25 16:56:52 +0000

    tweaked he HTTPRequest to work on devel server

--- qg.canonicalurl/trunk/qg/canonicalurl/__init__.py
+++ qg.canonicalurl/trunk/qg/canonicalurl/__init__.py
@@ -1,46 +1,23 @@
 #
-from config import DOES_PATCH_TRAVERSABLE
-from OFS.Application import Application 
+from ZPublisher.HTTPRequest import HTTPRequest
 
-if DOES_PATCH_TRAVERSABLE:
-    from OFS.Traversable import Traversable
-    from Acquisition import aq_inner, aq_parent, aq_base
-    from interfaces import ICanonicalURL, ICanonicalURLRoot
-
-    def getCanonicalURL(self):
-        ob = self
-        while not ICanonicalURLRoot.providedBy(ob):
-            try:
-                ob = aq_parent(aq_inner(ob))
-            except:
-                return self.absolute_url()
-
-        curl = ICanonicalURL(ob).getCanonicalURL()
-        rel_path = self.absolute_url(1)[len(ob.absolute_url(1)):]
-        return curl + rel_path
-
-
-    def getCURL(self, relative=0):
-        if relative:
-            return self.original_absolute_url(relative)
-        ob = self
-        while not ICanonicalURLRoot.providedBy(ob):
-            if type(aq_base(ob)) == Application:
-                return ob.original_absolute_url()
-            try:
-                ob = aq_parent(aq_inner(ob))                
-            except:
-                return self.original_absolute_url()
-       
-        curl = ICanonicalURL(ob).getCanonicalURL()
-        rel_path = self.original_absolute_url(1)[len(ob.original_absolute_url(1)):]
-        if rel_path:
-            return '/'.join((curl, rel_path))
-        else: 
-            return curl
-
-    Traversable.original_absolute_url = Traversable.absolute_url
-    Traversable.absolute_url = getCURL
-    Traversable.getCURL = getCURL
-
-    print "%"*20, "APPLY CANONICAL_URL PATCH"
+from ZPublisher.BaseRequest import BaseRequest, quote
+def physicalPathToURL(self, path, relative=0):
+    """ Convert a physical path into a URL in the current context """
+    url = self['SERVER_URL']
+    if not type(path) == type(''):
+        ps = '/'.join(path)
+    else:
+        ps = path
+    if ps.startswith('/www'):
+        url = "http://bipp.quintagroup.com"
+        if ps.startswith('/www/programs/energy-forum'):
+            url = "http://energy.bipp.quintagroup.com"
+            path = ps[len('/www/programs/energy-forum'):]
+    path = self._script + map(quote, self.physicalPathToVirtualPath(path))
+    if relative:
+        path.insert(0, '')
+    else:
+        path.insert(0, url)
+    return '/'.join(path)
+HTTPRequest.physicalPathToURL = physicalPathToURL

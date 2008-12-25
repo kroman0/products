cda39f0bda6058af1b0540fa1c306fdfc309415a
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2008-12-25 23:04:59 +0000

    Added possiblity to manage subpath-domains map with 'path_domain_map' property of protal. Remove needless code

--- qg.canonicalurl/trunk/qg/canonicalurl/__init__.py
+++ qg.canonicalurl/trunk/qg/canonicalurl/__init__.py
@@ -1,23 +1,28 @@
 #
 from ZPublisher.HTTPRequest import HTTPRequest
+from ZPublisher.BaseRequest import quote
 
-from ZPublisher.BaseRequest import BaseRequest, quote
 def physicalPathToURL(self, path, relative=0):
     """ Convert a physical path into a URL in the current context """
     url = self['SERVER_URL']
+    proto = url.split('://')[0]
+
     if not type(path) == type(''):
-        ps = '/'.join(path)
-    else:
-        ps = path
-    if ps.startswith('/www'):
-        url = "http://bipp.quintagroup.com"
-        if ps.startswith('/www/programs/energy-forum'):
-            url = "http://energy.bipp.quintagroup.com"
-            path = ps[len('/www/programs/energy-forum'):]
+        path = '/'.join(path)
+
+    path_domain_map = getattr(self, 'path_domain_map', ())
+    for subpath, domain in path_domain_map:
+        if len(path) >= len(subpath) \
+           and path.startswith(subpath):
+            url = "%s://%s" % (proto, domain)
+            path = path[len(subpath):]
+            break
+
     path = self._script + map(quote, self.physicalPathToVirtualPath(path))
     if relative:
         path.insert(0, '')
     else:
         path.insert(0, url)
     return '/'.join(path)
+
 HTTPRequest.physicalPathToURL = physicalPathToURL

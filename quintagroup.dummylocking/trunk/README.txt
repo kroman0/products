439508379a6619e8e8449c0e0fa663139ad8fa5c
koval <koval@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2008-12-29 15:42:27 +0000

    package cleaning before release

--- quintagroup.dummylocking/trunk/README.txt
+++ quintagroup.dummylocking/trunk/README.txt
@@ -1,4 +1,8 @@
-Introduction
-============
+quintagroup.dummylocking Package Readme
+=========================
 
+Overview
+--------
 
+This package turns off Plone locking. It overrides standard locking adapter with
+one that does nothing.
--- quintagroup.dummylocking/trunk/quintagroup/dummylocking/lockable.py
+++ quintagroup.dummylocking/trunk/quintagroup/dummylocking/lockable.py
@@ -14,16 +14,16 @@ class DummyTTWLockable(object):
 
     def __init__(self, context):
         self.context = context
-        
+
     def lock(self, lock_type=STEALABLE_LOCK, children=False):
         pass
 
     def unlock(self, lock_type=STEALABLE_LOCK, stealable_only=True):
         pass
-        
+
     def clear_locks(self):
         pass
-        
+
     def locked(self):
         return False
 
@@ -32,6 +32,6 @@ class DummyTTWLockable(object):
 
     def stealable(self, lock_type=STEALABLE_LOCK):
         return True
-        
+
     def lock_info(self):
         return []

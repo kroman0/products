d697d5d361ab0bddd7888c210b8a181d1956681c
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2008-12-30 13:15:18 +0000

    Tag 0.2 version

---
+++ qLocalSkin/tags/0.2/interfaces.py
@@ -0,0 +1,6 @@
+from zope.interface import Interface
+
+class IShiftPortalUrl(Interface):
+    """ Marker interface for shift portal_url
+        to location of marked folder.
+    """
---
+++ qLocalSkin/tags/0.2/layer.py
@@ -0,0 +1,23 @@
+from zope.component import queryUtility, queryMultiAdapter
+
+from zope.publisher.interfaces.browser import IBrowserSkinType
+from zope.publisher.browser import applySkin
+
+from adapters.interfaces import ISkinNameExtractor, IRequestPortalUrlAnnotator
+
+def mark_layer(context, event):
+    """Mark the request with a layer corresponding to the current marked object.
+    """
+
+    skin_name = None
+    extractor = ISkinNameExtractor(context, None)
+    if extractor is not None:
+        skin_name = extractor.getSkinName()
+    if skin_name is not None:
+        skin = queryUtility(IBrowserSkinType, name=skin_name)
+        if skin is not None:
+            applySkin(event.request, skin)
+            context.changeSkin(skin_name, event.request)
+            annotator = IRequestPortalUrlAnnotator(event.request, None)
+            if annotator is not None:
+                annotator.annotate(context.absolute_url())

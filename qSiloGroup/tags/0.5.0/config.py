d6be7b82fe599b6431594c10ac90360f1dcc600a
fenix <fenix@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-01-14 10:52:51 +0000

    tagging 0.5.0 version

---
+++ qSiloGroup/tags/0.5.0/config.py
@@ -0,0 +1,11 @@
+from Products.CMFCore import permissions
+
+VIEW_PERMISSION = permissions.ManagePortal
+
+PROJECTNAME = 'qSiloGroup'
+SKINS_DIR = 'skins'
+
+GLOBALS = globals()
+
+JS = [{'cacheable': True, 'compression': 'safe', 'expression': '', 'enabled': True, 'inline': '', 'cookable': True, 'id': 'adjustIFrameSize.js'},]
+CSS = []

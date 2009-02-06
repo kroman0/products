077a4c3522331148c66c99887da6f0da1499c503
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-02-06 11:50:47 +0000

    Turn off allow discussion for Document portal type. Turn on discussion for blogpost on adding it to blog

--- quintagroup.quills.extras/trunk/quintagroup/quills/extras/Extensions/utils.py
+++ quintagroup.quills.extras/trunk/quintagroup/quills/extras/Extensions/utils.py
@@ -5,6 +5,7 @@ from quills.core.interfaces import IPossibleWeblog
 from quills.core.interfaces.enabled import IPossibleWeblogEntry
 
 def set_layout(sc_info):
+    portal = sc_info.getPortal()
     obj = sc_info.object
     weblog = recurseToInterface(obj, IPossibleWeblog)
     if weblog:
@@ -13,3 +14,6 @@ def set_layout(sc_info):
         # add providing of IPossibleWeblogEntry interface
         if not IPossibleWeblogEntry.providedBy(obj):
             alsoProvides(obj, IPossibleWeblogEntry)
+        # allow discussion for object
+        dt = getToolByName(portal,'portal_discussion')
+        dt.overrideDiscussionFor(obj, 1)

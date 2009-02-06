015f32ab169fd914d275781166c64df7c1c68887
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-02-06 13:42:22 +0000

    Create external method for update all blogentries in blog

--- quintagroup.quills.extras/trunk/quintagroup/quills/extras/Extensions/utils.py
+++ quintagroup.quills.extras/trunk/quintagroup/quills/extras/Extensions/utils.py
@@ -7,15 +7,28 @@ from quills.core.interfaces.enabled import IPossibleWeblogEntry
 from Products.CMFCore.utils import getToolByName
 
 def set_layout(sc_info):
-    portal = sc_info.getPortal()
+    #portal = sc_info.getPortal()
     obj = sc_info.object
     weblog = recurseToInterface(obj, IPossibleWeblog)
     if weblog:
-        # set default layout
-        obj.setLayout('weblogentry_view')
-        # add providing of IPossibleWeblogEntry interface
-        if not IPossibleWeblogEntry.providedBy(obj):
-            alsoProvides(obj, IPossibleWeblogEntry)
-        # allow discussion for object
-        dt = getToolByName(portal,'portal_discussion')
-        dt.overrideDiscussionFor(obj, 1)
+      updateBlogPost(obj)
+
+def updateBlogPost(obj):
+    # set default layout
+    obj.setLayout('weblogentry_view')
+    # add providing of IPossibleWeblogEntry interface
+    if not IPossibleWeblogEntry.providedBy(obj):
+        alsoProvides(obj, IPossibleWeblogEntry)
+    # allow discussion for object
+    dt = getToolByName(obj,'portal_discussion')
+    dt.overrideDiscussionFor(obj, 1)
+
+def processBlog(self, path, blogentry_types=["Document",]):
+    catalog = getToolByName(self,'portal_catalog')
+    brains = catalog(path=path, portal_type=blogentry_types)
+    for b in brains:
+        obj = b.getObject()
+        updateBlogPost(obj)
+        print b.getURL(), 'updated'
+    #raise Exception("some error")
+

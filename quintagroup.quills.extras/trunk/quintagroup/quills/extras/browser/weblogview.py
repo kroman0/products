3be318829b324d34194629d8a7a3fbd201f85460
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-02-12 15:52:16 +0000

    Remove debugging code

--- quintagroup.quills.extras/trunk/quintagroup/quills/extras/browser/weblogview.py
+++ quintagroup.quills.extras/trunk/quintagroup/quills/extras/browser/weblogview.py
@@ -5,7 +5,6 @@ from plone.memoize.view import memoize
 from Products.QuillsEnabled.browser.weblogview import WeblogEntryView
 
 class CustomWeblogEntryView(WeblogEntryView):
-    #implements(IWeblogEntryView)
 
     @memoize
     def date(self):
@@ -13,7 +12,6 @@ class CustomWeblogEntryView(WeblogEntryView):
 
     @property
     def month(self):
-        #import pdb;pdb.set_trace()
         return DateTime(self.date()).strftime('%b').capitalize()
 
     @property

51260d2e64d998659fe118f995738f928e9440c5
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-02-13 13:20:42 +0000

    Change weblogadmin portlet: add JavaScript based blogentry adder to specified blog's subfolder

---
+++ quintagroup.quills.extras/trunk/quintagroup/quills/extras/browser/weblogadmin.py
@@ -0,0 +1,34 @@
+from quills.app.portlets.weblogadmin import *
+
+class CustomRenderer(Renderer):
+
+    _template = ViewPageTemplateFile('weblogadmin.pt')
+
+
+    @property
+    def weblog_url(self):
+        weblog_content = self.getWeblogContentObject()
+        return weblog_content.absolute_url()
+
+    @property
+    def create_url(self):
+        weblog_content = self.getWeblogContentObject()
+        try:
+            config = IWeblogEnhancedConfiguration(weblog_content)
+            type_name = config.default_type
+        except TypeError: # Could not adapt, so fall back to default.
+            type_name = 'WeblogEntry'
+        return "/createObject?type_name=%s" % type_name
+
+    @property
+    def options(self):
+        weblog_content = self.getWeblogContentObject()
+        catalog = getToolByName(self.context, 'portal_catalog')
+        bfolders = catalog(
+            path='/'.join(weblog_content.getPhysicalPath()),
+            portal_type='Folder'
+        )
+        wc_path_len = len('/'.join(weblog_content.getPhysicalPath()))
+        cats = [b.getPath()[wc_path_len+1:] for b in bfolders if not len(b.getPath())==wc_path_len ]
+        return cats
+                

a718463958b2824c41dd8d4d85c51e4724b474f6
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-02-13 12:58:15 +0000

    Fix possible problem with FLOOR_DATE breakage

--- quintagroup.quills.extras/trunk/quintagroup/quills/extras/browser/weblogview.py
+++ quintagroup.quills.extras/trunk/quintagroup/quills/extras/browser/weblogview.py
@@ -1,14 +1,19 @@
 from DateTime.DateTime import DateTime
 
 from plone.memoize.view import memoize
-
+from Products.Archetypes.ExtensibleMetadata import FLOOR_DATE
 from Products.QuillsEnabled.browser.weblogview import WeblogEntryView
 
 class CustomWeblogEntryView(WeblogEntryView):
 
     @memoize
     def date(self):
-        return self.context.effective()
+        date = self.context.effective()
+        if date == FLOOR_DATE:
+            date = self.context.modified()
+        if date == FLOOR_DATE:
+            date = self.context.created()
+        return date
 
     @property
     def month(self):

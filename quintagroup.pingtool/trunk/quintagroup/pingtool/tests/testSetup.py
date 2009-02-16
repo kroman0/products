6157a9eb00d8ac39204e2f3886e652af650c5a92
liebster <liebster@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-02-16 19:51:12 +0000

    Modified tests

--- quintagroup.pingtool/trunk/quintagroup/pingtool/tests/testSetup.py
+++ quintagroup.pingtool/trunk/quintagroup/pingtool/tests/testSetup.py
@@ -4,7 +4,7 @@
 
 from quintagroup.pingtool.config import PROJECTNAME
 from base import *
-from config import istalled_types, types_actions
+from config import istalled_types
 
 class TestSetup(TestCase):
 
@@ -14,43 +14,59 @@ class TestSetup(TestCase):
         self.ttool = getattr(self.portal.aq_explicit, 'portal_types')
         self.ptool =  getattr(self.portal.aq_explicit, 'portal_properties')
 
-    def test_installed_products(self):
-        qi = self.qi
-        installed = [ prod['id'] for prod in qi.listInstalledProducts() ]
-        self.failUnless(PROJECTNAME in installed, installed)
-        #self.failUnless('Quills' in installed, installed)
+    def test_installed_uninstalled_products(self):
+        # test that package are installed/uninstalled well
+        self.assertNotEqual(self.qi.isProductInstalled(PROJECTNAME), False, '%s is already installed' % PROJECTNAME)
+        
+        self.qi.uninstallProducts([PROJECTNAME])
+        self.assertNotEqual(self.qi.isProductInstalled(PROJECTNAME), True, '%s is already installed' % PROJECTNAME)
 
-    def test_tool_installed(self):
-        t = getToolByName(self.portal, 'portal_pingtool', None)
+    def test_tool_install_uninstall(self):
+        # test that tool are installed/uninstalled well
+        tname = 'portal_pingtool'
+        t = getToolByName(self.portal, tname, None)
+        self.assertNotEqual(t, False, 'Tool %s not found after installation' % tname)
         self.failUnless(t, t)
         self.failUnless(isinstance(t, PingTool.PingTool), t.__class__)
         self.failUnlessEqual(t.meta_type, 'PingTool')
-        self.failUnlessEqual(t.getId(), 'portal_pingtool')
+        self.failUnlessEqual(t.getId(), tname)
 
-    def test_installedAllTypes(self):
-        # test that all types are installed well
+        self.qi.uninstallProducts([PROJECTNAME])
+        self.assertNotEqual(self.qi.isProductInstalled(PROJECTNAME), True, '%s is already installed' % PROJECTNAME)
+
+        t = getToolByName(self.portal, tname, None)
+	self.assertNotEqual(t, True, 'Tool %s found after uninstallation' % tname)
+
+    def test_install_uninstall_types(self):
+        # test that types are installed/uninstalled well
         ttool = self.ttool
         tids = ttool.objectIds()
         for id in istalled_types:
-            self.failUnless(id in tids, (id, tids))
+            self.assertNotEqual(id in tids, False, 'Type %s not found after installation' % id)
             tinfo = ttool[id]
             self.failUnless(tinfo.product == PROJECTNAME, tinfo.product)
 
-    def test_added_action(self):
-        t = getToolByName(self.portal, 'portal_pingtool', None)
-        ttool = self.ttool
-        for type_actions in types_actions:
-            pttool = ttool[type_actions['type']]
-            for id, name, action, permission, category, visible in type_actions['actions']:
-                pt_actions_ids = [a.getId() for a in pttool.listActions()]
-                pt_action = [a for a in pttool.listActions() if a.id == id][0]
-                self.failUnless(id in pt_actions_ids, pt_actions_ids)
-                self.failUnless(name == pt_action.Title(), pt_action)
-                self.failUnless(action == pt_action.getActionExpression(), pt_action)
-                self.failUnless(permission == pt_action.getPermissions(), pt_action)
-                self.failUnless(category == pt_action.getCategory(), pt_action)
-                self.failUnless(visible == pt_action.getVisibility(), pt_action)
+        self.qi.uninstallProducts([PROJECTNAME])
+        self.assertNotEqual(self.qi.isProductInstalled(PROJECTNAME), True, '%s is already installed' % PROJECTNAME)
+        
+	tids = ttool.objectIds()
+        for id in istalled_types:
+            self.assertNotEqual(id in tids, True, 'Type %s found after uninstallation' % id)
 
+    def test_actions_install_uninstall(self):
+        # test that actions are installed/uninstalled well
+	action = 'ping'
+        ttool = self.ttool
+        portal_action = getToolByName(self.portal, 'portal_actions')
+        object_buttons = portal_action.object_buttons
+        pt_actions_ids = [a.id for a in object_buttons.listActions()]
+        self.assertNotEqual(action in pt_actions_ids, False, 'Action for %s not found after installation' % action)
+        
+        self.qi.uninstallProducts([PROJECTNAME])
+        self.assertNotEqual(self.qi.isProductInstalled(PROJECTNAME), True, '%s is already installed' % PROJECTNAME)
+	
+        pt_actions_ids = [a.id for a in object_buttons.listActions()]
+        self.assertNotEqual(action in pt_actions_ids, True, 'Action for %s found after uninstallation' % action)
 
 def test_suite():
     from unittest import TestSuite, makeSuite

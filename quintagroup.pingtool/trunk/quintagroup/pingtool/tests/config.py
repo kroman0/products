77f5d4103504a771e168cee35c72d6a0d5408140
liebster <liebster@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-02-16 19:51:12 +0000

    Modified tests

--- quintagroup.pingtool/trunk/quintagroup/pingtool/tests/config.py
+++ quintagroup.pingtool/trunk/quintagroup/pingtool/tests/config.py
@@ -7,18 +7,3 @@ s_properties = (("use_folder_tabs", "lines", ["PingTool"]),
 
 # for test_installedAllTypes
 istalled_types = ['PingTool', 'PingInfo']
-
-# for test_added_action
-types_actions = ({'type':'PingTool',
-                  'actions':(('view', 'View', 'string:${object_url}/view', ('Manage portal', ), 'object', True),
-                             ('edit', 'Edit', 'string:${object_url}/edit', ('Modify portal content', ), 'object', True),
-                            ),
-                 },
-                 {'type':'PingInfo',
-                  'actions':(('view', 'View', 'string:${object_url}/view', ('View',), 'object', True),
-                             ('edit', 'Edit', 'string:${object_url}/edit', ('Modify portal content', ), 'object', True),
-                             ('metadata', 'Properties', 'string:${object_url}/properties', ('Modify portal content', ), 'object', True),
-                             ('references', 'References', 'string:${object_url}/reference_graph', ('Modify portal content', 'Review portal content'), 'object', True),
-                            ),
-                 },
-                )

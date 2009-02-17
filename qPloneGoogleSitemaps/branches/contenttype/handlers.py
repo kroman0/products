c686053a8c060e865e1a8af8c280d95296dfd86c
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-02-17 11:15:15 +0000

    Fix bug with breakage on object creation

--- qPloneGoogleSitemaps/branches/contenttype/handlers.py
+++ qPloneGoogleSitemaps/branches/contenttype/handlers.py
@@ -6,13 +6,18 @@ from Products.qPloneGoogleSitemaps.utils import ping_google
 
 @adapter(IAfterTransitionEvent)
 def pingGoogle(event):
+    tr_id = getattr(event.transition,'id','')
+    if not tr_id:
+        # object under creation
+        return 0
+
     object = event.object
     catalog = getToolByName(object, 'portal_catalog')
 
     sitemaps = [b.getObject() for b in catalog(portal_type='Sitemap')]
     if sitemaps:
         plone_home = getToolByName(object, 'portal_url').getPortalObject().absolute_url()
-        wftrans_name = "%s#%s" % (event.workflow.id, event.transition.id)
+        wftrans_name = "%s#%s" % (event.workflow.id, tr_id)
         obj_ptype = object.portal_type
         for sm in sitemaps:
             if wftrans_name in sm.getPingTransitions() \

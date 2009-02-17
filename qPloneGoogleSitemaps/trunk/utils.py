b3a9075c1dc60ccc30d3448c31ed9b2be8edcd57
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-02-17 11:30:31 +0000

    Merged revisions 1673 via svnmerge from http://svn.quintagroup.com/products/qPloneGoogleSitemaps/branches/contenttype
    ........
      r1673 | mylan | 2009-02-17 13:15:15 +0200 (Tue, 17 Feb 2009) | 1 line
      
      Fix bug with breakage on object creation
    ........

--- qPloneGoogleSitemaps/trunk/utils.py
+++ qPloneGoogleSitemaps/trunk/utils.py
@@ -1,12 +1,18 @@
 from urllib2 import urlopen
 from urllib  import quote as urlquote
 
+from Globals import DevelopmentMode
 from OFS.ObjectManager import BadRequestException
 
 from Products.qPloneGoogleSitemaps.config import testing
 
 def ping_google(url, sitemap_id):
     """Ping sitemap to Google"""
+    if DevelopmentMode:
+        #prevent pinging in debug mode
+        print "Pinging Google ...", url, sitemap_id
+        return 0
+
     resurl = url + "/" + sitemap_id
     sitemap_url = urlquote(resurl)
     g = urlopen('http://www.google.com/webmasters/sitemaps/ping?sitemap='+sitemap_url)

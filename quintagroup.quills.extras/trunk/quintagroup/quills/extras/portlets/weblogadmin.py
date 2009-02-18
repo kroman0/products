af801bceb0300ca65dbfaab900728513ae17484a
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-02-18 14:57:22 +0000

    Optimize speed and memory usage for quick blogpost creation portlet

--- quintagroup.quills.extras/trunk/quintagroup/quills/extras/portlets/weblogadmin.py
+++ quintagroup.quills.extras/trunk/quintagroup/quills/extras/portlets/weblogadmin.py
@@ -1,10 +1,14 @@
+from plone.memoize import ram
 from Products.CMFCore.utils import getToolByName
 from quills.app.portlets.weblogadmin import *
+#from plone.app.portlets.cache import render_cachekey
+
+def _categroies_key(method, self):
+    return self.context.getPhysicalPath()
 
 class CustomRenderer(Renderer):
 
     _template = ViewPageTemplateFile('weblogadmin.pt')
-
 
     @property
     def weblog_url(self):
@@ -12,7 +16,8 @@ class CustomRenderer(Renderer):
         return weblog_content.absolute_url()
 
     @property
-    def create_url(self):
+    @ram.cache(_categroies_key)
+    def creation_url(self):
         weblog_content = self.getWeblogContentObject()
         try:
             config = IWeblogEnhancedConfiguration(weblog_content)
@@ -21,8 +26,8 @@ class CustomRenderer(Renderer):
             type_name = 'WeblogEntry'
         return "/createObject?type_name=%s" % type_name
 
-    @property
-    def options(self):
+    @ram.cache(_categroies_key)
+    def categories(self):
         weblog_content = self.getWeblogContentObject()
         catalog = getToolByName(self.context, 'portal_catalog')
         bfolders = catalog(

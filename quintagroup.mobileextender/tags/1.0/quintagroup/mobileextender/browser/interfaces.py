06af869f5cc8155ab44fc1d9fdfac257f0ad65e5
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-03-11 18:06:41 +0000

    Tagging 1.0

---
+++ quintagroup.mobileextender/tags/1.0/quintagroup/mobileextender/browser/interfaces.py
@@ -0,0 +1,64 @@
+from zope import interface, schema
+from zope.schema import vocabulary
+
+from Products.CMFCore.utils import getToolByName
+from quintagroup.mobileextender import mobileextenderMessageFactory as _
+
+class IMobileConfiglet(interface.Interface):
+    """A portlet which can render a classic Plone portlet macro
+    """
+
+    path = schema.TextLine(
+        title=_(u"label_path", default=u"Path"),
+        description=_(u"help_path",
+            default=u"The Physical path for objects found. "
+            "Leave blank to ignore this criterion."),
+        default=u"",
+        required=False,
+    )
+    ptypes = schema.List(
+        title=_(u'Portal Types'),
+        description=_(u"You may search for and choose portal type(s) "
+            "of object(s) to find. Leave blank to ignore this criterion."),
+        default=[],
+        #source = "quintagroup.mobileextender.ptypes",
+        value_type = schema.Choice( title=u"ptypes", source="quintagroup.mobileextender.ptypes" ),
+        required=False,
+    )
+    wfstates = schema.List(
+        title=_(u'Workflow States'),
+        description=_(u"You may search for and choose workflow review state(s) "
+            "of object(s) to find. Leave blank to ignore this criterion."),
+        default=[],
+        value_type = schema.Choice( title=u"review_states", source="quintagroup.mobileextender.wfstates" ),
+        required=False,
+    )
+    excludeids = schema.Text(
+        title=_(u'Exclude Ids'),
+        description=_(u"List ids, for exclude from mobile content marking. Ids must be separated "
+            "by new line. Leave blank to ignore this criterion."),
+        default=u"",
+        #value_type = schema.TextLine(),
+        required=False
+    )
+    excludepaths = schema.Text(
+        title=_(u'Exclude by Physical Paths'),
+        description=_(u"All objects, which physical path starts with one of specified one - will "
+            "be excluded from mobile content marking. Paths must start with portal id "
+            "(as in 'Path' field), and seaprated by new line. Leave blank to ignore this criterion."),
+        default=u"",
+        required=False
+    )
+    #sorton = schema.Choice(
+        #title=_(u'Sort index'),
+        #description=_(u"Check sort index."),
+        #vocabulary = "quintagroup.mobileextender.sortindices",
+        #default='',
+        #required=False
+    #)
+
+    #@interface.invariant
+    #def checkForACriterion(formdata):
+        #if not formdata.ptypes \
+           #and not formdata.path:
+            #raise interface.Invalid("Path of portal type(s) must be entered")
---
+++ quintagroup.mobileextender/tags/1.0/quintagroup/mobileextender/browser/mobilecontrol.py
@@ -0,0 +1,118 @@
+#from zope.component import queryMultiAdapter
+from zope.interface import implements, alsoProvides, Interface
+from zope.component import adapts, getMultiAdapter
+
+
+
+from zope.formlib import form
+from zope.app.form.browser import MultiSelectWidget, TextAreaWidget
+from Products.Five.formlib import formbase
+
+from Products.Five import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFCore.utils import getToolByName
+
+from quintagroup.mobileextender import mobileextenderMessageFactory as _
+from quintagroup.mobileextender.interfaces import IMobile
+from interfaces import IMobileConfiglet
+
+def SelectWidgetFactory( field, request ):
+    vocabulary = field.value_type.vocabulary
+    return MultiSelectWidget( field, vocabulary, request )
+
+def ListTextAreaWidgetFactory( field, request ):
+    taw = TextAreaWidget( field, request )
+    taw.width = 20
+    taw.height = 5
+    return taw
+
+class MobileControl(object):
+    implements(IMobileConfiglet)
+    adapts(Interface)
+
+    ptypes = []
+    excludeids = []
+    wfstates = []
+    excludepaths = []
+
+    def __init__(self, context):
+        self.context = context
+        self.path = getToolByName(context, 'portal_url').getPortalPath()
+
+
+class MobileControlView(formbase.FormBase):
+    """
+    Configlet settings browser view
+    """
+    template = ViewPageTemplateFile('mobilecontrol.pt')
+
+    form_fields = form.Fields(IMobileConfiglet)
+
+    label = _(u"Define mobile content")
+    form_name = _(u"Define mobile content")
+    description = _(u"This configlet allows you to define content, " \
+                    u"which should be identified as mobile content")
+
+    form_fields["ptypes"].custom_widget = SelectWidgetFactory
+    form_fields["wfstates"].custom_widget = SelectWidgetFactory
+    form_fields["excludeids"].custom_widget = ListTextAreaWidgetFactory
+    form_fields["excludepaths"].custom_widget = ListTextAreaWidgetFactory
+    form_fields["path"].render_context = True
+
+    def setUpWidgets(self, ignore_request=False):
+            self.adapters = {}
+            self.adapters[IMobileConfiglet] = MobileControl(self.context)
+
+            self.widgets = form.setUpWidgets(
+                self.form_fields, self.prefix, self.context, self.request,
+                form=self, adapters=self.adapters, ignore_request=ignore_request)
+
+    def is_fieldsets(self):
+        # We need to be able to test for non-fieldsets in templates.
+        return False
+
+    @form.action(_(u"label_search", default=u"Search"), name=u'Search')
+    def handle_search(self, action, data):
+        res = self.getFilteredContent(data)
+        if res:
+            exclids = data.get('excludeids', '')
+            if exclids:
+                exclids = filter(None, [i.strip() for i in exclids.split('\n')])
+                res = filter(lambda b:not b.getId in exclids, res)
+
+            exclpaths = data.get('excludepaths', '')
+            if exclpaths:
+                exclpaths = filter(None, [i.strip() for i in exclpaths.split('\n')])
+                res = filter(lambda b:not [1 for ep in exclpaths if b.getPath().startswith(ep)], res)
+
+            # Mark objects with interface
+            for b in res:
+                ob = b.getObject()
+                if not IMobile.providedBy(ob):
+                    alsoProvides(ob, IMobile)
+
+            #self.status = "Marked %d objects: \n %s" % (len(res), map(lambda b:b.getPath(), res))
+            self.status = "Marked %d objects" % len(res)
+        else:
+            self.status = "No objects found for given criterion"
+
+        #raise Exception(self.status)
+
+        return ''
+
+
+    def getFilteredContent(self, data):
+        purl = getToolByName(self.context, 'portal_url')
+        catalog = getToolByName(self.context, 'portal_catalog')
+
+        ptypes = data.get('ptypes',[])
+        sorton = data.get('sorton',None)
+        wfstates = data.get('wfstates', [])
+
+        query = {'path' : data.get('path', purl.getPortalPath()),}
+        if ptypes: query.update({'portal_type':ptypes})
+        if wfstates: query.update({'review_state':wfstates})
+
+        #print ">>>>>>> query: ", query
+
+        return catalog(**query)

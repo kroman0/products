c1ca3246ac88524a4ce19a4de4d2babcdf10bd62
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-03-12 18:28:10 +0000

    Tagging 1.0.2

---
+++ quintagroup.mobileextender/tags/1.0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.2'
+
+setup(name='quintagroup.mobileextender',
+      version=version,
+      description="Extend some AT content types with mobile_text field and overlap appropriate content types views",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone mobile quintagroup',
+      author='Quintagroup',
+      author_email='support@quintagroup.com',
+      url='http://svn.quintagroup.com/products/quintagroup.mobileextender',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['quintagroup'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )

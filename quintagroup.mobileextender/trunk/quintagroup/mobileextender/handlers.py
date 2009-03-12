96364acacc12f290eebeb17b1ee6c54a8ee76323
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-03-12 18:21:59 +0000

    reindex object when handler add interface

--- quintagroup.mobileextender/trunk/quintagroup/mobileextender/handlers.py
+++ quintagroup.mobileextender/trunk/quintagroup/mobileextender/handlers.py
@@ -5,4 +5,4 @@ def markAsMobile(object, event):
     mobfield = object.Schema().get('mobile_content',None)
     if mobfield and mobfield.get(object):
         alsoProvides(object, IMobile)
-
+        object.reindexObject(idxs=['object_provides'])

35a8ae44261211d90db02fc14df2cb4ae360d7b5
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-03-12 18:08:17 +0000

    Add handlers for automatically mark object with IMobile interface on editing mobile_content field

---
+++ quintagroup.mobileextender/trunk/quintagroup/mobileextender/handlers.py
@@ -0,0 +1,8 @@
+from zope.interface import alsoProvides
+from interfaces import IMobile
+
+def markAsMobile(object, event):
+    mobfield = object.Schema().get('mobile_content',None)
+    if mobfield and mobfield.get(object):
+        alsoProvides(object, IMobile)
+

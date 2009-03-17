6d086a0b339265730a25263b9cc3fa2310e20ef2
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-03-17 16:47:03 +0000

    Hide actions box viewlet for topics categories

--- quintagroup.quills.extras/trunk/quintagroup/quills/extras/browser/viewlets.py
+++ quintagroup.quills.extras/trunk/quintagroup/quills/extras/browser/viewlets.py
@@ -1,9 +1,15 @@
+from zope import interface
 from plone.app.viewletmanager.manager import BaseOrderedViewletManager
 from quills.core.interfaces.enabled import IWeblogEnhanced
+from quills.core.interfaces import ITopicContainer, ITopic
+from quintagroup.quills.extras.browser.interfaces import IWeblogCategory
 
 class ConditionalViewletManager(BaseOrderedViewletManager):
 
+    FILTER_INTERFACES = set([ITopic, IWeblogEnhanced, IWeblogCategory])
     def render(self):
-        if not IWeblogEnhanced.providedBy(self.context):
-            return super(ConditionalViewletManager, self).render()
-        return u''
+        iprovided = set(list(interface.providedBy(self.context)))
+        if iprovided.intersection(self.FILTER_INTERFACES):
+            return u''
+        return super(ConditionalViewletManager, self).render()
+        

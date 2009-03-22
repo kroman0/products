1a1a6731bf5604d62bb14d4bd6f7f07d479ca953
piv <piv@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-03-22 21:56:10 +0000

    add editable mode field as tal expression, remove editable mode for anonymous user, now it is possible to do this using edit mode field

--- quintagroup.ploneformgen.readonlystringfield/trunk/quintagroup/ploneformgen/readonlystringfield/widget.py
+++ quintagroup.ploneformgen.readonlystringfield/trunk/quintagroup/ploneformgen/readonlystringfield/widget.py
@@ -1,9 +1,3 @@
-from zope.component import getMultiAdapter
-
-from AccessControl import ClassSecurityInfo
-
-from Products.CMFCore.permissions import View
-
 from Products.Archetypes.Widget import StringWidget
 from Products.Archetypes.Registry import registerWidget
 
@@ -13,16 +7,6 @@ class ReadonlyStringWidget(StringWidget):
     _properties.update({
         'macro' : "readonlystring",
         })
-
-    security = ClassSecurityInfo()
-    
-    security.declareProtected(View, 'readonly')
-    def readonly(self, context, request):
-        portal_state = getMultiAdapter((context, request), name="plone_portal_state")
-        if portal_state.anonymous():
-            return None
-        else:
-            return '1'
 
 registerWidget(ReadonlyStringWidget,
                title='ReadonlyString',

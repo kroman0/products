b53b2b4bfe0c97e5f67ba40f76de103b3233a199
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-03-23 17:51:53 +0000

    Tagging 1.3.5

---
+++ quintagroup.quills.extras/tags/1.3.5/quintagroup/quills/extras/adapters.py
@@ -0,0 +1,24 @@
+from zope.interface import implements
+
+from quills.core.interfaces import IWeblogEntry
+from quills.core.interfaces.enabled import IPossibleWeblogEntry
+from quills.app.utilities import getArchivePathFor, getArchivePathFor
+from quintagroup.canonicalpath.interfaces import ICanonicalPath
+
+from Products.CMFCore.utils import getToolByName
+
+class quillsCanonicalPathAdapter(object):
+    """Adapts quills entry content to canonical path.
+    """
+    implements(ICanonicalPath)
+
+    def __init__(self, context):
+        self.context = context
+
+    def canonical_path(self):
+        purl = getToolByName(self.context,'portal_url')
+        entry = IWeblogEntry(self.context).__of__(self.context.aq_inner.aq_parent)
+        weblog_content = entry.getWeblogContentObject()
+        weblog_path = '/'+'/'.join(purl.getRelativeContentPath(weblog_content))
+
+        return '%s/%s' % (weblog_path,'/'.join(getArchivePathFor(entry, weblog_content)))

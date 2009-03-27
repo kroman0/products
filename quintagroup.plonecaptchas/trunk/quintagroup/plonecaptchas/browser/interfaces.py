06dd63fb3dd8a01d14588383eff39caacbc4846b
koval <koval@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-03-27 16:35:17 +0000

    added captcha browser view

---
+++ quintagroup.plonecaptchas/trunk/quintagroup/plonecaptchas/browser/interfaces.py
@@ -0,0 +1,16 @@
+from zope.interface import Interface
+
+class ICaptchaView(Interface):
+    """ Captcha generating and verifying view that is wrapper around
+        captcha generation scripts located in skins folder.
+
+        Use the view from a page to generate an image tag ('image_tag' method) 
+        and to verify user input ('verify' method).
+    """
+
+    def image_tag():
+        """Generate an image tag linking to a captcha"""
+
+    def verify(input):
+        """ Verify user input.
+        """

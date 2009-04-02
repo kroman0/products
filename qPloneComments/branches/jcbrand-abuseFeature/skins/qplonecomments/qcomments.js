5e9a60f5bcd549d0f7a4b0223a824f04e3cd74a5
jcbrand <jcbrand@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-04-02 10:00:14 +0000

    Bugfix, preventDefault for all the forms.

--- qPloneComments/branches/jcbrand-abuseFeature/skins/qplonecomments/qcomments.js
+++ qPloneComments/branches/jcbrand-abuseFeature/skins/qplonecomments/qcomments.js
@@ -1,12 +1,12 @@
 function render_abuse_report_form(comment_id) {
-    jq('form#form-report').bind("submit", function(event){
+    jq('form.report_abuse').bind("submit", function(event){
         event.preventDefault();
     });
     render_button = 'input#input-render-abuse-cancel-' + comment_id;
     jq(render_button).attr('disabled', 'disabled');
     var holder = 'span#span-reply-form-holder-' + comment_id;
     var form = holder + ' > span#span-reply-form';
-    jq(form).slideToggle(700);
+    jq(form).slideToggle(500);
     var cancel_button = holder + ' input#input-report-abuse-cancel';
     var qq = jq(cancel_button);
     jq(cancel_button).attr('comment_id', comment_id);

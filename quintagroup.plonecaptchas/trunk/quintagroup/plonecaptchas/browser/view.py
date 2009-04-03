f7d32c17acb81bf3db363181ea7c823bae8b7821
koval <koval@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-04-03 09:17:08 +0000

    now captcha view can be safely called form captcha widget

--- quintagroup.plonecaptchas/trunk/quintagroup/plonecaptchas/browser/view.py
+++ quintagroup.plonecaptchas/trunk/quintagroup/plonecaptchas/browser/view.py
@@ -2,6 +2,7 @@ from DateTime import DateTime
 
 from zope.interface import implements
 
+from Acquisition import aq_parent
 from Products.Five import BrowserView
 from Products.CMFCore.utils import getToolByName
 
@@ -13,21 +14,32 @@ COOKIE_ID = 'captchahashkey'
 class Captcha(BrowserView):
     implements(ICaptchaView)
 
+    def getSafeContext(self):
+        """ Return context for this view that is acquisition aware (it's needed
+            because when this view is called from captcha widget self.context 
+            may be some adapted object and it isn't aqcuisiton wrapped).
+        """
+        if aq_parent(self.context) is not None:
+            return self.context
+        else:
+            return self.context.context
+
     def image_tag(self):
         """ Generate an image tag linking to a captcha """
-        hk = self.context.getCaptcha()
+        context = self.getSafeContext()
+        hk = context.getCaptcha()
         resp = self.request.response
         if COOKIE_ID in resp.cookies:
             # clear the cookie first, clearing out any expiration cookie
             # that may have been set during verification
             del resp.cookies[COOKIE_ID]
         resp.setCookie(COOKIE_ID, hk, path='/')
-        portal_url = getToolByName(self.context, 'portal_url')()
+        portal_url = getToolByName(context, 'portal_url')()
         img_url = '%s/getCaptchaImage/%s' % (portal_url, hk)
         return '<img src="%s" />' % img_url
 
     def verify(self, input):
-        context = self.context
+        context = self.getSafeContext()
         result = False
         try:
             hashkey = self.request[COOKIE_ID]

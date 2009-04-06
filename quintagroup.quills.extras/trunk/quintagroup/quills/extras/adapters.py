0f66c66233e9254e7bf961d53ed2c95673ace2d6
Volodymyr Cherepanyak <chervol@users.noreply.github.com>
2009-04-06 15:10:12 +0000

    canonical path adapter fixes

--- quintagroup.quills.extras/trunk/quintagroup/quills/extras/adapters.py
+++ quintagroup.quills.extras/trunk/quintagroup/quills/extras/adapters.py
@@ -14,8 +14,10 @@ class quillsCanonicalPathAdapter(object):
 
     def canonical_path(self):
         purl = getToolByName(self.context,'portal_url')
+        pw = getToolByName(self.context,'portal_workflow')
+        if not pw.getInfoFor(self.context, 'review_state') == 'published':
+            return '/' + purl.getRelativeContentURL(self.context)
         entry = IWeblogEntry(self.context).__of__(self.context.aq_inner.aq_parent)
         weblog_content = entry.getWeblogContentObject()
-        weblog_path = '/'+'/'.join(purl.getRelativeContentPath(weblog_content))
-
+        weblog_path = '/' + purl.getRelativeContentURL(weblog_content)
         return '%s/%s' % (weblog_path,'/'.join(getArchivePathFor(entry, weblog_content)))

692fc594f9e43aed54413b230420919f60c11b25
koval <koval@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-04-10 16:56:11 +0000

    initial import

---
+++ quintagroup.portlet.cumulus/trunk/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='quintagroup.portlet.cumulus',
+      version=version,
+      description="This package allows you to display your site's tags using a Flash movie that rotates them in 3D. This is a ported to Plone WordPress WP-Cumulus plugin.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone portlet flash tag cloud',
+      author='Bohdan Koval',
+      author_email='koval@quintagroup.com',
+      url='http://svn.quintagroup.com/products/quintagroup.portlet.cumulus',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['quintagroup', 'quintagroup.portlet'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )

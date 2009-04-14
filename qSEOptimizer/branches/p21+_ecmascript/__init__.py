f3c5713aaaa93ad538e7a4bd0024d1f50618152d
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-04-14 11:56:39 +0000

    Remove bad named branch

--- qSEOptimizer/branches/p21+_ecmascript/__init__.py
+++
@@ -1,8 +0,0 @@
-import Globals
-from Products.CMFCore.DirectoryView import registerDirectory
-from AccessControl import allow_module
-
-allow_module('Products.qSEOptimizer.util')
-
-qSEO_globals = globals()
-registerDirectory('skins', qSEO_globals)

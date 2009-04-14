96187d6310f1300622768479421b4efbcf0b3581
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-04-14 11:56:39 +0000

    Remove bad named branch

--- qSEOptimizer/branches/p21+_ecmascript/util.py
+++
@@ -1,6 +0,0 @@
-def createMultiColumnList(self,slist, numCols, sort_on='title_or_id'):
-    try:
-        mcl = self.createMultiColumnList(slist, numCols, sort_on=sort_on)
-        return mcl
-    except AttributeError:
-        return [slist]

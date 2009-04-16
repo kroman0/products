06d3065bf22c3099cf6cf2de9bb75698d4012c06
koval <koval@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-04-16 12:28:45 +0000

    fixed bug with non-ascii characters in tags

--- quintagroup.portlet.cumulus/trunk/quintagroup/portlet/cumulus/catalog.py
+++ quintagroup.portlet.cumulus/trunk/quintagroup/portlet/cumulus/catalog.py
@@ -8,6 +8,8 @@ class GlobalTags(object):
 
     def __init__(self, context):
         self.context = context
+        portal_properties = getToolByName(self.context, 'portal_properties')
+        self.default_charset = portal_properties.site_properties.getProperty('default_charset', 'utf-8')
 
     def getTags(self, number=None):
         """ Entries of 'Categories' archetype field on content are assumed to be tags.
@@ -20,6 +22,6 @@ class GlobalTags(object):
                 number_of_entries = len(index._index[name])
             except TypeError:
                 number_of_entries = 1
-            tags.append((name, number_of_entries, '#'))
+            tags.append((name.decode(self.default_charset), number_of_entries, '#'))
 
         return tags

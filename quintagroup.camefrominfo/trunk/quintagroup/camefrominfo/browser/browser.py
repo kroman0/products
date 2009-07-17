7f6ea2f99b58117e4ff01c375535807fb676d40f
kroman0 <kroman0@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-07-17 11:03:55 +0000

    Updated quintagroup.camefrominfo package

---
+++ quintagroup.camefrominfo/trunk/quintagroup/camefrominfo/browser/browser.py
@@ -0,0 +1,12 @@
+from Products.Five import BrowserView
+from quintagroup.camefrominfo.interfaces import ICameFromInfoUtility
+from zope.component import getUtility
+
+class CameFromInfoView(BrowserView):
+    """
+    """
+    def __call__(self):
+        """get info"""
+        service = getUtility(ICameFromInfoUtility)
+        res = service.getInfo(self.context)
+        return res

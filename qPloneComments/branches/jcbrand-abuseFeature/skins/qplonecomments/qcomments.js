b1c304d670baf42c3677a97c00e07bfe712a7975
jcbrand <jcbrand@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-07-24 23:07:58 +0000

    Fixed a bug with syntax recognition of an older version of jquery

--- qPloneComments/branches/jcbrand-abuseFeature/skins/qplonecomments/qcomments.js
+++ qPloneComments/branches/jcbrand-abuseFeature/skins/qplonecomments/qcomments.js
@@ -4,9 +4,9 @@ function render_abuse_report_form(comment_id) {
     });
     var render_button = 'input#input-render-abuse-cancel-' + comment_id;
     jq(render_button).attr('disabled', 'disabled');
-    var holder = 'span#span-reply-form-holder-' + comment_id;
-    var form = holder + ' > span#span-reply-form';
+    var form = 'span#span-reply-form-' + comment_id;
     jq(form).slideToggle(500);
+    var holder = 'span#span-reply-form-holder-' + comment_id;
     var cancel_button = holder + ' input#input-report-abuse-cancel';
     var qq = jq(cancel_button);
     jq(cancel_button).attr('comment_id', comment_id);
@@ -16,8 +16,7 @@ function remove_abuse_report_form(comment_id, html) {
     jq('form.report_abuse').bind("submit", function(event){
         event.preventDefault();
     });
-    var holder = 'span#span-reply-form-holder-' + comment_id;
-    var form = holder + ' > span#span-reply-form';
+    var form = 'span#span-reply-form-' + comment_id;
     jq(form).fadeOut();
     var render_button = 'input#input-render-abuse-cancel-' + comment_id;
     jq(render_button).attr('disabled', '');

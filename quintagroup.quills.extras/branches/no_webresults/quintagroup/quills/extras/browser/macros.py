162c5d1ce0145b67a25e9e0fde0ac350a52f2e80
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-08-06 16:26:31 +0000

    Move entry & weblog macroses from skins to /browser (acording to quills.app v.1.7.3)

--- quintagroup.quills.extras/branches/no_webresults/quintagroup/quills/extras/browser/macros.py
+++ quintagroup.quills.extras/branches/no_webresults/quintagroup/quills/extras/browser/macros.py
@@ -3,3 +3,9 @@ from quills.app.browser.macros import *
 
 class HeaderMacros(Macros):
     template = ViewPageTemplateFile('quills_header_macros.pt')
+
+class WeblogEntryMacros(Macros):
+    template = ViewPageTemplateFile('quills_entry_macros.pt')
+
+class WeblogMacros(Macros):
+    template = ViewPageTemplateFile('quills_weblog_macros.pt')

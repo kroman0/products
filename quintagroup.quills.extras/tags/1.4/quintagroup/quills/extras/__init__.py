feaa99ab3a691eb6b862191c92aa8064bfa203fb
Volodymyr Cherepanyak <chervol@users.noreply.github.com>
2009-09-10 15:46:30 +0000

    [releaser] tag for 1.4 release

---
+++ quintagroup.quills.extras/tags/1.4/quintagroup/quills/extras/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+#import weblogfolder_patch, keywords_widget_patch
+import keywords_widget_patch
+from zope.i18nmessageid import MessageFactory
+quintagroupQuillsMessageFactory = MessageFactory('quintagroup.quills.extras')

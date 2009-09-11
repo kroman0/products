cb37880b2c15ad1d5f7933c4ea67b51780a17ea0
chervol <chervol@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-09-11 10:23:23 +0000

    [releaser] tag for 0.2 release

---
+++ quintagroup.skin.switcher/tags/0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.2'
+
+setup(name='quintagroup.skin.switcher',
+      version=version,
+      description="Package for switching skin by domain prefix",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='Quintagroup skin switching',
+      author='Quintagroup',
+      author_email='support@quintgroup.com',
+      url='quintagroup.com',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['quintagroup', 'quintagroup.skin'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )

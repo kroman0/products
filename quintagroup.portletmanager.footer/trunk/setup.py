705966e93dd4c68a5d8efc57a864de6494e539ba
olha <olha@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-10-06 13:47:39 +0000

    doc files updated

--- quintagroup.portletmanager.footer/trunk/setup.py
+++ quintagroup.portletmanager.footer/trunk/setup.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 import os
 
@@ -6,7 +7,8 @@ version = '0.1'
 setup(name='quintagroup.portletmanager.footer',
       version=version,
       description="Portlet manager that is rendered in page footer",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.txt").read() + "\n\n" +
+                       open(os.path.join("docs", "INSTALL.txt")).read() + "\n\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[

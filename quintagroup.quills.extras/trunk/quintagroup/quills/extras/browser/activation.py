afeed37a04f284ff0e798cb5cc4592832871b7b0
fenix <fenix@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-10-08 12:50:45 +0000

    fixed ticket #106

---
+++ quintagroup.quills.extras/trunk/quintagroup/quills/extras/browser/activation.py
@@ -0,0 +1,41 @@
+#zope imports 
+from zope.interface import alsoProvides
+from zope.interface import noLongerProvides
+from Products.Five import BrowserView
+
+#plone imports
+from Products.statusmessages.interfaces import IStatusMessage
+
+#quills imports
+from quills.core.interfaces.enabled import IPossibleWeblog
+
+#other imports
+from quintagroup.quills.extras.browser.interfaces import IWeblogCategory
+
+class CategoryActivation(BrowserView):
+    """ This view determines if it's possible to
+        activate blog category or deactivate it.
+    """
+
+    def can_activate(self):
+        return IPossibleWeblog.providedBy(self.context) and \
+               not IWeblogCategory.providedBy(self.context)
+
+    def can_deactivate(self):
+        return IWeblogCategory.providedBy(self.context)
+
+class ActivateBlogCategory(BrowserView):
+
+    def __call__(self):
+        if IWeblogCategory.providedBy(self.context):
+            # deactivate blog category
+            noLongerProvides(self.context, IWeblogCategory)
+            msg = 'blog categroy deactivated'
+        elif IPossibleWeblog.providedBy(self.context):
+            alsoProvides(self.context, IWeblogCategory)
+            msg = 'blog category activated'
+        else:
+            msg = 'not bloggable'
+
+        IStatusMessage(self.request).addStatusMessage(msg, type='info')
+        self.request.response.redirect(self.context.absolute_url())

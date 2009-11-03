2036b7664393dd50f4c5373729552cb0550732c5
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-11-03 16:32:17 +0000

    Added backward compatibility for pathing theme variables in egg_info/theme_vars.txt file

--- quintagroup.themetemplate/trunk/quintagroup/themetemplate/__init__.py
+++ quintagroup.themetemplate/trunk/quintagroup/themetemplate/__init__.py
@@ -1,8 +1,11 @@
 #
 import os
+import warnings
 from paste.script import pluginlib
 
 from quintagroup.themetemplate.qplone3_theme import qPlone3Theme
+
+warned = False
 
 def getEggInfo(output_dir):
     """ Return path to egg info directory, raise error if not found.
@@ -16,4 +19,15 @@ def getEggInfo(output_dir):
 def getThemeVarsFP(egg_info):
     """ Return file system path to theme vars configurations
     """
+    global warned
+    old_path = os.path.join(egg_info, 'theme_vars.txt')
+    if os.path.exists(old_path):
+        if not warned:
+            warnings.warn(
+                "In 1.0 version theme variables from <the.theme.package>/theme_vars.cfg " \
+                "file only will be used. Currently " \
+                "<the.theme.package>/<the.theme.package>.egg_info/theme_vars.txt " \
+                "also supported." , DeprecationWarning, 2)
+            warned = True
+        return old_path
     return os.path.join(egg_info, '..', 'theme_vars.cfg')

bbb4286fdcf243a453aae2f13009aeefa10a131f
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-11-04 16:25:24 +0000

    Fix #133: Added patch for prevent archive url of blog-posts

--- quintagroup.quills.extras/trunk/quintagroup/quills/extras/__init__.py
+++ quintagroup.quills.extras/trunk/quintagroup/quills/extras/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 #import weblogfolder_patch, keywords_widget_patch
 import keywords_widget_patch
+import archive_path_patch
 from zope.i18nmessageid import MessageFactory
 quintagroupQuillsMessageFactory = MessageFactory('quintagroup.quills.extras')

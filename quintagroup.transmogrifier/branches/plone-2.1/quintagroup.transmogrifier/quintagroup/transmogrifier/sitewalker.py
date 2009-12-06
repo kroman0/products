3f1e971315d202012bd26d202b174167ce562be7
piv <piv@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-12-06 08:56:16 +0000

    replace exlude-types option with condition which is far more generic, update test respectively

--- quintagroup.transmogrifier/branches/plone-2.1/quintagroup.transmogrifier/quintagroup/transmogrifier/sitewalker.py
+++ quintagroup.transmogrifier/branches/plone-2.1/quintagroup.transmogrifier/quintagroup/transmogrifier/sitewalker.py
@@ -3,6 +3,7 @@ from zope.app.annotation.interfaces import IAnnotations
 
 from collective.transmogrifier.interfaces import ISection, ISectionBlueprint
 from collective.transmogrifier.interfaces import IFolderish
+from collective.transmogrifier.utils import Condition
 
 from quintagroup.transmogrifier.interfaces import IBaseFolder
 from quintagroup.transmogrifier.logger import VALIDATIONKEY
@@ -21,19 +22,19 @@ class SiteWalkerSection(object):
         self.anno = IAnnotations(transmogrifier)
         self.storage = self.anno.setdefault(VALIDATIONKEY, [])
 
-        excluded_types = options.get('excluded-types', '')
-        self.excluded_types = filter(None, [i.strip() for i in excluded_types.splitlines()])
+        self.condition = Condition(options.get('condition', 'python:True'),
+                                   transmogrifier, name, options)
 
     def getContained(self, obj):
-        contained = [(k, v.getPortalTypeName()) for k, v in obj.contentItems()]
-        contained = [i for i in contained if i[1] not in self.excluded_types]
+        contained = [(k, v) for k, v in obj.contentItems()
+                        if self.condition(None, context=v)]
         return tuple(contained)
 
     def walk(self, obj):
         if IFolderish.providedBy(obj) or IBaseFolder.providedBy(obj):
             contained = self.getContained(obj)
-            yield obj, contained
-            for v in obj.contentValues():
+            yield obj, tuple([(k, v.getPortalTypeName()) for k, v in contained])
+            for k, v in contained:
                 for x in self.walk(v):
                     yield x
         else:
@@ -43,13 +44,13 @@ class SiteWalkerSection(object):
         for item in self.previous:
             yield item
 
+        if not self.condition(None, context=self.context):
+            raise StopIteration
+
         for obj, contained in self.walk(self.context):
-            type_ = obj.getPortalTypeName()
-            if type_ in self.excluded_types:
-                continue
             item = {
                 self.pathkey: '/'.join(obj.getPhysicalPath()[2:]),
-                self.typekey: type_,
+                self.typekey: obj.getPortalTypeName(),
             }
             if contained:
                 item[self.entrieskey] = contained

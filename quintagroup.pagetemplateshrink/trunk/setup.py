e02ffb014b809953ebe5347027fd3a91f1bed3d1
Volodymyr Cherepanyak <chervol@users.noreply.github.com>
2009-12-25 14:02:32 +0000

    typo fixed

--- quintagroup.pagetemplateshrink/trunk/setup.py
+++ quintagroup.pagetemplateshrink/trunk/setup.py
@@ -5,7 +5,7 @@ version = '1.0'
 
 setup(name='quintagroup.pagetemplateshrink',
       version=version,
-      description="Pagetemplates monkeyatch to shrink rendered HTML",
+      description="Pagetemplates monkeypatch to shrink rendered HTML",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers

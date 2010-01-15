1ef8ef366ada9292025b42298101323aaf6c4a5c
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-01-15 05:12:52 +0000

    Force making object with IMobile inteface only if it not yet provided

--- quintagroup.mobileextender/trunk/quintagroup/mobileextender/handlers.py
+++ quintagroup.mobileextender/trunk/quintagroup/mobileextender/handlers.py
@@ -1,8 +1,9 @@
-from zope.interface import alsoProvides
+from zope.interface import alsoProvides, providedBy
 from interfaces import IMobile
 
 def markAsMobile(object, event):
     mobfield = object.Schema().get('mobile_content',None)
     if mobfield and mobfield.get(object):
-        alsoProvides(object, IMobile)
-        object.reindexObject(idxs=['object_provides'])
+        if not IMobile in providedBy(object):
+            alsoProvides(object, IMobile)
+            object.reindexObject(idxs=['object_provides'])

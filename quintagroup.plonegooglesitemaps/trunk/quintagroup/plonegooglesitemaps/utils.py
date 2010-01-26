2fc5017b0684b9f2a875d9a9b6ab0a79a1b9bcaa
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-01-26 18:47:51 +0000

    Add content from qPloneGoogleSitemaps. Fix all imports

---
+++ quintagroup.plonegooglesitemaps/trunk/quintagroup/plonegooglesitemaps/utils.py
@@ -0,0 +1,23 @@
+from urllib2 import urlopen
+from urllib  import quote as urlquote
+
+from Globals import DevelopmentMode
+from OFS.ObjectManager import BadRequestException
+
+from quintagroup.plonegooglesitemaps.config import testing
+
+def ping_google(url, sitemap_id):
+    """Ping sitemap to Google"""
+    if DevelopmentMode:
+        #prevent pinging in debug mode
+        print "Pinging Google ...", url, sitemap_id
+        return 0
+
+    resurl = url + "/" + sitemap_id
+    sitemap_url = urlquote(resurl)
+    g = urlopen('http://www.google.com/webmasters/sitemaps/ping?sitemap='+sitemap_url)
+    result = g.read()
+    g.close()
+    if testing:
+        print "Pinged %s sitemap to google" % resurl
+    return 0

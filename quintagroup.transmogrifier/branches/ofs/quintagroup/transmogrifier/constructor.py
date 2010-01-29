bb0390cfcf3d2664c34789d96f60032254a10f63
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-01-29 12:47:24 +0000

    Updated constructor section for possibility to construct not only CMF objects, registered in fti

--- quintagroup.transmogrifier/branches/ofs/quintagroup/transmogrifier/constructor.py
+++ quintagroup.transmogrifier/branches/ofs/quintagroup/transmogrifier/constructor.py
@@ -1,10 +1,13 @@
 from zope.interface import classProvides, implements
+from zope.component import queryUtility
 from collective.transmogrifier.interfaces import ISectionBlueprint
 from collective.transmogrifier.interfaces import ISection
 from collective.transmogrifier.utils import defaultMatcher
 
 from Acquisition import aq_base
 from Products.CMFCore.utils import getToolByName
+
+from quintagroup.transmogrifier.interfaces import IFTIConstructor4MetaType
 
 class ConstructorSection(object):
     classProvides(ISectionBlueprint)
@@ -18,6 +21,8 @@ class ConstructorSection(object):
         self.typekey = defaultMatcher(options, 'type-key', name, 'type', 
                                       ('portal_type', 'Type'))
         self.pathkey = defaultMatcher(options, 'path-key', name, 'path')
+        self.meta_types = filter(None, [i.strip() for i in
+                                         options.get('meta-types', '').splitlines()])
     
     def __iter__(self):
         for item in self.previous:
@@ -32,20 +37,25 @@ class ConstructorSection(object):
             
             fti = self.ttool.getTypeInfo(type_)
             if fti is None:                           # not an existing type
-                yield item; continue
-            
+                # Look for an IFTIConstructor4MetaType adapter
+                mt = type_.startswith("meta_type:") and type_[10:] or None
+                if mt in self.meta_types:
+                    fti = queryUtility(IFTIConstructor4MetaType, name=mt)
+                if fti is None:
+                    yield item; continue
+
             elems = path.strip('/').rsplit('/', 1)
             container, id = (len(elems) == 1 and ('', elems[0]) or elems)
             context = self.context.unrestrictedTraverse(container, None)
             if context is None:                       # container doesn't exist
                 yield item; continue
-            
+
             if getattr(aq_base(context), id, None) is not None: # item exists
                 yield item; continue
-            
+
             obj = fti._constructInstance(context, id)
             obj = fti._finishConstruction(obj)
             if obj.getId() != id:
                 item[pathkey] = '%s/%s' % (container, obj.getId())
-            
+
             yield item

d6bf8d2606291c6441a57e8cc7c216706452804f
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-01-29 14:05:17 +0000

    Added ofsimporter section for possibility to import OFS Image and File objects

--- quintagroup.transmogrifier/branches/ofs/quintagroup/transmogrifier/ofsmanager.py
+++ quintagroup.transmogrifier/branches/ofs/quintagroup/transmogrifier/ofsmanager.py
@@ -48,6 +48,10 @@ class OFSExporterSection(object):
                     fields['width'] = getattr(obj, 'width', 0)
                     fields['height'] = getattr(obj, 'height', 0)
 
+                if not self.condition(item, context=obj,
+                                      fprop_info=fields):
+                    continue
+
                 files['file-properties'] = {
                     'name': '.file-properties.xml',
                     'data': self.createManifest(fields),
@@ -95,3 +99,76 @@ class OFSExporterSection(object):
         doc.unlink()
         return data
 
+
+class OFSImporterSection(object):
+    classProvides(ISectionBlueprint)
+    implements(ISection)
+
+    def __init__(self, transmogrifier, name, options, previous):
+        self.previous = previous
+        self.context = transmogrifier.context
+
+        self.pathkey = defaultMatcher(options, 'path-key', name, 'path')
+        self.fileskey = defaultMatcher(options, 'files-key', name, 'files')
+        self.contextkey = defaultMatcher(options, 'context-key', name, 'import_context')
+
+        self.condition = Condition(options.get('condition', 'python:True'),
+                                   transmogrifier, name, options)
+        self.ttt = 0
+
+    def __iter__(self):
+        for item in self.previous:
+            pathkey = self.pathkey(*item.keys())[0]
+            fileskey = self.fileskey(*item.keys())[0]
+
+            if not (pathkey and fileskey):
+                yield item; continue
+            if 'file-properties' not in item[fileskey]:
+                yield item; continue
+
+            path = item[pathkey]
+            obj = self.context.unrestrictedTraverse(path, None)
+            if obj is None:         # path doesn't exist
+                yield item; continue
+
+            if obj.meta_type in ["File", "Image"]:
+                if not self.ttt:
+                    self.ttt = 1
+                    import pdb;pdb.set_trace()
+                try:
+                    manifest = item[fileskey]['file-properties']['data']
+                    prop_info = self.parseManifest(manifest)
+                    # Update file/image properties
+                    if not self.condition(item, context=obj,
+                                          fprop_info=prop_info):
+                        continue
+                    for prop_id, prop_val in prop_info.items():
+                        setattr(obj, prop_id, prop_val)
+
+                    # Add data for file/image
+                    name = prop_info.get('__name__', None)
+                    if name is None:
+                        continue
+                    data = item[fileskey].get(name, None)
+                    if data is None:
+                        continue
+                    obj.data = data['data']
+                    
+                except Exception, e:
+                    print "Exception in ofsimporter section:"
+                    print '-'*60
+                    traceback.print_exc()
+                    print '-'*60
+
+            yield item
+
+    def parseManifest(self, manifest):
+        doc = minidom.parseString(manifest)
+        props = {}
+        for prop in doc.getElementsByTagName('prop'):
+            prop_id = str(prop.getAttribute('id'))
+            if not prop_id:
+                continue
+            props[prop_id] = str(prop.firstChild.nodeValue.strip())
+
+        return props

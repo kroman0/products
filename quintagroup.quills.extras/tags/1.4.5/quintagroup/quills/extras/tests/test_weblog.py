25027b04c1a596b57e11b50005e5d9f4669b6d1e
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-02-05 10:26:59 +0000

    [releaser] removing branch

--- quintagroup.quills.extras/tags/1.4.5/quintagroup/quills/extras/tests/test_weblog.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-""" Unit test for weblogish folders.
-
-$Id: test_weblog.py 81282 2009-02-24 22:26:35Z jhackel $
-"""
-from base import *
-from Products.QuillsEnabled.tests import test_weblog
-
-test_suite = test_weblog.test_suite

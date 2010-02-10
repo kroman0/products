9122af598a855a6e9b4ea1f44cf695fb80292f32
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-02-10 12:21:38 +0000

    Fix deprication warining in catalog index registration

--- quintagroup.canonicalpath/trunk/quintagroup/canonicalpath/catalog.py
+++ quintagroup.canonicalpath/trunk/quintagroup/canonicalpath/catalog.py
@@ -1,15 +1,14 @@
+from zope.interface import Interface
 from zope.component import queryAdapter
-from Products.CMFPlone.CatalogTool import registerIndexableAttribute
+from plone.indexer.decorator import indexer
 
 from interfaces import ICanonicalPath
 
-
-def canonical_path(obj, portal, **kwargs):
+@indexer(Interface)
+def canonical_path(obj, **kwargs):
     """Return canonical_path property for the object.
     """
     cpath = queryAdapter(obj, interface=ICanonicalPath)
     if cpath:
         return cpath.canonical_path()
     return None
-
-registerIndexableAttribute('canonical_path', canonical_path)

852e7124d7b2ef80a49f00a6a686846c58b5e70b
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-02-11 12:49:49 +0000

    Remove installation tests as needless

--- quintagroup.canonicalpath/trunk/quintagroup/canonicalpath/tests.py
+++ quintagroup.canonicalpath/trunk/quintagroup/canonicalpath/tests.py
@@ -67,51 +67,9 @@ class TestAdapter(TestCase):
         self.assertFalse(wrapper is None, "No indexer registered for document object")
         
         
-        
-# class TestInstallation(TestCase):
-
-#     def afterSetUp(self):
-#         self.qi = self.portal.portal_quickinstaller
-#         self.qi.installProduct("quintagroup.canonicalpath")
-
-#         self.purl = getToolByName(self.portal, 'portal_url')
-#         self.catalog = getToolByName(self.portal, 'portal_catalog')
-
-#     def testCatalogMetadata(self):
-#         self.assertTrue('canonical_path' in self.catalog._catalog.names,
-#             "'canonical_path' metadata not added to catalog.")
-
-#     def testIndexer(self):
-#         self.loginAsPortalOwner()
-#         my_doc = makeContent(self.portal, portal_type='Document', id='my_doc')
-#         my_doc.update(title='My document')
-
-#         cpadapter = queryAdapter(my_doc, ICanonicalPath)
-#         cpmydoc = cpadapter.canonical_path()
-#         cpbrain = self.catalog(path='/'+my_doc.absolute_url(1))[0].canonical_path
-#         self.assertTrue(cpmydoc == cpbrain,
-#             "Canonical Path from adapter: '%s' not equals with brains data: '%s'" % (
-#              cpmydoc, cpbrain))
-
-#         self.logout()
-
-#     def testCatalogUpdateOnInstallation(self):
-#         self.loginAsPortalOwner()
-#         fp = self.portal['front-page']
-#         cpadapter = queryAdapter(fp, ICanonicalPath)
-#         cpfp = cpadapter.canonical_path()
-#         cpbrain = self.catalog(path='/'+fp.absolute_url(1))[0].canonical_path
-#         self.assertTrue(cpfp == cpbrain,
-#             "Catalog not updated on installation: canonical path from adapter: " \
-#             "'%s' not equal to brain data: '%s'" % (cpfp, cpbrain))
-
-#         self.logout()
-
-
 def test_suite():
     return unittest.TestSuite([
         unittest.makeSuite(TestAdapter),
-        #unittest.makeSuite(TestInstallation),
         ])
 
 if __name__ == '__main__':

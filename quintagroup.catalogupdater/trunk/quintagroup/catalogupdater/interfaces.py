8762cedcddc248ec39d6159e4447738252dce41b
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-02-16 16:04:07 +0000

    Update utility, and interface for updating list of columns in catalog

--- quintagroup.catalogupdater/trunk/quintagroup/catalogupdater/interfaces.py
+++ quintagroup.catalogupdater/trunk/quintagroup/catalogupdater/interfaces.py
@@ -2,8 +2,13 @@ from zope.interface import Interface
 
 class ICatalogUpdater(Interface):
 
-    def updateMetadata4All(catalog, column):
-        """ Update only the column metadata in the catalog
-            for all records
+    def updateMetadata4All(catalog, columns):
+        """ Update metadata in the *catalog* for each column
+            in the *columns* list for all records.
+
+              * catalog - ZCatalog descendent catalog;
+              * columns - list of metadata names, or
+                          string with name of single
+                          metadata, which must be updated.
         """
 

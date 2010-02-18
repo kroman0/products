f115b01dbc3f52c575011f1b821a8de782dc6990
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-02-18 12:28:33 +0000

    Remove needless development dependencies

--- quintagroup.catalogupdater/trunk/setup.py
+++ quintagroup.catalogupdater/trunk/setup.py
@@ -33,6 +33,4 @@ setup(name='quintagroup.catalogupdater',
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      setup_requires=["PasteScript"],
-      paster_plugins = ["ZopeSkel"],
       )

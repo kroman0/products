6158d336ed79496a16b4081770580b2343ecc336
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-03-16 16:00:52 +0000

    #177: Remove experimental code artefacts

--- quintagroup.canonicalpath/trunk/quintagroup/canonicalpath/interfaces.py
+++ quintagroup.canonicalpath/trunk/quintagroup/canonicalpath/interfaces.py
@@ -1,14 +1,8 @@
 from zope.interface import Interface, Attribute
-from zope.schema import URI
 
 class ICanonicalPath(Interface):
     """canonical_path provider interface
     """
-
-    # canonical_path = URI(
-    #     title=u"canonical_path",
-    #     description = u"canonical_path - for the object. Adapter must " \
-    #                   u"implement *setter* and *getter* for the attribute")
 
     canonical_path = Attribute("canonical_path",
         "canonical_path - for the object. Adapter must implement "

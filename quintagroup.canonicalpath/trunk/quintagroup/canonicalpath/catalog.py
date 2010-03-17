dbbbfce32ad800203c7429b0ab7678df2b0a40d2
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-03-17 16:41:32 +0000

    #177: Add canonical_link indexer, fix canonical_path indexer

--- quintagroup.canonicalpath/trunk/quintagroup/canonicalpath/catalog.py
+++ quintagroup.canonicalpath/trunk/quintagroup/canonicalpath/catalog.py
@@ -3,12 +3,22 @@ from zope.component import queryAdapter
 from plone.indexer.decorator import indexer
 
 from interfaces import ICanonicalPath
+from interfaces import ICanonicalLink
 
 @indexer(Interface)
 def canonical_path(obj, **kwargs):
     """Return canonical_path property for the object.
     """
-    cpath = queryAdapter(obj, interface=ICanonicalPath)
-    if cpath:
-        return cpath.canonical_path()
+    adapter = queryAdapter(obj, interface=ICanonicalPath)
+    if adapter:
+        return adapter.canonical_path
+    return None
+
+@indexer(Interface)
+def canonical_link(obj, **kwargs):
+    """Return canonical_link property for the object.
+    """
+    adapter = queryAdapter(obj, interface=ICanonicalLink)
+    if adapter:
+        return adapter.canonical_link
     return None

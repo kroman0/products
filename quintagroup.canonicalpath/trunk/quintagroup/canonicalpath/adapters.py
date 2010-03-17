1044f53c9fbbd6da2a9cc86696b0e67bb1c39511
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-03-17 16:41:03 +0000

    #177: Add ICanonicalLink interface and default adapter

--- quintagroup.canonicalpath/trunk/quintagroup/canonicalpath/adapters.py
+++ quintagroup.canonicalpath/trunk/quintagroup/canonicalpath/adapters.py
@@ -7,8 +7,10 @@ from OFS.interfaces import ITraversable
 from Products.CMFCore.utils import getToolByName
 
 from quintagroup.canonicalpath.interfaces import ICanonicalPath
+from quintagroup.canonicalpath.interfaces import ICanonicalLink
 
-PROPERTY = "canonical_path"
+PROPERTY_PATH = "canonical_path"
+PROPERTY_LINK = "canonical_link"
 
 _is_canonical = re.compile(
     r"\S*$"               # non space and no new line(should be pickier)
@@ -23,32 +25,70 @@ class DefaultCanonicalPathAdapter(object):
 
     def __init__(self, context):
         self.context = context
+        self.purl = getToolByName(self.context,'portal_url')
 
-    def _validate(self, value):
+    def _validate_path(self, value):
+        value.strip()
+        if not _is_canonical(value):
+            raise InvalidValue(value)
+        return value
+
+    def getCanonicalPath(self):
+        """ First of all return value from the PROPERTY_PATH,
+        if PROPERTY_PATH not exist - return default value
+        """
+        if self.context.hasProperty(PROPERTY_PATH):
+            return self.context.getProperty(PROPERTY_PATH)
+
+        return '/'+'/'.join(self.purl.getRelativeContentPath(self.context))
+
+    def setCanonicalPath(self, value):
+        """ First validate value, than add/updater PROPERTY_PATH
+        """
+        value = self._validate_path(value)
+
+        if self.context.hasProperty(PROPERTY_PATH):
+            self.context._updateProperty(PROPERTY_PATH, value)
+        else:
+            self.context._setProperty(PROPERTY_PATH, value, type="string")
+
+    canonical_path = property(getCanonicalPath, setCanonicalPath)
+
+
+class DefaultCanonicalLinkAdapter(object):
+    """Adapts base content to canonical link.
+    """
+    adapts(ITraversable)
+    implements(ICanonicalLink)
+
+    def __init__(self, context):
+        self.context = context
+        self.purl = getToolByName(self.context,'portal_url')
+
+    def _validate_link(self, value):
         value.strip()
         if not _is_canonical(value):
             raise InvalidValue(value)
         return value
         
-
-    def getCanonicalPath(self):
-        """ First of all return value from the PROPERTY,
-        if PROPERTY not exist - return default value
+    def getCanonicalLink(self):
+        """ First of all return value from the PROPERTY_LINK,
+        if PROPERTY_LINK not exist - return default value
         """
-        if self.context.hasProperty(PROPERTY):
-            return self.context.getProperty(PROPERTY)
+        if self.context.hasProperty(PROPERTY_LINK):
+            return self.context.getProperty(PROPERTY_LINK)
 
-        purl = getToolByName(self.context,'portal_url')
-        return '/'+'/'.join(purl.getRelativeContentPath(self.context))
+        return self.context.absolute_url()
 
-    def setCanonicalPath(self, value):
-        """ First validate value, than add/updater PROPERTY
+    def setCanonicalLink(self, value):
+        """ First validate value, than add/updater PROPERTY_LINK
         """
-        value = self._validate(value)
+        value = self._validate_link(value)
 
-        if self.context.hasProperty(PROPERTY):
-            self.context._updateProperty(PROPERTY, value)
+        if self.context.hasProperty(PROPERTY_LINK):
+            self.context._updateProperty(PROPERTY_LINK, value)
         else:
-            self.context._setProperty(PROPERTY, value, type="string")
+            self.context._setProperty(PROPERTY_LINK, value, type="string")
 
-    canonical_path = property(getCanonicalPath, setCanonicalPath)
+    canonical_link = property(getCanonicalLink, setCanonicalLink)
+
--- quintagroup.canonicalpath/trunk/quintagroup/canonicalpath/interfaces.py
+++ quintagroup.canonicalpath/trunk/quintagroup/canonicalpath/interfaces.py
@@ -8,3 +8,11 @@ class ICanonicalPath(Interface):
         "canonical_path - for the object. Adapter must implement "
         "*setter* and *getter* for the attribute")
 
+class ICanonicalLink(Interface):
+    """canonical_link provider interface
+    """
+
+    canonical_link = Attribute("canonical_link",
+        "canonical_link - for the object. Adapter must implement "
+        "*setter* and *getter* for the attribute")
+

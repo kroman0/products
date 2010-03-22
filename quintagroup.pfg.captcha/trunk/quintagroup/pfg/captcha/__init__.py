f85aae423db43808b7ca7b4119389526f60cd115
liebster <liebster@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-03-22 15:32:20 +0000

    Copy all files from qPloneCaptchaField product

--- quintagroup.pfg.captcha/trunk/quintagroup/pfg/captcha/__init__.py
+++ quintagroup.pfg.captcha/trunk/quintagroup/pfg/captcha/__init__.py
@@ -1,5 +1,34 @@
-from zope.i18nmessageid import MessageFactory
-ExamplePortletMessageFactory = MessageFactory('quintagroup.pfg.captcha')
+from Products.CMFCore import utils
+from Products.CMFCore.interfaces import ISiteRoot
+from Products.CMFCore.DirectoryView import registerDirectory
+from Products.Archetypes.atapi import process_types, listTypes
+from Products.GenericSetup import EXTENSION, profile_registry
+
+from permissions import ADD_PERMISSION
+from config import *
+import validators
+import content
+
+registerDirectory(SKINS_DIR, GLOBALS)
 
 def initialize(context):
-    """Initializer called when used as a Zope 2 product."""
+
+    content_types, constructors, ftis = process_types(listTypes(PROJECTNAME), PROJECTNAME)
+
+    utils.ContentInit(
+        PROJECTNAME + ' Content',
+        content_types      = content_types,
+        permission         = ADD_PERMISSION,
+        extra_constructors = constructors,
+        fti                = ftis,
+        ).initialize(context)
+
+    profile_desc = "Installs CaptchaField content type."
+    profile_registry.registerProfile('default',
+                                      PROJECTNAME,
+                                      profile_desc,
+                                     'profiles/default',
+                                      PROJECTNAME,
+                                      EXTENSION,
+                                      for_=ISiteRoot,
+                                    )

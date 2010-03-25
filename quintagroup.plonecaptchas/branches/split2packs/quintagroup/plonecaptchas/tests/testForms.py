09744f3ab32f32351d37886a4ac00a3254ba2586
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-03-25 20:03:05 +0000

    #175: Tests: refactoring TestFormMixing - added publishForm method (unified post and get request methods), take into consideration authenticator

--- quintagroup.plonecaptchas/branches/split2packs/quintagroup/plonecaptchas/tests/testForms.py
+++ quintagroup.plonecaptchas/branches/split2packs/quintagroup/plonecaptchas/tests/testForms.py
@@ -1,4 +1,6 @@
 from base import *
+from urllib import urlencode
+from StringIO import StringIO
 from DateTime import DateTime
 
 # USE PATCH FROM quintagroup.captcha.core
@@ -12,60 +14,74 @@ class TestFormMixing(FunctionalTestCase):
         self.addProduct(PRODUCT_NAME)
         # Add test_captcha layer from quintagroup.captcah.core
         addTestLayer(self)
-
+        # Prepare form data
         self.basic_auth = ':'.join((portal_owner,default_password))
+        self.form_url = ''
+        self.form_method = "POST"
+        self.hasAuthenticator = False
+        self.form_data = self.getFormData()
+        # Prepare captcha related test data
         self.captcha_key = self.portal.captcha_key
-
         self.hashkey = self.portal.getCaptcha()
-        self.req_method = self.getRequestMethod()
-        self.save_url = self.getSaveURL()
-        self.form_extra = self.getFormExtra()
-        self.form_extra['hashkey'] = self.hashkey
-        self.form_extra['key'] = ''
+        self.form_data['hashkey'] = self.hashkey
+        self.form_data['key'] = ''
 
-    def getSaveURL(self):
+    def getFormData(self):
         raise NotImplementedError(
-            "getSaveURL not implemented")
-
-    def getFormExtra(self):
-        raise NotImplementedError(
-            "getFormExtra not implemented")
+            "getFormData not implemented")
         
+    def publishForm(self):
+        stdin_data = None
+        form_url = self.portal.absolute_url(1) + self.form_url
+        # Prepare form data
+        if self.hasAuthenticator:
+            self.form_data['_authenticator'] = self._getauth()
+        form_data = urlencode(self.form_data)
+        if self.form_method.upper() == 'GET':
+            form_url += "?%s" % form_data
+        else:
+            stdin_data = StringIO(form_data)
+        # Publish form and get response
+        response = self.publish(form_url, self.basic_auth,
+            request_method=self.form_method, stdin=stdin_data)
+        return response
+
+    def _getauth(self):
+        # Fix authenticator for the form
+        authenticator = self.portal.restrictedTraverse("@@authenticator")
+        html = authenticator.authenticator()
+        handle = re.search('value="(.*)"', html).groups()[0]
+        return handle
+
     def testImage(self):
-        response = self.publish(self.save_url, self.basic_auth,
-            request_method=self.req_method).getBody()
+        response = self.publishForm().getBody()
         patt = re.compile(IMAGE_PATT  % self.portal.absolute_url())
         match_obj = patt.search(response)
         img_url = match_obj.group(1)
 
-        content_type = self.publish('/plone' + img_url,
-            self.basic_auth).getHeader('content-type')
+        content_type = self.publish('/plone' + img_url).getHeader('content-type')
         self.assertTrue(content_type.startswith('image'),
             "Wrong captcha image content type")
 
     def testSubmitRightCaptcha(self):
         key = getWord(int(parseKey(decrypt(self.captcha_key, self.hashkey))['key'])-1)
-        self.form_extra['key'] = key
+        self.form_data['key'] = key
         
-        response = self.publish(self.save_url, self.basic_auth,
-            extra=self.form_extra, request_method=self.req_method).getBody()
+        response = self.publishForm().getBody()
         self.assertFalse(NOT_VALID.search(response))
 
     def testSubmitWrongCaptcha(self):
-        self.form_extra['key'] = 'wrong word'
-        response = self.publish(self.save_url, self.basic_auth,
-            extra=self.form_extra, request_method=self.req_method).getBody()
+        self.form_data['key'] = 'wrong word'
+        response = self.publishForm().getBody()
+        open('/tmp/pc.wrongcaptcha.html','w').write(response)
         self.assertTrue(NOT_VALID.search(response))
 
     def testSubmitRightCaptchaTwice(self):
         key = getWord(int(parseKey(decrypt(self.captcha_key, self.hashkey))['key'])-1)
-        self.form_extra['key'] = key
+        self.form_data['key'] = key
 
-        self.publish(self.save_url, self.basic_auth,
-            extra=self.form_extra, request_method=self.req_method)
-
-        response = self.publish(self.save_url, self.basic_auth,
-            extra=self.form_extra, request_method=self.req_method).getBody()
+        self.publishForm()
+        response = self.publishForm().getBody()
         
         open('/tmp/pc.rct.html','w').write(response)
         self.assertTrue(NOT_VALID.search(response))

27b14146b154c9f9102426a6bb53e2c329a54f20
fenix <fenix@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-03-26 16:18:38 +0000

    added compatibility with plone 2.5-3.3

--- quintagroup.canonicalpath/trunk/quintagroup/canonicalpath/catalog.py
+++ quintagroup.canonicalpath/trunk/quintagroup/canonicalpath/catalog.py
@@ -1,7 +1,24 @@
 from zope.interface import Interface
 from zope.component import queryAdapter
-from plone.indexer.decorator import indexer
 
+#for compatibility with older plone versions 
+try:
+    from plone.indexer.decorator import indexer 
+    IS_NEW = True
+except:
+    IS_NEW = False
+    class IDummyInterface:pass
+    class indexer:
+
+         def __init__(self, *interfaces):
+            self.interfaces = IDummyInterface,
+
+         def __call__(self, callable):
+             callable = lambda *args, **kw:None
+             callable.__component_adapts__ = self.interfaces
+             callable.__implemented__ = Interface
+             return callable
+    
 from interfaces import ICanonicalPath
 from interfaces import ICanonicalLink
 
@@ -22,3 +39,9 @@ def canonical_link(obj, **kwargs):
     if adapter:
         return adapter.canonical_link
     return None
+
+#for compatibility with older plone versions 
+if not IS_NEW:
+    from Products.CMFPlone.CatalogTool import registerIndexableAttribute
+    map(registerIndexableAttribute, ('canonical_path', 'canonical_link'),
+                                    (canonical_path, canonical_link))

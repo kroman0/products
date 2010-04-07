2f1cfd78316a46464a5fa2b1453960091494c3e1
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-04-07 14:04:51 +0000

    #185: Add registration tests

--- quintagroup.z3cform.captcha/branches/captchas-refactoring/quintagroup/z3cform/captcha/tests.py
+++ quintagroup.z3cform.captcha/branches/captchas-refactoring/quintagroup/z3cform/captcha/tests.py
@@ -1,10 +1,24 @@
 import unittest
 
+from zope.interface import alsoProvides
+from zope.schema.interfaces import IField
+from zope.component import queryMultiAdapter
+from zope.publisher.browser import TestRequest
+
+from z3c.form.interfaces import IFormLayer
+from z3c.form.interfaces import IValidator
+from z3c.form.interfaces import IFieldWidget
+from z3c.form.interfaces import IErrorViewSnippet
+
 from Products.Five import zcml
 from Products.Five import fiveconfigure
 from Testing import ZopeTestCase as ztc
 from Products.PloneTestCase.layer import onsetup
 from Products.PloneTestCase import PloneTestCase as ptc
+
+from quintagroup.z3cform.captcha import Captcha
+from quintagroup.z3cform.captcha import CaptchaWidget
+from quintagroup.z3cform.captcha.widget import CaptchaWidgetFactory
 
 @onsetup
 def setup_product():
@@ -23,6 +37,41 @@ setup_product()
 ptc.setupPloneSite(extension_profiles=['quintagroup.captcha.core:default',])
 
 
+class TestRegistrations(ptc.PloneTestCase):
+
+    def afterSetUp(self):
+        super(TestRegistrations, self).afterSetUp()
+        self.request = self.app.REQUEST
+        alsoProvides(self.request, IFormLayer)
+
+    def testCaptchaFieldInterface(self):
+        self.assertEqual(IField.implementedBy(Captcha), True)
+
+    def testCaptchaWidgetInterface(self):
+        self.assertEqual(IFieldWidget.implementedBy(CaptchaWidgetFactory), True)
+
+    def testWidgetRegistration(self):
+        cfield = Captcha()
+        cwidget = queryMultiAdapter((cfield, self.request), IFieldWidget)
+        self.assertNotEqual(cwidget, None)
+
+    def testValidatorRegistration(self):
+        cfield = Captcha()
+        cvalidator = queryMultiAdapter((None, self.request, None, cfield, None),
+                IValidator)
+        self.assertNotEqual(cvalidator, None)
+
+    def testErrorViewRegistration(self):
+        cfield = Captcha()
+        cwidget = queryMultiAdapter((cfield, self.request), IFieldWidget)
+        error = ValueError()
+        eview = queryMultiAdapter(
+            (error, self.request, cwidget, cfield, None, None),
+            IErrorViewSnippet)
+        self.assertNotEqual(eview, None)
+
+
 def test_suite():
     suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestRegistrations))
     return suite

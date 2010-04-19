912f21fbde0501aac04aa02af977448e7a348569
olha <olha@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-04-19 15:02:04 +0000

    doc files updated.

--- quintagroup.canonicalpath/trunk/setup.py
+++ quintagroup.canonicalpath/trunk/setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.6dev'
+version = '0.6'
 
 tests_require=['zope.testing']
 setup(name='quintagroup.canonicalpath',
       version=version,
-      description="Bring canonical path calculation functionality",
+      description="Brings canonical path calculation functionality",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers

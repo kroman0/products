7d5efc8d961bc7c6d815ee2a7bcdc756c61f1078
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-05-05 14:33:31 +0000

    Added basic ReferenceDataGridField, ReferenceDataGridWidget

--- quintagroup.referencedatagridfield/trunk/quintagroup/referencedatagridfield/__init__.py
+++ quintagroup.referencedatagridfield/trunk/quintagroup/referencedatagridfield/__init__.py
@@ -1,4 +1,5 @@
-  # -*- extra stuff goes here -*- 
+from quintagroup.referencedatagridfield._field import ReferenceDataGridField
+from quintagroup.referencedatagridfield._field import ReferenceDataGridWidget
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""

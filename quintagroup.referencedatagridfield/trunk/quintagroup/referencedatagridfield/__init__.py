30816c1eeba96f4cffc96c62880edef0f0f1310c
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-05-05 19:34:23 +0000

    Added example content type

--- quintagroup.referencedatagridfield/trunk/quintagroup/referencedatagridfield/__init__.py
+++ quintagroup.referencedatagridfield/trunk/quintagroup/referencedatagridfield/__init__.py
@@ -1,5 +1,23 @@
 from quintagroup.referencedatagridfield._field import ReferenceDataGridField
 from quintagroup.referencedatagridfield._field import ReferenceDataGridWidget
 
+from Products.CMFCore.permissions import AddPortalContent
+from Products.CMFCore.utils import ContentInit
+from Products.Archetypes.atapi import listTypes
+from Products.Archetypes.atapi import process_types
+
+PKG_NAME = 'quintagroup.referencedatagridfield'
+
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
+    # Example content type initialization
+    import quintagroup.referencedatagridfield.examples
+    content_types, constructors, ftis = process_types(listTypes(PKG_NAME), PKG_NAME,)
+
+    ContentInit(
+        '%s Content' % PKG_NAME,
+        content_types = content_types,
+        permission = AddPortalContent,
+        extra_constructors = constructors,
+        fti = ftis,
+        ).initialize(context)

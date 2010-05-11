d84372ecdf1ae6efd0759444bbdb857e1464fa8c
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-05-11 10:41:04 +0000

    Added BlockColumn with possiblity to blocking and styling column based on row data values

--- quintagroup.referencedatagridfield/trunk/quintagroup/referencedatagridfield/columns.py
+++ quintagroup.referencedatagridfield/trunk/quintagroup/referencedatagridfield/columns.py
@@ -26,3 +26,52 @@ class HiddenColumn(Column):
 
 # Initializes class security
 InitializeClass(HiddenColumn)
+
+class BlockColumn(Column):
+    """ Column with possibility to block the cell data from changing,
+        based on the value of the row data.
+    """
+    security = ClassSecurityInfo()
+
+    def __init__(self, label, default=None, label_msgid=None,
+                 column_on_class=None, column_off_class=None,
+                 columns=[], invert=False, read_only=None):
+        """ Create a column, with adding class attribute depend on value presence for the cell.
+        
+            @param column_on_class set class, which will be added to the <input> tag if columns has value
+            @param column_off_class set class, which will be added to the <input> tag if columns not has value
+            @param columns list of columns names, which values will be checked for set class attribute
+            @param invert - invert the adding class logic
+            @param read_only - check-on/off readOnly attribute for the <input> tag
+        """
+        Column.__init__(self, label, default, label_msgid)
+        self.column_on_class = column_on_class
+        self.column_off_class = column_off_class
+        self.columns = columns
+        self.invert = invert
+        self.read_only = read_only
+
+    def passCondition(self, row_data):
+        """ Return calculated class attribute."""
+        res = sum([1 for c in self.columns if row_data.get(c,0)])
+        return res == len(self.columns) and not self.invert
+    
+    security.declarePublic('getAttributesData')
+    def getAttributesData(self, row_data):
+        """ Return calculated class attribute."""
+        res = {'style_class': None, 'read_only': None}
+        isPassCondition = self.passCondition(row_data)
+        if self.column_on_class or self.column_off_class:
+           res['style_class'] = isPassCondition and self.column_on_class or self.column_off_class
+        if self.read_only:
+           res['read_only'] = isPassCondition and True or False
+
+        return res
+    
+    security.declarePublic('getMacro')
+    def getMacro(self):
+        """ Return macro used to render this column in view/edit """
+        return "datagrid_block_cell"
+
+# Initializes class security
+InitializeClass(BlockColumn)

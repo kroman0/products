be84ef0d2ed5568e502279ff07047ff3b2531274
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-05-14 14:31:54 +0000

    Added focus and blur event-handlers to referencedatagridwidget javascripts

--- quintagroup.referencedatagridfield/branches/plone4/quintagroup/referencedatagridfield/skins/referencedatagridfield/referencedatagridwidget.js
+++ quintagroup.referencedatagridfield/branches/plone4/quintagroup/referencedatagridfield/skins/referencedatagridfield/referencedatagridwidget.js
@@ -18,3 +18,46 @@ dataGridFieldFunctions.addReferenceDataGridRow = function(id) {
     // Update row with Reference popup functionality
     jq(preLastRow).prepRefPopup();
 }
+
+// Service scripts used in referencebrowser.js
+
+function setClassAttr(element, value) {
+    if (element.className) {
+        element.className = value
+    } else {
+        element.setAttribute("CLASS", value)
+    } 
+}
+
+function triggerTitleClass(e) {
+    var currnode = window.event ? window.event.srcElement : e.currentTarget;
+    
+	// fetch required data structure   
+    var element = getThisOrParentElement(currnode, "INPUT");
+    // If no input tag found - leave function
+    if (element == null || element.tagName.toUpperCase() == "BODY")
+	return;
+    
+    var current = element.value;
+    var initial = element.getAttribute("default_value");
+    if (initial == null || current == null)
+	return;
+
+    if (initial == current) {
+       setClassAttr(element, "not-changed-title-field")
+    } else {
+       setClassAttr(element, "changed-title-field")
+    }
+}
+
+// Trigger styles on focusing on the element
+function triggerOnFocusStyles(e) {
+    var currnode = window.event ? window.event.srcElement : e.currentTarget;
+    
+	// fetch required data structure   
+    var element = getThisOrParentElement(currnode, "INPUT");
+    // If no input tag found - leave function
+    if (element == null || element.tagName.toUpperCase() == "BODY")
+	return;
+    setClassAttr(element, "changed-title-field")
+}

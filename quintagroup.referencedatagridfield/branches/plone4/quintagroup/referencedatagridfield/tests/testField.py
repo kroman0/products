c04e33d5bb7544e9ba4af7a7f5ceb4089db12fe6
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-05-18 20:29:24 +0000

    Added test for link, uid, title for the field

--- quintagroup.referencedatagridfield/branches/plone4/quintagroup/referencedatagridfield/tests/testField.py
+++ quintagroup.referencedatagridfield/branches/plone4/quintagroup/referencedatagridfield/tests/testField.py
@@ -5,17 +5,14 @@ from quintagroup.referencedatagridfield.tests.base import TestCase
 from quintagroup.referencedatagridfield import ReferenceDataGridWidget
 
 
-class TestField(TestCase):
-    """ Field unit tests """
+class TestReferenceDataGridField(TestCase):
+    """ ReferenceDataGridField unit tests """
 
     def afterSetUp(self):
         self.createDemo()
         self.loginAsPortalOwner()
         self.refcat = self.portal.reference_catalog
         self.field = self.demo.getField('demo_rdgf')
-        self.data = [{"uid": self.doc.UID(),
-                      "link": "http://test.link",
-                      "title": "test"},]
 
     def testColumnProperties(self):
         self.assertEqual(hasattr(self.field, "columns"), True)
@@ -32,9 +29,10 @@ class TestField(TestCase):
         self.assertEqual(len(field_data), 0)
 
     def testGet(self):
+        data = [{"uid": self.doc.UID(), "link": "http://test.link", "title": "test"},]
         # if data set:
         # result is list with dictionary items, as in DataGridField
-        self.field.set(self.demo, self.data)
+        self.field.set(self.demo, data)
 
         field_data = self.field.get(self.demo)
         self.assertEqual(type(field_data), ListType)
@@ -48,8 +46,75 @@ class TestField(TestCase):
         self.assertEqual(item_data.has_key("link"), True)
         self.assertEqual(item_data.has_key("title"), True)
 
+    def getData(self, key, index=0):
+        data = self.field.get(self.demo)
+        return data and data[index].get(key, None) or None
+
+    def getRefsCount(self):
+        return len(self.refcat.getReferences(self.demo, self.field.relationship))
+
+    def testSetUID(self):
+        # link always must present in the data
+        row = {"uid": "", "link": "/"}
+        data = [row,]
+        # If set unexistent UID - UID - not set
+        row['uid'] = "123"
+        self.field.set(self.demo, data)
+        self.assertEqual(self.getData("uid"), None)
+        # No references to the object
+        self.assertEqual(self.getRefsCount(), 0)
+
+        # If link is not remote url and passed uid of existent object  - uid is saved
+        row["uid"] = self.doc.UID()
+        self.field.set(self.demo, data)
+        self.assertEqual(self.getData("uid"), self.doc.UID())
+        # Also reference added to the object catalog
+        self.assertEqual(self.getRefsCount(), 1)
+
+    def testSetTitleForLink(self):
+        row = {"link": "http://google.com"}
+        data = [row,]
+        # If there is title data with external link - it is stored in the field
+        row["title"] = "google"
+        self.field.set(self.demo, data)
+        self.assertEqual(self.getData("title"), "google")
+
+        # If No title specified for the external link title will be  equals to link
+        row["title"] = ""
+        self.field.set(self.demo, data)
+        self.assertEqual(self.getData("title"), "http://google.com")
+        
+        self.assertEqual(self.getRefsCount(), 0)
+        
+    def testSetTitleForUID(self):
+        row = {"uid": self.doc.UID(), "link": "/"}
+        data = [row,]
+        # If there is title data with correct uid - it is stored in the field
+        row["title"] = "Custom Title"
+        self.field.set(self.demo, data)
+        self.assertEqual(self.getData("title"), "Custom Title")
+
+        # If No title specified with correct portal UID object - 
+        # title will be get from the object
+        row["title"] = ""
+        self.field.set(self.demo, data)
+        self.assertEqual(self.getData("title"), "Test Document")
+
+    def testNoLink(self):
+        # Link is key data for the field.
+        # If no link present in the data - no data will be saved
+        # even with correct uid.
+        data = [{"uid": self.doc.UID(), "title": "Title"},]
+        self.field.set(self.demo, data)
+        self.assertEqual(self.field.get(self.demo), [])
+        # If a external link present in the data - it will be saved    
+        data = [{"link": "http://google.com"},]
+        self.field.set(self.demo, data)
+        self.assertEqual(self.getData("link"), "http://google.com")
+       
+        
 
 def test_suite():
     return unittest.TestSuite([
-        unittest.makeSuite(TestDataGridRelatedField),
+        unittest.makeSuite(TestReferenceDataGridField),
         ])

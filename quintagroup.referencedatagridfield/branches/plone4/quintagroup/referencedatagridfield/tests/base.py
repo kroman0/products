57c1d883a21ee244a6318f91f866822026bcb380
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-05-18 16:04:12 +0000

    Added base tests structure, move tests into the tests package

---
+++ quintagroup.referencedatagridfield/branches/plone4/quintagroup/referencedatagridfield/tests/base.py
@@ -0,0 +1,61 @@
+import unittest
+
+#from zope.testing import doctestunit
+#from zope.component import testing
+from Products.Five import zcml
+from Products.Five import fiveconfigure
+from Testing import ZopeTestCase as ztc
+
+from Products.Archetypes.tests.utils import makeContent
+
+from Products.Five import fiveconfigure
+from Products.PloneTestCase import PloneTestCase as ptc
+from Products.PloneTestCase.layer import PloneSite
+
+# install site
+ptc.setupPloneSite(extension_profiles=[
+        'quintagroup.referencedatagridfield:default',
+        'quintagroup.referencedatagridfield:examples'
+        ])
+
+import quintagroup.referencedatagridfield
+
+class MixIn(object):
+    """ Mixin for setting up the necessary bits for testing the
+        quintagroup.referencedatagridfield
+    """
+
+    class layer(PloneSite):
+
+        @classmethod
+        def setUp(cls):
+            fiveconfigure.debug_mode = True
+            zcml.load_config('configure.zcml',
+                             quintagroup.referencedatagridfield)
+            ztc.installPackage(quintagroup.referencedatagridfield)
+            fiveconfigure.debug_mode = False
+
+        @classmethod
+        def tearDown(cls):
+            pass
+
+    # def createDefaultStructure(self):
+    #     if 'layer1' not in self.portal.objectIds():
+    #         self.setRoles(['Manager'])
+    #         makeContent(self.portal, portal_type='Folder', id='layer1')
+    #         self.portal.layer1.setTitle('Layer1')
+    #         self.portal.layer1.reindexObject()
+    #         makeContent(self.portal.layer1, portal_type='Folder', id='layer2')
+    #         self.folder = self.portal.layer1.layer2
+    #         self.folder.setTitle('Layer2')
+    #         self.folder.reindexObject()
+    #         self.setRoles(['Member'])
+    #     return self.portal.layer1.layer2
+
+    # def removeDefaultStructure(self):
+    #     if 'layer1' in self.portal.objectIds():
+    #         self.portal._delObject('layer1')
+
+
+class TestCase(MixIn, ptc.PloneTestCase):
+    """ Base TestCase for quintagroup.referencedatagridfield """

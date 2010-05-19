d2b89fcd3d8fc8b109e3a104d29037fbf164276e
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-05-19 17:57:47 +0000

    Copy plone3 version into branches/plone3

---
+++ quintagroup.referencedatagridfield/branches/plone3/quintagroup/referencedatagridfield/examples/ReferenceDataGridDemoType.py
@@ -0,0 +1,28 @@
+from AccessControl import ClassSecurityInfo
+
+from Products.Archetypes.public import *
+from Products.ATContentTypes.content.base import ATCTContent
+
+
+from quintagroup.referencedatagridfield import PKG_NAME
+from quintagroup.referencedatagridfield import ReferenceDataGridField
+from quintagroup.referencedatagridfield import ReferenceDataGridWidget
+
+class ReferenceDataGridDemoType(ATCTContent):
+    """ Simple ReferenceDataGridField demo."""
+    security = ClassSecurityInfo()
+    schema = BaseSchema + Schema((
+
+        ReferenceDataGridField('DemoReferenceDataGridField',
+            schemata='default',
+            relationship="demo_relation",
+            widget = ReferenceDataGridWidget(
+                label = "Reference DataGrid Field(s)",
+                visible = {'edit' : 'visible', 'view' : 'visible'}
+            )
+        ),
+    ))
+
+    meta_type = portal_type = archetype_name = 'ReferenceDataGridDemoType'
+
+registerType(ReferenceDataGridDemoType, PKG_NAME)

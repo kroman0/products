2c3efeba413546e5e727c50a872f45746e0c0547
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-05-19 16:10:49 +0000

    Added tests for pupup related staff in edit mode of ReferenceDataGridWidget

--- quintagroup.referencedatagridfield/branches/plone4/quintagroup/referencedatagridfield/tests/testPopup.py
+++ quintagroup.referencedatagridfield/branches/plone4/quintagroup/referencedatagridfield/tests/testPopup.py
@@ -1,3 +1,4 @@
+import re
 import unittest
 
 from zope.publisher.browser import TestRequest
@@ -6,8 +7,12 @@ from zope.component import queryAdapter, queryMultiAdapter
 
 from Products.Five import BrowserView
 
+from Products.PloneTestCase.PloneTestCase import portal_owner
+from Products.PloneTestCase.PloneTestCase import default_password
+
 from quintagroup.referencedatagridfield import ReferenceDataGridWidget
 from quintagroup.referencedatagridfield.tests.base import TestCase
+from quintagroup.referencedatagridfield.tests.base import FunctionalTestCase
 
 
 class TestPopupRegistrations(TestCase):
@@ -30,7 +35,27 @@ class TestPopupRegistrations(TestCase):
         self.assertEqual(rdgw_props.get("popup_name", ""), "datagridref_popup")
 
 
+class TestPopupRelatedStaff(FunctionalTestCase):
+    """Test pupup related staff in edit mode of ReferenceDataGridWidget."""
+
+    def testAddReferenceButton(self):
+        self.loginAsPortalOwner()
+        # Prepare testing data and data for functional test
+        self.createDemo()
+        self.demo.edit(demo_rdgf=[{"link": "http://google.com"}])
+        html = self.publish("/%s/edit" % self.demo.absolute_url(1),
+                            portal_owner+':'+default_password).getBody()
+        # Get Add... button
+        rebutt = re.compile("<input\s+[^>]*type=\"button\"\s*[^>]*>", re.S)
+        add_button = filter(lambda k:not "_new" in k, rebutt.findall(html))[0]
+        src = re.search("src=\"([^\"]*)\"", add_button).groups()[0]
+        # src url must points to refdatagridbrowser_popup view
+        url = self.demo.absolute_url() + '/refdatagridbrowser_popup?'
+        self.assertEqual(src.startswith(url), True)
+
+
 def test_suite():
     return unittest.TestSuite([
         unittest.makeSuite(TestPopupRegistrations),
+        unittest.makeSuite(TestPopupRelatedStaff),
         ])

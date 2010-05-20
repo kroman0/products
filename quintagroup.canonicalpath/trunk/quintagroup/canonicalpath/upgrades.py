d106937071c73d985a5abb8e03aea29677b46f03
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-05-20 16:52:54 +0000

    Add logging to upgrades.CanonicalConverter

--- quintagroup.canonicalpath/trunk/quintagroup/canonicalpath/upgrades.py
+++ quintagroup.canonicalpath/trunk/quintagroup/canonicalpath/upgrades.py
@@ -1,5 +1,7 @@
-import logging
-from zope.component import queryAdapter
+from logging import NOTSET, DEBUG, INFO, ERROR
+from logging import Logger, StreamHandler, Formatter
+from StringIO import StringIO
+from zope.component import getAdapter
 #from zope.component import queryMultiAdapter
 from Acquisition import aq_base, aq_inner
 #from Products.CMFCore.utils import getToolByName
@@ -12,17 +14,21 @@ from quintagroup.canonicalpath.interfaces  import ICanonicalLink
 class CanonicalConvertor(object):
     """Convert canonical link to canonical path and vice versa."""
 
-    def __init__(self, portal_url, logger_name="quintagroup.canonicalpath"):
+    def __init__(self, portal_url):
         """Create instanse of convertor.
            - *portal_url* (string), add in the front of canonical path property
              value for get canonical link.
            - *logger_name* - name of the logger
         """
-        self.initLogger(logger_name)
+        self._initLogger()
         self.portal_url = portal_url
 
-    def initLogger(self, lname):
-        self.logger = logging.getLogger(lname)
+    def getLogs(self):
+        self._inout.flush()
+        return self._inout.getvalue()
+
+    def cleanupLogs(self):
+        self._inout = StringIO()
 
     def convertLinkToPath(self, obj):
         """Convert canonical link to canonical path"""
@@ -41,38 +47,46 @@ class CanonicalConvertor(object):
            Return True is successfull, False otherwise.
            Log results in logger.
         """
-        src = queryAdapter(obj, src_iface)
-        dst = queryAdapter(obj, dst_iface)
-        # XXX: Check is this correct work XXX
-        obj = aq_base(aq_inner(obj))
-        # XXX
-        if src is not None \
-           and dst is not None:
-            msg = "Migrate %s into %s for %s object: " \
-                   % (src_iface, dst_iface, obj.absolute_url())
-            try:
-                converter(src, dst)
-            except Exception, e:
-                lev = logging.ERROR
-                msg += "WITH ERROR: %s" % str(e)
-            else:
-                lev = logging.INFO
-                msg += "SUCCESSFUL"
-
-            self.logger.log(lev, msg)
+        msg = "Migrate %s into %s for %s object: " \
+               % (src_iface.__name__, dst_iface.__name__, obj.absolute_url())
+        try:
+            src = getAdapter(obj, src_iface)
+            dst = getAdapter(obj, dst_iface)
+            # XXX: Check is this correct work XXX
+            obj = aq_base(aq_inner(obj))
+            # XXX
+            converter(src, dst)
+        except Exception:
+            import sys
+            et, em, etr = map(str, sys.exc_info())
+            lev = ERROR
+            msg += "ERROR: %s: %s" % (et, em)
+        else:
+            lev = INFO
+            msg += "SUCCESS"
+        self._logger.log(lev, msg)
             
-            return lev == logging.INFO and True or False
+        return lev == INFO and True or False
     
     def _convertP2L(self, src, dst):
         """Convert canonical path to canonical link."""
         cpath = src.canonical_path
         cpath = cpath.startswith('/') and cpath or '/' + cpath
         dst.canonical_link = self.portal_url + cpath
-        del dst.canonical_path
+        del src.canonical_path
 
     def _convertL2P(self, src, dst):
         """Convert canonical link to canonical path."""
         raise NotImplementedError(
             "Convertion from canonical link to canonical path not implemented")
 
+    def _initLogger(self):
+        self._inout = StringIO()
+        handler = StreamHandler(self._inout)
+        handler.setLevel(DEBUG)
+        formatter = Formatter(fmt="[%(asctime)s]: %(message)s",
+                                      datefmt="%H:%M:%S")
+        handler.setFormatter(formatter)
+        self._logger = Logger("quintagroup.canonicalpath", NOTSET)
+        self._logger.addHandler(handler)
 

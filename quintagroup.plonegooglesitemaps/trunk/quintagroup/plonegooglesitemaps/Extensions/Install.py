cd6e2faa67e97ded8f5b6abed7d1ab8860ff02ea
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-05-25 14:34:32 +0000

    Added upgrade steps from 1.0 to 1.1 and from 1.1 to 1.2 versions

--- quintagroup.plonegooglesitemaps/trunk/quintagroup/plonegooglesitemaps/Extensions/Install.py
+++ quintagroup.plonegooglesitemaps/trunk/quintagroup/plonegooglesitemaps/Extensions/Install.py
@@ -1,4 +1,5 @@
 from Products.CMFCore.utils import getToolByName
+from Products.GenericSetup.upgrade import _upgrade_registry
 
 PROFILE = "profile-quintagroup.plonegooglesitemaps:default"
 
@@ -8,8 +9,25 @@ def install(self, reinstall=False):
     ps = getToolByName(self, 'portal_setup')
     mtool = getToolByName(self, 'portal_migration')
     plone_version = mtool.getFileSystemVersion()
+    isPlone3 = plone_version.startswith('3')
+    isPlone4 = plone_version.startswith('4')
 
-    if plone_version.startswith('3'):
+    if reinstall and (isPlone3 or isPlone4):
+        step = None
+        profile_id = 'quintagroup.plonegooglesitemaps:default'
+        steps_to_run = [s['id'] for s in setup_tool.listUpgrades(profile_id, show_old=False)]
+        for step_id in steps_to_run:
+            step = _upgrade_registry.getUpgradeStep(profile_id, step_id)
+            step.doStep(setup_tool)
+            msg = "Ran upgrade step %s for profile %s" % (step.title, profile_id)
+            logger.log(logging.INFO, msg)
+        # We update the profile version to the last one we have reached
+        # with running an upgrade step.
+        if step and step.dest is not None and step.checker is None:
+           setup_tool.setLastVersionForProfile(profile_id, step.dest)
+        return "Ran all reinstall steps."
+
+    if (isPlone3 or isPlone4):
         # if this is plone 3.x
         (ps.aq_base).__of__(self).runAllImportStepsFromProfile(PROFILE)
     else:

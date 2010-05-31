8d926d82dcc6e4625eb027851e4df0f7d1614bb1
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-05-31 17:38:58 +0000

    #131: Added tests for upgrade from 1.2 to 1.3 version

--- quintagroup.plonegooglesitemaps/trunk/quintagroup/plonegooglesitemaps/tests/testUpgrade.py
+++ quintagroup.plonegooglesitemaps/trunk/quintagroup/plonegooglesitemaps/tests/testUpgrade.py
@@ -3,12 +3,16 @@
 #
 
 from base import *
+from zope.component import getSiteManager
+
 from Products.CMFPlone.utils import _createObjectByType
 from Products.GenericSetup.upgrade import _upgrade_registry
+from archetypes.schemaextender.interfaces import ISchemaExtender
 from quintagroup.plonegooglesitemaps import config
 from quintagroup.plonegooglesitemaps import upgrades as gsm_upgrades
 from quintagroup.canonicalpath.interfaces import ICanonicalPath
 from quintagroup.canonicalpath.interfaces import ICanonicalLink
+from quintagroup.plonegooglesitemaps.content.newsextender import NewsExtender
 
 class TestUpgrade(TestCase):
 
@@ -25,15 +29,17 @@ class TestUpgrade(TestCase):
     def test_upgradeStepsRegistration(self):
         # Test upgrade steps
         upgrades = self.setup.listUpgrades(self.profile, show_old=True)
-        self.assertEqual(len(upgrades), 2)
+        self.assertEqual(len(upgrades), 3)
         self.assertEqual(upgrades[0]["title"].endswith("1.0 to 1.1"), True)
         self.assertEqual(upgrades[1]["title"].endswith("1.1 to 1.2"), True)
+        self.assertEqual(upgrades[2]["title"].endswith("1.2 to 1.3"), True)
 
     def test_upgradeSetupRegistration(self):
         # Test registered upgrade profiles
         pids = [i['id'] for i in self.setup.listProfileInfo()]
         self.assertEqual("quintagroup.plonegooglesitemaps:upgrade_1_0_to_1_1" in pids, True)
         self.assertEqual("quintagroup.plonegooglesitemaps:upgrade_1_1_to_1_2" in pids, True)
+        self.assertEqual("quintagroup.plonegooglesitemaps:upgrade_1_2_to_1_3" in pids, True)
 
     def test_step_1_0_to_1_1(self):
         # Prepare testing data
@@ -82,7 +88,30 @@ class TestUpgrade(TestCase):
         brain = catalog(id="test_folder")[0]
         self.assertEqual(brain.canonical_link, fldr_clink)
 
-    def testUpgradeCallOnReinstall(self):
+    def test_step_1_2_to_1_3(self):
+        # Prepare testing data
+        cols = ["Language", "gsm_access", "gsm_genres"]
+        lsm = getSiteManager(self.portal)
+        catalog = self.portal.portal_catalog
+        # Remove tested columns, if its exists
+        [catalog.delColumn(col) for col in cols if col in catalog._catalog.names]
+        # Remove schema extender adapter from local component registry, if its exists
+        for r in tuple(lsm.registeredAdapters()):
+            if r.factory == NewsExtender:
+                lsm.unregisterAdapter(factory=r.factory, name=r.name,
+                    required=r.required, provided=r.provided)
+        # Upgrade to 1.3 version
+        step = self.getUpgradeStep(3)
+        if step is not None:
+            step.doStep(self.setup)
+        # Test if columns added to portal_catalog
+        for col in cols:
+            self.assertEqual(col in catalog._catalog.names, True)
+        # Test if schema extender adapter added into local component registry
+        factories = [r.factory for r in tuple(lsm.registeredAdapters())]
+        self.assertEqual(NewsExtender in factories, True)
+
+    def testUpgradeCallOnQIReinstall(self):
         # Get upgrade steps
         upgrades = _upgrade_registry.getUpgradeStepsForProfile(self.profile)
         upgrades = dict([(u.sortkey, u) for u in upgrades.values()])
@@ -91,15 +120,18 @@ class TestUpgrade(TestCase):
             called = []
             upgrades[1].handler = lambda st:called.append("1.0 to 1.1")
             upgrades[2].handler = lambda st:called.append("1.1 to 1.2")
+            upgrades[3].handler = lambda st:called.append("1.2 to 1.3")
             # Run reinstallation
             self.portal.portal_quickinstaller.reinstallProducts(products=config.PROJECTNAME)
             # Test upgrades call
             self.assertEqual("1.0 to 1.1" in called, True)
             self.assertEqual("1.1 to 1.2" in called, True)
+            self.assertEqual("1.2 to 1.3" in called, True)
         finally:
             # Restore original upgrade handlers
             upgrades[1].handler = gsm_upgrades.upgrade_1_0_to_1_1
             upgrades[2].handler = gsm_upgrades.upgrade_1_1_to_1_2
+            upgrades[3].handler = gsm_upgrades.upgrade_1_2_to_1_3
 
         
 

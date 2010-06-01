1590c013b359d620c9ebfe5c7e736d8244fb56b4
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-06-01 19:20:45 +0000

    Remove before retagging bugfix version

--- quintagroup.seoptimizer/tags/4.0/quintagroup/seoptimizer/__init__.py
+++
@@ -1,6 +0,0 @@
-from AccessControl import allow_module
-from zope.i18nmessageid import MessageFactory
-
-SeoptimizerMessageFactory = MessageFactory('quintagroup.seoptimizer')
-
-allow_module('quintagroup.seoptimizer.util')

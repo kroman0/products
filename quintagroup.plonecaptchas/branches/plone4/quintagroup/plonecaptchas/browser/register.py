c13101cf292b068192c3496a1c2f8456964c6542
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-06-04 13:50:04 +0000

    #194: Extended CaptchaRegisterForm with CaptchaField

--- quintagroup.plonecaptchas/branches/plone4/quintagroup/plonecaptchas/browser/register.py
+++ quintagroup.plonecaptchas/branches/plone4/quintagroup/plonecaptchas/browser/register.py
@@ -1,8 +1,28 @@
-from plone.app.users.browser.register import RegistrationForm
+from zope.interface import Interface
 from plone.app.users.browser.register import AddUserForm
+from plone.app.users.browser.register import RegistrationForm
+
+from quintagroup.formlib.captcha import Captcha
+from quintagroup.formlib.captcha import CaptchaWidget
+
+from quintagroup.plonecaptchas import ProductMessageFactory as _
+
+#from quintagroup.formlib.captcha.field import Captcha
+class CaptchaSchema(Interface):
+    captcha = Captcha(
+        title=_(u'Type the code'),
+        description=_(u'Type the code from the picture shown below.'))
 
 class CaptchaRegistrationForm(RegistrationForm):
     """Registration form with captacha."""
 
-class CaptchaAddUserForm(RegistrationForm):
+    @property
+    def form_fields(self):
+        """Add captcha field to form_fields."""
+        ffields = super(CaptchaRegistrationForm, self).form_fields
+        
+        return ffields and ffields + form.Fields(CaptchaSchema) or ffields
+
+
+class CaptchaAddUserForm(AddUserForm):
     """Add user form with captacha."""

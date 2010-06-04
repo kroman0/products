c31ab0f215510cd660859fc5e00c19e784e745b6
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-06-04 19:34:17 +0000

    #194: Remove needless developer package data

--- quintagroup.plonecaptchas/branches/plone4/setup.py
+++ quintagroup.plonecaptchas/branches/plone4/setup.py
@@ -31,15 +31,7 @@ setup(name='quintagroup.plonecaptchas',
       ],
       entry_points="""
       # -*- Entry points: -*-
-
-      [distutils.setup_keywords]
-      paster_plugins = setuptools.dist:assert_string_list
-
-      [egg_info.writers]
-      paster_plugins.txt = setuptools.command.egg_info:write_arg
-
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      paster_plugins = ["ZopeSkel"],
       )

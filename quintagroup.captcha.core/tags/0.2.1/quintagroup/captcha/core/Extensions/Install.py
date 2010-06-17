11f551b21549eafa92191fa25b1ae240f2aa1e09
olha <olha@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-06-17 08:32:01 +0000

    [releaser] removing branch

--- quintagroup.captcha.core/tags/0.2.1/quintagroup/captcha/core/Extensions/Install.py
+++
@@ -1,7 +0,0 @@
-import transaction
-from Products.CMFCore.utils import getToolByName
-
-def uninstall(self):
-    portal_setup = getToolByName(self, 'portal_setup')
-    portal_setup.runAllImportStepsFromProfile('profile-quintagroup.captcha.core:uninstall', purge_old=False)
-    transaction.savepoint()

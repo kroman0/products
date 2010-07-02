d19f47e5b128b118c4b7749173513a6d0ff9a2b7
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-07-02 09:52:56 +0000

    Added SafeQuery class, which provide safeQuery method with automatic relogin in case of expired token

--- quintagroup.gauth/trunk/quintagroup/gauth/tests.py
+++ quintagroup.gauth/trunk/quintagroup/gauth/tests.py
@@ -27,6 +27,42 @@ class TestCase(ptc.PloneTestCase):
             pass
 
 
+# class SafeQuery(object):
+#     def safeCall(self, serv, meth, methargs=[], methkwargs={}):
+#         print "safecall %s: serv: %s, meth: %s, methargs: %s, methkwargs: %s" % (
+#             self, serv, meth, methargs, methkwargs)
+#         try:
+#             return meth(*methargs, **methkwargs)
+#         except gdata.service.RequestError, e:
+#             print "Token Expired -> Update it."
+#             logException("Token Expired -> Update it.")
+#             serv.ProgrammaticLogin()
+#             return meth(*methargs, **methkwargs)
+# counter = 0
+# class DummyServ:
+#     def ProgrammaticLogin(self, captcha_token=None, captcha_response=None):
+#         print "for '%s' called ProgrammaticLogin" % str(self)
+#     def Query(self, *args, **kwargs):
+#         global counter
+#         if counter > 3:
+#             counter = 0
+#             raise gdata.service.RequestError("Some Problem in Query")
+#         print "for '%s' called Query with args: %s, kwargs: %s" % tuple(
+#             map(str,[self,args,kwargs]))
+#         counter += 1
+# class C(SafeQuery):
+#     serv = None
+#     def __init__(self):
+#         self.serv = DummyServ()
+#     def operation(self, a):
+#         for i in range(5):
+#             self.safeCall(self.serv, self.serv.Query, [i,], {"q": i*i})
+
+
+
+
+
+
 def test_suite():
     return unittest.TestSuite([
 
--- quintagroup.gauth/trunk/quintagroup/gauth/utility.py
+++ quintagroup.gauth/trunk/quintagroup/gauth/utility.py
@@ -2,6 +2,7 @@
     Provide utility for getting GData portal account email and password.
 """
 
+import logging
 from zope.interface import implements
 from zope.component import queryMultiAdapter, queryAdapter
 from plone.memoize.view import memoize_contextless
@@ -9,6 +10,13 @@ from plone.memoize.view import memoize_contextless
 from quintagroiup.gauth.interfaces import IGAuthInterface
 from quintagroup.gdata.browser.configlet import IGDataConfigletSchema
 
+logger = logging.getLogger('quintagroup.gauth')
+def logException(msg, context=None):
+    logger.exception(msg)
+    if context is not None:
+        error_log = getattr(context, 'error_log', None)
+        if error_log is not None:
+            error_log.raising(sys.exc_info())
 
 class GAuthUtility(object):
     implements(IGAuthInterface)
@@ -33,8 +41,41 @@ class GAuthUtility(object):
         return self.gdataconf.gdata_pass
 
 
-    # @property
-    # @memoize_contextless
-    # def portal(self):
-    #     pps = queryMultiAdapter((self.context, self.context.REQUEST), name="plone_portal_state")
-    #     return pps.portal()
+class SafeQuery(object):
+    """ Base class for perform safe Google data service calls,
+        with automatic programming re-loginning.
+
+        So if class need automatic relogin to Google Date services, it must
+        inherit from the class and use safeQuery metho to call to services methods
+
+        For example ...
+
+        class MyGdataService(SafeQuery):
+
+            def __init__(self):
+                gauth_util = queryUtility(IGAuthInterface)
+                self.service = gdata.spreadsheet.service.SpreadsheetService(
+                                   gauth_util.email, gauth_util.password)
+                self.service.ProgrammaticLogin()
+
+            def fooservice(self):
+                ...
+                self.safeQuery(self.service, self.service.GetSpreadsheetsFeed,
+                               title="some title")
+                ...
+
+        So call to fooservice should automatic relogin to SpreadsheetService even
+        if token expired.
+    """
+
+    def safeQuery(self, serv, meth, *margs, **mkwargs):
+        try:
+            return meth(*margs, **mkwargs)
+        except gdata.service.RequestError, e:
+            logException("Token Expired -> Update it.")
+            if hasattr(serv, 'ProgrammaticLogin'):
+                serv.ProgrammaticLogin()
+                return meth(*margs, **mkwargs)
+            else:
+                raise
+

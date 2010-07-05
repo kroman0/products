d6ec745919189cff4fb2effe6df4d1f50b52edfd
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-07-05 17:33:43 +0000

    Added uninstallation

---
+++ quintagroup.gauth/trunk/quintagroup/gauth/Extensions/Install.py
@@ -0,0 +1,18 @@
+import logging
+from Products.CMFCore.utils import getToolByName
+from Products.GenericSetup.upgrade import _upgrade_registry
+
+logger = logging.getLogger('quintagroup.gauth')
+
+def uninstall(portal, reinstall=False):
+    """ Uninstall this product.
+
+        This external method is need, because portal_quickinstaller doens't know
+        what GenericProfile profile to apply when uninstalling a product.
+    """
+    setup_tool = getToolByName(portal, 'portal_setup')
+    if reinstall:
+        return "Ran all reinstall steps."
+    else:
+        setup_tool.runAllImportStepsFromProfile('profile-quintagroup.gauth:uninstall')
+        return "Ran all uninstall steps."

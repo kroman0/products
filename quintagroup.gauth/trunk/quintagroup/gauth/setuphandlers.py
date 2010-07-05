36e33f7d2d52049f8584abe6e054e5351db7d3b0
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-07-05 17:33:43 +0000

    Added uninstallation

--- quintagroup.gauth/trunk/quintagroup/gauth/setuphandlers.py
+++ quintagroup.gauth/trunk/quintagroup/gauth/setuphandlers.py
@@ -20,3 +20,45 @@ def registerGAuthUtility(context):
     sm.registerUtility(component=gauth,
                        provided=IGAuthUtility)
     logger.log(logging.INFO, "Registered IGAuthUtility, bound it to the site.")
+
+
+def uninstallStuff(context):
+    if context.readDataFile('quintagroup_gauth_unisntall.txt') is None:
+        return
+
+    site = context.getSite() 
+    unregisterUtility(site)
+    removeGauthProperties(site)
+    removeConfiglet(site)
+    removeActionIcons(site)
+
+def unregisterUtility(site):
+    sm = getSiteManager(site)
+    existing = queryUtility(IGAuthUtility)
+    if existing is None:
+        logger.log(logging.WARN, "No GAuthUtility is registered.")
+
+    sm.unregisterUtility(component=existing, provided=IGAuthUtility)
+    logger.log(logging.INFO, "Unregistered IGAuthUtility, from local sitemanager.")    
+
+def removeGauthProperties(site):
+    pp = getToolByName(site, "portal_properties")
+    if not "gauth_properties" in pp.objectIds():
+        logger.log(logging.WARN, "No 'gauth_properties' present in portal_properties.")
+    pp.manage_delObjects(ids="gauth_properties")
+    logger.log(logging.INFO, "Removed 'gauth_properties' from portal_properties.")
+
+def removeConfiglet(site):
+    pcp = getToolByName(site, "portal_controlpanel")
+    if not "quintagroup.gauth" in pcp.objectIds():
+        logger.log(logging.WARN, "No 'quintagroup.gauth' configlet.")
+    pcp.unregisterConfiglet("quintagroup.gauth")
+    logger.log(logging.INFO, "Removed 'quintagroup.gauth' configlet.")
+    
+def removeActionIcons(site):
+    pai = getToolByName(site, "portal_actionicons")
+    if not "quintagroup.gauth" in pai.objectIds():
+        logger.log(logging.WARN, "No 'quintagroup.gauth' configlet.")
+
+    pai.manage_removeActionIcon(category="controlpanel", action_id="quintagroup.gauth")
+    logger.log(logging.INFO, "Removed 'quintagroup.gauth' action icon.")

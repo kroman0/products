cdd5623cc96974a5045beb72c5a7d2bf167fbbf1
chervol <chervol@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-07-08 17:41:08 +0000

    Updated required packages

--- quintagroup.blog.star/trunk/setup.py
+++ quintagroup.blog.star/trunk/setup.py
@@ -24,6 +24,7 @@ setup(name='quintagroup.blog.star',
       zip_safe=False,
       install_requires=[
           'setuptools',
+          'collective.blog.star',
           # -*- Extra requirements: -*-
       ],
       entry_points="""

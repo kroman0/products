e8d48b51246ac8b93b4eed6e8b1e9ec800f6c2d1
liebster <liebster@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-07-09 10:05:56 +0000

    Fix path

--- quintagroup.gdocs.spreadsheet/trunk/quintagroup/gdocs/spreadsheet/interfaces/gspreadsheet.py
+++ quintagroup.gdocs.spreadsheet/trunk/quintagroup/gdocs/spreadsheet/interfaces/gspreadsheet.py
@@ -4,7 +4,7 @@ from zope.interface import Interface
 from zope.app.container.constraints import contains
 from zope.app.container.constraints import containers
 
-from quintagroup.spreadsheet import spreadsheetMessageFactory as _
+from quintagroup.gdocs.spreadsheet import spreadsheetMessageFactory as _
 
 class IGSpreadsheet(Interface):
     """Lets you select google spreadsheet and worksheet id"""

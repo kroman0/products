09a72c91d9e053159feaf86078d799cc9c7e4a20
liebster <liebster@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-07-13 18:49:11 +0000

    Added field for define title of column

--- quintagroup.gdocs.spreadsheet/trunk/quintagroup/gdocs/spreadsheet/browser/viewworksheetview.py
+++ quintagroup.gdocs.spreadsheet/trunk/quintagroup/gdocs/spreadsheet/browser/viewworksheetview.py
@@ -51,27 +51,29 @@ class ViewWorksheetView(BrowserView):
     def renderWorksheet(self, ssh_id="", wsh_id='', startrow_idx=0):
         """
         """
+        table = ''
         feed = self.getFeed(ssh_id, wsh_id, startrow_idx)
         if isinstance(feed, SpreadsheetsListFeed):
-            indx_keys = filter(None, [elem['select_title'] for elem in self.context.title_column])
-            titles_columns = filter(None, [self.context.order_columns[int(i)] for i in indx_keys])
-
-            # Prepare table
-            table = "<table id=\"sshwsh\">"
-            table += "<tr>"
-            table += ''.join(["<th>%s</th>" % key for key in titles_columns])
-            table += "</tr>"
-            for i, entry in enumerate(feed.entry):
-                if i+1 > startrow_idx:
-                    td_row = "<tr>"
-                    for key in titles_columns:
-                        td_row += "<td>%s</td>" \
-                            % (not (entry.custom[key].text == 'None') and entry.custom[key].text or '')
-                    td_row += "</tr>\n"
-                    table += td_row
-            table += "</table>"
-        else:
-            table = ''
+            # akc is a list of keys of all columns. The context is GSpreadsheet content type
+            akc = self.context.all_keys_columns
+            # key_title_columns is list of tuples with two elements (column_key, column_title or column_key)
+            key_title_columns = [(e['column_key'], e['column_title'] or e['column_key'])
+                for e in self.context.order_columns if e['column_key'] in akc]
+            if key_title_columns:
+                # Prepare table
+                table = "<table id=\"sshwsh\">"
+                table += "<tr>"
+                table += ''.join(["<th>%s</th>" % el[1] for el in key_title_columns])
+                table += "</tr>"
+                for i, entry in enumerate(feed.entry):
+                    if i >= startrow_idx:
+                        td_row = "<tr>"
+                        for key in zip(*key_title_columns)[0]:
+                            td_row += "<td>%s</td>" \
+                                % (not (entry.custom[key].text == 'None') and entry.custom[key].text or '')
+                        td_row += "</tr>\n"
+                        table += td_row
+                table += "</table>"
         return table
 
     def getFeed(self, ssh_id="", wsh_id='', startrow_idx=0):
@@ -83,7 +85,7 @@ class ViewWorksheetView(BrowserView):
         self.sh_client.ProgrammaticLogin()
         try:
             feed = self.sh_client.GetListFeed(ssh_id, wksht_id=wsh_id, query=self.query)
-            self.context.order_columns = len(feed.entry) and feed.entry[0].custom.keys() or []
+            self.context.all_keys_columns = len(feed.entry) and feed.entry[0].custom.keys() or []
         except Exception:
             logException('GetListFeed function call: '
                          'key=%s, wksht_id=%s' % (ssh_id, wsh_id), self.context)

6e7bd03e0da88e08747f1542e032f2d9e5fda62d
liebster <liebster@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-07-13 11:12:40 +0000

    Added 'view' for GSpreadsheet content type

--- quintagroup.gdocs.spreadsheet/trunk/quintagroup/gdocs/spreadsheet/browser/viewworksheetview.py
+++ quintagroup.gdocs.spreadsheet/trunk/quintagroup/gdocs/spreadsheet/browser/viewworksheetview.py
@@ -1,29 +1,43 @@
+from zope.component import queryUtility
 from zope.interface import implements, Interface
 
 from Products.Five import BrowserView
 from Products.CMFCore.utils import getToolByName
 
+from plone.memoize.view import memoize_contextless
+
+from quintagroup.gauth.interfaces import IGAuthUtility
+from gdata.spreadsheet.service import SpreadsheetsService
+from gdata.spreadsheet.service import DocumentQuery as shDocumentQuery
+
 from quintagroup.gdocs.spreadsheet import spreadsheetMessageFactory as _
+from quintagroup.gdocs.spreadsheet import logException, logger
+from quintagroup.gdocs.spreadsheet.config import ORDER_COLUMNS
 
 
-class IviewWorksheetView(Interface):
+class IViewWorksheetView(Interface):
     """
-    viewWorksheet view interface
+    ViewWorksheet view interface
     """
 
-    def test():
-        """ test method"""
+    def renderWorksheet(ssh_id, wsh_idx, startrow_idx):
+        """
+            ssh_id - Id of the spreadsheet
+            wsh_id - Id of the worksheet
+            startrow_idx - index of row, from which table should be rendered
+        """
 
 
-class viewWorksheetView(BrowserView):
+class ViewWorksheetView(BrowserView):
     """
-    viewWorksheet browser view
+    ViewWorksheet browser view
     """
-    implements(IviewWorksheetView)
+    implements(IViewWorksheetView)
 
     def __init__(self, context, request):
         self.context = context
         self.request = request
+        self.query = shDocumentQuery()
 
     @property
     def portal_catalog(self):
@@ -33,10 +47,39 @@ class viewWorksheetView(BrowserView):
     def portal(self):
         return getToolByName(self.context, 'portal_url').getPortalObject()
 
-    def test(self):
+    @memoize_contextless
+    def renderWorksheet(self, ssh_id="",
+                        wsh_id='', startrow_idx=0):
         """
-        test method
         """
-        dummy = _(u'a dummy string')
 
-        return {'dummy': dummy}
+        # Authorization on spreadsheets.google.com
+        gauth = queryUtility(IGAuthUtility)
+        self.sh_client = SpreadsheetsService(email=gauth.email, password=gauth.password)
+        sh_client = self.sh_client
+        sh_client.ProgrammaticLogin()
+
+        try:
+            feed = sh_client.GetListFeed(ssh_id, wksht_id=wsh_id, query=self.query)
+        except Exception:
+            logException('GetListFeed function call: '
+                         'key=%s, wksht_id=%s' % (ssh_id, wsh_id))
+            return ""
+
+        # Prepare table
+        table = "<table id=\"sshwsh\">"
+        table += "<tr>"
+        table += ''.join(["<th>%s</th>" % key for key in ORDER_COLUMNS])
+        table += "</tr>"
+
+        for i, entry in enumerate(feed.entry):
+            if i+1 > startrow_idx:
+                td_row = "<tr>"
+                for key in ORDER_COLUMNS:
+                    td_row += "<td>%s</td>" % 
+                        (not (entry.custom[key].text == 'None') and entry.custom[key].text or '')
+                td_row += "</tr>\n"
+                table += td_row
+
+        table += "</table>"
+        return table

95fb2120468d6302473bb9f8c4013501f3833ffa
liebster <liebster@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-07-13 18:49:11 +0000

    Added field for define title of column

--- quintagroup.gdocs.spreadsheet/trunk/quintagroup/gdocs/spreadsheet/content/gspreadsheet.py
+++ quintagroup.gdocs.spreadsheet/trunk/quintagroup/gdocs/spreadsheet/content/gspreadsheet.py
@@ -9,6 +9,7 @@ from Products.ATContentTypes.content import schemata
 
 from Products.DataGridField import DataGridField, DataGridWidget
 from Products.DataGridField.SelectColumn import SelectColumn
+from Products.DataGridField.Column import Column
 
 from quintagroup.gdocs.spreadsheet import spreadsheetMessageFactory as _
 from quintagroup.gdocs.spreadsheet.interfaces import IGSpreadsheet
@@ -51,20 +52,21 @@ GSpreadsheetSchema = schemata.ATContentTypeSchema.copy() + atapi.Schema((
     ),
 
     DataGridField(
-        name='title_column',
+        name='order_columns',
         searchable = True,
         languageIndependent=True,
         storage=atapi.AnnotationStorage(),
-        columns=("select_title",),
+        columns=("column_key", "column_title"),
         widget = DataGridWidget(
             label = _(
-                u'label_title_column',
-                default=u'Titles of columns'),
+                u'label_order_column',
+                default=u'Ordering columns'),
             description=_(
-                u'help_title_column',
-                default=u"Choose titles of columns"),
+                u'help_order_column',
+                default=u"Choose keys of columns and enter them titles"),
             columns={
-                'select_title' : SelectColumn("Titles of columns", vocabulary="getTitleColumnVocabulary"),
+                'column_key' : SelectColumn("Key of column", vocabulary="getKeyColumnVocabulary"),
+                'column_title' : Column("Title of column"),
             },
         ),
     ),
@@ -82,25 +84,25 @@ GSpreadsheetSchema['description'].storage = atapi.AnnotationStorage()
 schemata.finalizeATCTSchema(GSpreadsheetSchema, moveDiscussion=False)
 
 class GSpreadsheet(base.ATCTContent):
-    """ Lets you select google spreadsheet worksheet id and choose title of columns """
+    """ Lets you select google spreadsheet id, worksheet id, choose keys of columns and define them title """
     implements(IGSpreadsheet)
 
     meta_type = "GSpreadsheet"
     schema = GSpreadsheetSchema
 
-    order_columns = []
+    all_keys_columns = []
     title = atapi.ATFieldProperty('title')
     description = atapi.ATFieldProperty('description')
     spreadsheet_id = atapi.ATFieldProperty('spreadsheet_id')
     worksheet_id = atapi.ATFieldProperty('worksheet_id')
-    title_column = atapi.ATFieldProperty('title_column')
+    order_columns = atapi.ATFieldProperty('order_columns')
 
     # -*- Your ATSchema to Python Property Bridges Here ... -*-
 
-    def getTitleColumnVocabulary(self):
-        """ Get a list of titles of columns """
+    def getKeyColumnVocabulary(self):
+        """ Get a list of keys of columns """
         return atapi.DisplayList(
-            ([('%s'%i, t) for i,t in enumerate(self.order_columns)])
+            ([(t, t) for t in self.all_keys_columns])
         )
 
 atapi.registerType(GSpreadsheet, PROJECTNAME)

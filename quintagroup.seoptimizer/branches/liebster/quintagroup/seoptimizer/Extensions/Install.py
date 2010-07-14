344b3448833e97e5e8bbcaff7b0ceb347240fb57
liebster <liebster@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-07-14 17:24:33 +0000

    Removed 'liebster' branches

--- quintagroup.seoptimizer/branches/liebster/quintagroup/seoptimizer/Extensions/Install.py
+++
@@ -1,9 +0,0 @@
-from Products.CMFCore.utils import getToolByName
-from quintagroup.seoptimizer.config import PROJECT_NAME
-
-
-def uninstall( portal ):
-    setup_tool = getToolByName(portal, 'portal_setup')
-    setup_tool.setBaselineContext('profile-%s:uninstall'%PROJECT_NAME)
-    setup_tool.runAllImportStepsFromProfile('profile-%s:uninstall'%PROJECT_NAME)
-    return "Ran all uninstall steps."
--- quintagroup.seoptimizer/branches/liebster/quintagroup/seoptimizer/interfaces.py
+++
@@ -1,8 +0,0 @@
-from zope.interface import Interface
-
-class IKeywords(Interface):
-    """Handle the available keywords.
-    """
-    def listKeywords():
-        """Returns all the existing keywords for the current content type.
-        """

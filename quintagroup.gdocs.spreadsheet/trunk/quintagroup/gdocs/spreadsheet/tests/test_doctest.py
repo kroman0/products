8d22866738e4ac951b657544654db13c3e4464ed
liebster <liebster@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-08-17 14:33:04 +0000

    Add doctests of context and adapter

--- quintagroup.gdocs.spreadsheet/trunk/quintagroup/gdocs/spreadsheet/tests/test_doctest.py
+++ quintagroup.gdocs.spreadsheet/trunk/quintagroup/gdocs/spreadsheet/tests/test_doctest.py
@@ -5,6 +5,7 @@ import zope.component
 from Testing import ZopeTestCase as ztc
 
 from quintagroup.gauth.interfaces import IGAuthUtility
+import quintagroup.gdocs.spreadsheet.content.gspreadsheet as gspreadsheet
 from quintagroup.gdocs.spreadsheet.tests import base
 
 
@@ -13,6 +14,11 @@ def setUp(test):
     """substitution using the adapter services gdata"""
     import quintagroup.gdocs.spreadsheet.tests.adapters
 
+
+def tearDown(test):
+    """This is the companion to setUp - it can be used to clean up the
+    test environment after each test.
+    """
 
 def test_suite():
     return unittest.TestSuite([
@@ -24,7 +30,25 @@ def test_suite():
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE |
                 doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS,
             setUp=setUp,
+            tearDown=tearDown,
             ),
+        ztc.ZopeDocFileSuite('tests/test_adapters.txt',
+            package='quintagroup.gdocs.spreadsheet',
+            test_class=base.FunctionalTestCase,
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE |
+                doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS,
+            setUp=setUp,
+            tearDown=tearDown,
+            ),
+        ztc.ZopeDocFileSuite('tests/test_context.txt',
+            package='quintagroup.gdocs.spreadsheet',
+            test_class=base.FunctionalTestCase,
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE |
+                doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS,
+            setUp=setUp,
+            tearDown=tearDown,
+            ),
+
         ])
 
 if __name__ == '__main__':

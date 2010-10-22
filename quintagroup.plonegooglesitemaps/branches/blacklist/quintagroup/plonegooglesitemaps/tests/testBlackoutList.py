ff5d8e35e84ad5bc47e27a18b7282857960d6a20
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-10-22 11:38:20 +0000

    #228: Added TestFormDataProcessing tests

--- quintagroup.plonegooglesitemaps/branches/blacklist/quintagroup/plonegooglesitemaps/tests/testBlackoutList.py
+++ quintagroup.plonegooglesitemaps/branches/blacklist/quintagroup/plonegooglesitemaps/tests/testBlackoutList.py
@@ -3,7 +3,7 @@
 #
 from base import *
 from types import ListType, TupleType
-from zope.component import queryUtility
+from zope.component import queryUtility, queryMultiAdapter
 
 from Products.CMFPlone.utils import _createObjectByType
 from quintagroup.plonegooglesitemaps.config import BLACKOUT_PREFIX
@@ -23,23 +23,27 @@ class TestBOFilterUtilities(TestCase):
             "Not registered default '%s' IBlackoutFilterUtility" % pathfname)
 
 
+def createTestContent(self):
+    # Add testing content to portal
+    for cont in [self.portal, self.folder]:
+        for i in range(1,4):
+            doc = _createObjectByType('Document', cont, id='doc%i' % i)
+            doc.edit(text_format='plain', text='hello world %i' % i)
+            self.workflow.doActionFor(doc, 'publish')
+
 class TestDefaultFilters(TestCase):
 
     def afterSetUp(self):
-        self.loginAsPortalOwner()
-        # Add testing content to portal
-        for cont in [self.portal, self.folder]:
-            for i in range(1,4):
-                doc = _createObjectByType('Document', cont, id='my_doc%i' % i)
-                doc.edit(text_format='plain', text='hello world %i' % i)
+        super(TestDefaultFilters, self).afterSetUp()
+        createTestContent(self)
         self.catres = self.portal.portal_catalog(portal_type=["Document",])
         self.logout()
 
     def testIdFilter(self):
         idfilter = queryUtility(IBlackoutFilterUtility, name=idfname)
         catpaths = [c.getPath() for c in self.catres]
-        filtered = [f.getPath() for f in idfilter.filterOut(self.catres, fkey="my_doc1")]
-        excluded = [c.getPath() for c in self.catres if c.id == "my_doc1"]
+        filtered = [f.getPath() for f in idfilter.filterOut(self.catres, fkey="doc1")]
+        excluded = [c.getPath() for c in self.catres if c.id == "doc1"]
         map(lambda l:l.sort(), [catpaths, filtered, excluded])
         self.assertTrue(type(filtered) in [ListType, TupleType],
             'Object type, returned by filteredOut method of "%s" utility '\
@@ -51,8 +55,8 @@ class TestDefaultFilters(TestCase):
     def testPathFilter(self):
         pathfilter = queryUtility(IBlackoutFilterUtility, name=pathfname)
         catpaths = [c.getPath() for c in self.catres]
-        filtered = [f.getPath() for f in pathfilter.filterOut(self.catres, fkey="/plone/my_doc1")]
-        excluded = [c.getPath() for c in self.catres if c.getPath() == "/plone/my_doc1"]
+        filtered = [f.getPath() for f in pathfilter.filterOut(self.catres, fkey="/plone/doc1")]
+        excluded = [c.getPath() for c in self.catres if c.getPath() == "/plone/doc1"]
         map(lambda l:l.sort(), [catpaths, filtered, excluded])
         self.assertTrue(type(filtered) in [ListType, TupleType],
             'Object type, returned by filteredOut method of "%s" utility '\
@@ -60,14 +64,53 @@ class TestDefaultFilters(TestCase):
         self.assertTrue(set(catpaths)-set(filtered) == set(excluded),
             'Wrong filtered-out by "%s" filter:\nsrc %s\nres %s\nexcluded %s' % (
              pathfname, catpaths, filtered, excluded))
-        
-        
+
+
+class TestFormDataProcessing(TestCase):
+
+    def afterSetUp(self):
+        super(TestFormDataProcessing, self).afterSetUp()
+        createTestContent(self)
+        # Add sitemap object
+        self.sm = _createObjectByType('Sitemap', self.portal, id='google-sitemaps')
+        self.smview = queryMultiAdapter((self.sm, self.app.REQUEST), name="sitemap.xml")
+        self.catres = self.portal.portal_catalog(portal_type=["Document",])
+
+    def testGetNamedFilterUtility(self):
+        self.sm.edit(blackout_list="path:/plone/doc1")
+        filtered = [f['url'] for f in self.smview.results()]
+        catpaths = [c.getURL() for c in self.catres]
+        excluded = [c.getURL() for c in self.catres \
+                    if c.getPath() == "/plone/doc1"]
+        map(lambda l:l.sort(), [catpaths, filtered, excluded])
+        self.assertTrue(set(catpaths)-set(filtered) == set(excluded),
+            'Wrong filtered-out by "%s" filter:\nsrc %s\nres %s\nexcluded %s' % (
+             idfname, catpaths, filtered, excluded))
+
+    def testDefaultFilterUtility(self):
+        self.sm.edit(blackout_list="id:doc1")
+        filtered = [f['url'] for f in self.smview.results()]
+        catpaths = [c.getURL() for c in self.catres]
+        excluded = [c.getURL() for c in self.catres if c.id == "doc1"]
+        map(lambda l:l.sort(), [catpaths, filtered, excluded])
+        self.assertTrue(set(catpaths)-set(filtered) == set(excluded),
+            'Wrong filtered-out by "%s" filter:\nsrc %s\nres %s\nexcluded %s' % (
+             idfname, catpaths, filtered, excluded))
+        # Now check is output of unnamed filter same to id-named one.
+        self.sm.edit(blackout_list="id:doc1")
+        filtered_def = [f['url'] for f in self.smview.results()]
+        filtered_def.sort()
+        self.assertTrue(filtered == filtered_def,
+            'Output of named "id" filter is not same to unnamed one:' \
+            'id-named: %s\nunnamed: %s' % (filtered, filtered_def))
+
 
 def test_suite():
     from unittest import TestSuite, makeSuite
     suite = TestSuite()
     suite.addTest(makeSuite(TestBOFilterUtilities))
     suite.addTest(makeSuite(TestDefaultFilters))
+    suite.addTest(makeSuite(TestFormDataProcessing))
     return suite
 
 if __name__ == '__main__':

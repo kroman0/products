7609d1d05075407c920bdf9cc30c77fd7f31759b
kroman0 <kroman0@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-10-22 09:52:00 +0000

    Fixed js

--- quintagroup.portlet.pfg/trunk/quintagroup/portlet/pfg/hidetextlabels.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    $('.portletPFG input[type=text]')
-        .attr('onblur',"if(this.value == '')this.value=this.defaultValue;")
-        .attr('onclick',"this.value = '';")
-});

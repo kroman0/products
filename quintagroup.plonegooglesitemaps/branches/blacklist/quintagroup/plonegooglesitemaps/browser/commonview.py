94016473c179f07a173cae3f9b9f470281d1905f
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-10-22 11:38:57 +0000

    #228: Added getBOFiltered method to sitemap common view

--- quintagroup.plonegooglesitemaps/branches/blacklist/quintagroup/plonegooglesitemaps/browser/commonview.py
+++ quintagroup.plonegooglesitemaps/branches/blacklist/quintagroup/plonegooglesitemaps/browser/commonview.py
@@ -1,11 +1,14 @@
 from string import find
 from zope.interface import implements, Interface, Attribute
+from zope.component import queryUtility
 
 from Acquisition import aq_inner, aq_parent
 from Products.Five import BrowserView
 from Products.CMFCore.utils import getToolByName
 
 from quintagroup.plonegooglesitemaps import qPloneGoogleSitemapsMessageFactory as _
+from quintagroup.plonegooglesitemaps.config import BLACKOUT_PREFIX
+from quintagroup.plonegooglesitemaps.interfaces import IBlackoutFilterUtility
 from quintagroup.plonegooglesitemaps.browser.utils import additionalURLs, applyOperations
 
 
@@ -67,13 +70,9 @@ class CommonSitemapView(BrowserView):
         """
         result = []
         objects = self.getFilteredObjects()
-        blackout_list = self.context.getBlackout_list()
         reg_exps = self.context.getReg_exp()
 
-        brain_url_map = applyOperations([ob for ob in objects 
-            if (ob.getId not in blackout_list)],
-            reg_exps)
-
+        brain_url_map = applyOperations(self.getBOFiltered(objects), reg_exps)
         # Prepare dictionary for view
         for url, b in brain_url_map.items():
             res_map = {'url' : url,}
@@ -81,6 +80,46 @@ class CommonSitemapView(BrowserView):
             result.append(res_map)
         self.num_entries = len(result)
         return result
+
+    def getBOFiltered(self, objects):
+        """Return blkack-out filtered objects
+          Every record in blackout_list filter should follow the spec:
+             [extra filter data, separated by ':':][<filter name>:]<filter data>
+          For example:
+          1|  index.html
+          2|  id:index.html
+          3|  path:/folder_1_level/obj_in_folder
+          4|  path:./folder_near_sitemap/obj_in_folder
+          5|  extra_arg1:extra_arg2:super_filter:super arg-1, super arg-2
+         
+          1->used default "id" filter - remove "index.html" objects;
+          2->explicit "id" filter - remove "index.html" objects;
+          3->"path" filter - remove /folder_1_level/obj_in_folder object,
+              path from the root of the plone site;
+          4->same to 3), but path get from the folder, where sitemap is located;
+          5->filter name is "super_filter" (must be registered IBlackoutFilterUtility,
+             named "seoptimzier.blackoutfilter.super_filter:), which get list of extra
+             parameters, e.g. [extra_arg1,extra_arg2] to the filterOut method in
+             "extras" option, and get filter arguments: super arg-1, super arg-2
+          
+          class SuperFilterUtility(object):
+              def filterOut(self, fdata, fargs, **kwargs):
+                  sitemap = kwargs.get("sitemap")
+                  extras = kwargs.get("extras") # [extra_arg1,extra_arg2]
+                  # some logic to filter-out fdata by fargs with taking into
+                  # consideration extras ...
+        """
+        blackout_list = filter(None, self.context.getBlackout_list())
+        for frec in blackout_list:
+            fspec = frec.split(":")
+            fargs = fspec.pop()
+            fname = BLACKOUT_PREFIX + (fspec and fspec.pop() or "id")
+            futility = queryUtility(IBlackoutFilterUtility, name=fname)
+            if futility:
+                kw = {"sitemap": self.context,
+                      "extras": fspec}
+                objects = futility.filterOut(objects, fargs, **kw)
+        return objects
 
     def updateRequest(self):
         self.request.RESPONSE.setHeader('Content-Type', 'text/xml')

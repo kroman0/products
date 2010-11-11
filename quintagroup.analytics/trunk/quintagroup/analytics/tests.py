5db9c67538fb48c3b4370fe6ba023268f527e343
fenix <fenix@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-11-11 19:24:30 +0000

    added test to verify if configlet is installed properly

--- quintagroup.analytics/trunk/quintagroup/analytics/tests.py
+++ quintagroup.analytics/trunk/quintagroup/analytics/tests.py
@@ -1,16 +1,16 @@
 import unittest
+import transaction
 
-from zope.testing import doctestunit
 from zope.component import testing
 from Testing import ZopeTestCase as ztc
 
 from Products.Five import zcml
 from Products.Five import fiveconfigure
 from Products.PloneTestCase import PloneTestCase as ptc
+from Products.PloneTestCase import setup as ptc_setup
 from Products.PloneTestCase.layer import PloneSite
+import quintagroup.analytics
 ptc.setupPloneSite()
-
-import quintagroup.contentstats
 
 class TestCase(ptc.PloneTestCase):
     class layer(PloneSite):
@@ -18,16 +18,48 @@ class TestCase(ptc.PloneTestCase):
         def setUp(cls):
             fiveconfigure.debug_mode = True
             zcml.load_config('configure.zcml',
-                             quintagroup.contentstats)
+                             quintagroup.analytics)
             fiveconfigure.debug_mode = False
+            ztc.installPackage('quintagroup.analytics')
+            app = ztc.app()
+            portal = app[ptc_setup.portal_name]
+
+            # Sets the local site/manager
+            ptc_setup._placefulSetUp(portal)
+
+            qi = getattr(portal, 'portal_quickinstaller', None)
+            qi.installProduct('quintagroup.analytics')
+            transaction.commit()
 
         @classmethod
         def tearDown(cls):
             pass
 
 
+#TO DO:=====================================================================
+#      add test to verify if QA control panel action is present;
+#      add tests for views registration;
+#      add tests for every views methods;
+#      add doc tests to validate if all needed elements are present on page;
+
+
+
+class TestQAInstallation(TestCase):
+    """ This class veryfies registrations of all needed views and
+        actions.
+    """
+
+    def test_cp_action_installation(self):
+        """Here we are testin control panel action. """
+        control_panel = self.portal.portal_controlpanel
+        self.assert_('QAnalytics' in [a.id for a in control_panel.listActions()],
+                     "Configlet for quintagroup.analitycs isn't registered.")
+
+
 def test_suite():
-    return unittest.TestSuite([
+    from unittest import TestSuite, makeSuite
+
+    test_suite = unittest.TestSuite([
 
         # Unit tests
         #doctestunit.DocFileSuite(
@@ -49,6 +81,9 @@ def test_suite():
         #    test_class=TestCase),
 
         ])
+
+    test_suite.addTest(makeSuite(TestQAInstallation))
+    return test_suite
 
 if __name__ == '__main__':
     unittest.main(defaultTest='test_suite')

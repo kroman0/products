4efb73a3720dcadf6ddfc28f06263ab95e2fd4e2
fenix <fenix@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2010-11-12 16:42:49 +0000

    added test for getUsers method of 'ownership_by_type' view.

--- quintagroup.analytics/trunk/quintagroup/analytics/tests.py
+++ quintagroup.analytics/trunk/quintagroup/analytics/tests.py
@@ -1,6 +1,7 @@
 import unittest
 import transaction
 
+from AccessControl.SecurityManagement import newSecurityManager
 from zope.component import testing, queryMultiAdapter
 from Testing import ZopeTestCase as ztc
 
@@ -12,35 +13,81 @@ from Products.PloneTestCase.layer import PloneSite
 import quintagroup.analytics
 ptc.setupPloneSite()
 
-class TestCase(ptc.PloneTestCase):
-    class layer(PloneSite):
-        @classmethod
-        def setUp(cls):
-            fiveconfigure.debug_mode = True
-            zcml.load_config('configure.zcml',
-                             quintagroup.analytics)
-            fiveconfigure.debug_mode = False
-            ztc.installPackage('quintagroup.analytics')
-            app = ztc.app()
-            portal = app[ptc_setup.portal_name]
+class Installed(PloneSite):
 
-            # Sets the local site/manager
-            ptc_setup._placefulSetUp(portal)
+    @classmethod
+    def setUp(cls):
+        fiveconfigure.debug_mode = True
+        zcml.load_config('configure.zcml',
+                         quintagroup.analytics)
+        fiveconfigure.debug_mode = False
+        ztc.installPackage('quintagroup.analytics')
+        app = ztc.app()
+        portal = app[ptc_setup.portal_name]
 
-            qi = getattr(portal, 'portal_quickinstaller', None)
-            qi.installProduct('quintagroup.analytics')
-            transaction.commit()
+        # Sets the local site/manager
+        ptc_setup._placefulSetUp(portal)
 
-        @classmethod
-        def tearDown(cls):
+        qi = getattr(portal, 'portal_quickinstaller', None)
+        qi.installProduct('quintagroup.analytics')
+        transaction.commit()
+
+    @classmethod
+    def tearDown(cls):
             pass
 
+class SetUpContent(Installed):
+
+    max = 10
+    types_ = ['Document', 'Event', 'Folder']
+    users = [('user%s'%i, 'user%s'%i, 'Member', None)
+             for i in xrange(max)]
+
+    @classmethod
+    def setupUsers(cls, portal):
+        """ Creates users."""
+        acl_users = portal.acl_users
+        mp = portal.portal_membership
+        map(acl_users._doAddUser, *zip(*cls.users))
+        if not mp.memberareaCreationFlag:
+            mp.setMemberareaCreationFlag()
+        map(mp.createMemberArea, [u[0] for u in cls.users])
+
+    @classmethod
+    def setupContent(cls, portal):
+        """ Creates users"""
+        uf = portal.acl_users
+        pm = portal.portal_membership
+        pc = portal.portal_catalog
+        users = [u[0] for u in cls.users]
+        for u in users:
+            folder = pm.getHomeFolder(u)
+            user = uf.getUserById(u)
+            if not hasattr(user, 'aq_base'):
+                user = user.__of__(uf)
+            newSecurityManager(None, user)
+            for i in xrange(users.index(u)+cls.max):
+                map(folder.invokeFactory, cls.types_, [t+str(i) for t in cls.types_])
+        transaction.commit()
+
+
+    @classmethod
+    def setUp(cls):
+        app = ztc.app()
+        portal = app[ptc_setup.portal_name]
+        cls.setupUsers(portal)
+        cls.setupContent(portal)
+
+    @classmethod
+    def tearDown(cls):
+        pass
+
+class TestCase(ptc.PloneTestCase):
+    layer = Installed
 
 #TO DO:=====================================================================
 #      add tests for every views methods;
 #      add doc tests to validate if all needed elements are present on page;
-
-
 
 class TestQAInstallation(TestCase):
     """ This class veryfies registrations of all needed views and
@@ -108,6 +155,22 @@ class TestQAInstallation(TestCase):
 
         self.assert_(view, "Portlets Stats view isn't registered")
 
+class TestOwnershipByType(TestCase):
+    """Tests all ownership by type view methods."""
+
+    layer = SetUpContent
+
+    def test_getUsers(self):
+        """
+        """
+        view = queryMultiAdapter((self.portal, self.portal.REQUEST),
+                                 name="ownership_by_type")
+
+        self.assert_(False in map(lambda u1, u2:u1==u2,
+                     [u[0] for u in self.layer.users], view.getUsers()))
+
+
+
 def test_suite():
     from unittest import TestSuite, makeSuite
 
@@ -135,6 +198,7 @@ def test_suite():
         ])
 
     test_suite.addTest(makeSuite(TestQAInstallation))
+    test_suite.addTest(makeSuite(TestOwnershipByType))
     return test_suite
 
 if __name__ == '__main__':

2d8b95cebb99784bb97a36e09c485cf734a0f9a0
vmaksymiv <vmaksymiv@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2011-04-15 14:30:28 +0000

    pylint fixes

--- quintagroup.formlib.captcha/trunk/quintagroup/formlib/captcha/example.py
+++ quintagroup.formlib.captcha/trunk/quintagroup/formlib/captcha/example.py
@@ -1,8 +1,6 @@
 from zope.schema import TextLine
-from zope.component import adapts
 from zope.interface import Interface, implements
 from zope.formlib.form import FormFields
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.form.base import EditForm
 
 from quintagroup.formlib.captcha import Captcha

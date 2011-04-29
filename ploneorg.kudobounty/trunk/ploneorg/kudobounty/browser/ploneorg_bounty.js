0844da10a2f3b8c3380c4f465e288376af0fbed3
mylan <mylan@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2011-04-29 13:07:10 +0000

    Little shuffle algoryth (in javascript) update

--- ploneorg.kudobounty/trunk/ploneorg/kudobounty/browser/ploneorg_bounty.js
+++ ploneorg.kudobounty/trunk/ploneorg/kudobounty/browser/ploneorg_bounty.js
@@ -1,23 +1,22 @@
 // Hide #portal-bounty-program container BEFORE it will be loaded
-// This willbe done only for browser with javascript support.
+// This will be done only for browser with javascript support.
 $("<style type='text/css'>#portal-bounty-program .hidden{display:none;}</style>").appendTo("head");
 
 function getRandomSubset(array, choice_num) {
-    // Return randomly selected *choice_num* elements from the array
-    var tmp, current, top = array.length;
-    if (array.length > choice_num) {
-        // Solution is based on Fisher-Yates (Knuth) algorithm
-        // (http://en.wikipedia.org/wiki/Fisher–Yates_shuffle).
-	for(top=array.length-1; top>array.length-choice_num; --top) {
-	    current = Math.floor(Math.random() * top);
-	    tmp = array[current];
-	    array[current] = array[top];
-	    array[top] = tmp;
-	}
-	return array.slice(array.length-choice_num,array.length)
-    } else {
+    // Return randomly selected *choice_num* elements from the array.
+    // Solution is based on the Fisher-Yates (Knuth) algorithm
+    // (http://en.wikipedia.org/wiki/Fisher–Yates_shuffle).
+    if (array.length < choice_num)
         return array;
+
+    var tmp, current, top = array.length;
+    for(top=array.length-1; top>array.length-choice_num; --top) {
+	current = Math.floor(Math.random() * top);
+	tmp = array[current];
+	array[current] = array[top];
+	array[top] = tmp;
     }
+    return array.slice(array.length-choice_num,array.length)
 };
 
 $(document).ready(function() {

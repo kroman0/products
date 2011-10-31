19ad31220ce9e76cbd76e29800a230df916eba43
olha <olha@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2011-10-31 12:14:12 +0000

    minor docs update

--- quintagroup.portlet.cumulus/trunk/setup.py
+++ quintagroup.portlet.cumulus/trunk/setup.py
@@ -7,7 +7,8 @@ setup(name='quintagroup.portlet.cumulus',
       version=version,
       description="A tag cloud portlet that rotates tags in 3D using a Flash movie",
       long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+          open(os.path.join("docs", "INSTALL.txt")).read() + "\n" +
+          open(os.path.join("docs", "HISTORY.txt")).read(),                                            
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
@@ -15,8 +16,8 @@ setup(name='quintagroup.portlet.cumulus',
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='plone portlet flash tag cloud',
-      author='Bohdan Koval',
-      author_email='koval@quintagroup.com',
+      author='Quintagroup',
+      author_email='support@quintagroup.com',
       url='http://svn.quintagroup.com/products/quintagroup.portlet.cumulus',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),

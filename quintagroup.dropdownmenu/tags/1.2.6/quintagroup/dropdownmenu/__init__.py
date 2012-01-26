0b7b1ed970b19fa29e1419f9f1d69085f0dd771c
Volodymyr Cherepanyak <chervol@users.noreply.github.com>
2012-01-26 14:57:24 +0000

    failed release removed

--- quintagroup.dropdownmenu/tags/1.2.6/quintagroup/dropdownmenu/__init__.py
+++
@@ -1,7 +0,0 @@
-import logging
-from zope.i18nmessageid import MessageFactory
-
-_ = MessageFactory("quintagroup.dropdownmenu")
-
-PROJECT_NAME = "quintagroup.dropdownmenu"
-logger = logging.getLogger(PROJECT_NAME)

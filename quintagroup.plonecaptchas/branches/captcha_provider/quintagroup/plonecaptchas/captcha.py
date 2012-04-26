4d9157b5ea3e924bd6688abcea3b3e429e690e73
Volodymyr Maksymiv <vmaksymiv@users.noreply.github.com>
2012-04-26 10:13:56 +0000

    switch captcha provider adapter to named utility

--- quintagroup.plonecaptchas/branches/captcha_provider/quintagroup/plonecaptchas/captcha.py
+++ quintagroup.plonecaptchas/branches/captcha_provider/quintagroup/plonecaptchas/captcha.py
@@ -5,13 +5,13 @@ from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from quintagroup.plonecaptchas.interfaces import IQGDiscussionCaptchas
 from quintagroup.plonecaptchas.interfaces import ICaptchaProvider
 from zope.interface import Interface
-from zope.component import adapts, getAdapters, queryAdapter
+from zope.component import adapts, getUtilitiesFor, queryUtility
 from plone.app.discussion.browser.comments import CommentForm
 
 
 class CaptchaProvider(object):
 
-    def __init__(self, context):
+    def __init__(self):
         self.widget_factory = CaptchaWidgetFactory
 
 
@@ -20,9 +20,8 @@ class CaptchaExtender(CaptchaExtender):
 
     def update(self):
         super(CaptchaExtender, self).update()
-        providers = getAdapters((self.context,), ICaptchaProvider)
-        if self.captcha in (n for n, a in providers) and self.isAnon:
-            captcha_provider = queryAdapter((self.context,), name=self.captcha)
+        if self.isAnon:
+            captcha_provider = queryUtility(ICaptchaProvider, name=self.captcha)
             if captcha_provider:
                 self.form.fields['captcha'].widgetFactory = \
                                                 captcha_provider.widget_factory
@@ -32,12 +31,12 @@ class CaptchaExtender(CaptchaExtender):
 def captcha_vocabulary(context):
     """ Extend captcha vocabulary with quintagroup.plonecaptchas"""
     terms = vocabularies.captcha_vocabulary(context)._terms
-    captchas = [t.value for t in terms]
+    captchas = set((t.value for t in terms))
 
-    adapters = getAdapters((context,), ICaptchaProvider)
-    for name, adapter in adapters:
+    providers = getUtilitiesFor(ICaptchaProvider)
+    for name, util in providers:
         if name and name not in captchas:
-            terms.append(SimpleTerm(value=name.lower(),
-                                    token=name.lower(),
-                                    title=name[0].upper() + name[1:]))
+            terms.append(SimpleTerm(value=name,
+                                    token=name,
+                                    title=name.capitalize()))
     return SimpleVocabulary(terms)

d72846e3f275c4446f7ce336ec67102fddc8d688
potar <potar@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2012-08-14 15:35:43 +0000

    added compability with Plone 3

--- quintagroup.plonegooglesitemaps/branches/sitemap_date/quintagroup/plonegooglesitemaps/indexers.py
+++ quintagroup.plonegooglesitemaps/branches/sitemap_date/quintagroup/plonegooglesitemaps/indexers.py
@@ -1,13 +1,33 @@
-from plone.indexer import indexer
 from plone.app.layout.navigation.defaultpage import getDefaultPage
 
 from zope.interface import Interface
 
 from quintagroup.plonegooglesitemaps.utils import dateTime
 
+#BBB: for compatibility with older plone versions
+# (Plone 3.0, Plone 3.1, Plone3.2)
+try:
+    from plone.indexer import indexer
+    IS_NEW = True
+except ImportError:
+    IS_NEW = False
+
+    class IDummyInterface:
+        pass
+
+    class indexer:
+
+        def __init__(self, *interfaces):
+            self.interfaces = IDummyInterface,
+
+        def __call__(self, callable):
+            callable.__component_adapts__ = self.interfaces
+            callable.__implemented__ = Interface
+            return callable
+
 
 @indexer(Interface)
-def sitemap_date(obj):
+def sitemap_date(obj, **kwargs):
     """ Method gets date for sitemap """
 
     def lastModificationDate(folderish_date, default_page):
@@ -19,7 +39,7 @@ def sitemap_date(obj):
         child_mdate = dateTime(default_page)
 
         if folderish_date > child_mdate:
-            last_date = folderish_date 
+            last_date = folderish_date
         else:
             last_date = child_mdate
 
@@ -37,3 +57,9 @@ def sitemap_date(obj):
         date = lastModificationDate(date, getattr(obj, default_page))
 
     return date.HTML4()
+
+#BBB: for compatibility with older plone versions
+# (Plone 3.0, Plone 3.1, Plone3.2)
+if not IS_NEW:
+    from Products.CMFPlone.CatalogTool import registerIndexableAttribute
+    registerIndexableAttribute('sitemap_date', sitemap_date)

f309d353de94061356f74c815c2676dc6e2dd221
potar <potar@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2012-08-15 10:23:48 +0000

    fixed pep8

--- quintagroup.plonegooglesitemaps/branches/sitemap_date/quintagroup/plonegooglesitemaps/content/newsextender.py
+++ quintagroup.plonegooglesitemaps/branches/sitemap_date/quintagroup/plonegooglesitemaps/content/newsextender.py
@@ -21,7 +21,7 @@ class ExtendableLinesField(ExtensionField, LinesField):
     """An extendable string field."""
 
 access_lst = ["Subscription", "Registration"]
-genres_lst = ["PressRelease", "Satire", "Blog", "OpEd", "Opinion", \
+genres_lst = ["PressRelease", "Satire", "Blog", "OpEd", "Opinion",
               "UserGenerated"]
 
 
@@ -30,45 +30,48 @@ class NewsExtender(object):
     implements(ISchemaExtender)
 
     fields = [
-        ExtendableStringField("gsm_access",
+        ExtendableStringField(
+            "gsm_access",
             accessor="gsm_access",
-            vocabulary=DisplayList(zip(["", ] + access_lst, \
+            vocabulary=DisplayList(zip(["", ] + access_lst,
                                    ["Open access", ] + access_lst)),
             default="",
             schemata="GoogleSitemap",
             widget=SelectionWidget(
                 label="Access",
-                description="Specifies whether an article is available to " \
-                    "all readers (in case of the emtpy field, or only to " \
+                description="Specifies whether an article is available to "
+                    "all readers (in case of the emtpy field, or only to "
                     "those with a free or paid membership to your site.",
                 format="radio"),
         ),
-        ExtendableLinesField("gsm_genres",
+        ExtendableLinesField(
+            "gsm_genres",
             accessor="gsm_genres",
             vocabulary=DisplayList(zip(genres_lst, genres_lst)),
             schemata="GoogleSitemap",
             default=(),
             widget=MultiSelectionWidget(
                 label="Genres",
-                description="Select one or more properties for an article, " \
-                    "namely, whether it is a press release, a blog post, an " \
-                    "opinion, an op-ed piece, user-generated content, " \
-                    "or satire.",
+                description="Select one or more properties for an article, "
+                            "namely, whether it is a press release, "
+                            "a blog post, an opinion, an op-ed piece, "
+                            "user-generated content, or satire.",
                 format="checkbox"),
         ),
-        ExtendableStringField("gsm_stock",
+        ExtendableStringField(
+            "gsm_stock",
             accessor="gsm_stock",
             default="",
             schemata="GoogleSitemap",
             widget=StringWidget(
                 label="Stock Tickers",
-                description="A comma-separated list of up to 5 stock tickers "\
-                    "of the companies, mutual funds, or other financial " \
-                    "entities that are the main subject of the article. " \
-                    "Relevant primarily for business articles. Each ticker " \
-                    "must be prefixed by the name of its stock exchange, " \
-                    "and must match its entry in Google Finance. " \
-                    "For example, \"NASDAQ:AMAT\" (but not \"NASD:AMAT\"), " \
+                description="A comma-separated list of up to 5 stock tickers "
+                    "of the companies, mutual funds, or other financial "
+                    "entities that are the main subject of the article. "
+                    "Relevant primarily for business articles. Each ticker "
+                    "must be prefixed by the name of its stock exchange, "
+                    "and must match its entry in Google Finance. "
+                    "For example, \"NASDAQ:AMAT\" (but not \"NASD:AMAT\"), "
                     "or \"BOM:500325\" (but not \"BOM:RIL\").",
                 size=70),
         ),

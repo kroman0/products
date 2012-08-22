790a562c42c7f1105d3e59991e6208fc392ab530
potar <potar@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2012-08-22 12:52:00 +0000

    reverting erroneous commit [4552] (svn merge -r 4552:4448 svn merge -r 4552:4448 http://svn.quintagroup.com/products/quintagroup.plonegooglesitemaps/tags)

--- quintagroup.plonegooglesitemaps/tags/1.8.0dev/quintagroup/plonegooglesitemaps/Extensions/ping_googlesitemap.py
+++
@@ -1,9 +0,0 @@
-from zope.event import notify
-from quintagroup.plonegooglesitemaps.events import AfterTransitionEvent
-
-
-def pingGoogleSitemap(stch):
-    notify(AfterTransitionEvent(stch.object,
-           stch.workflow, stch.old_state, stch.new_state,
-           stch.transition, stch.status, stch.kwargs))
-    return 0

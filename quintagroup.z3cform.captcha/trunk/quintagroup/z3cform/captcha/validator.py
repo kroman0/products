331755a093c5a7d939347f6a2a8ec32a3f67f255
Volodymyr Maksymiv <vmaksymiv@users.noreply.github.com>
2012-09-10 17:02:38 +0000

    PPP fixes

--- quintagroup.z3cform.captcha/trunk/quintagroup/z3cform/captcha/validator.py
+++ quintagroup.z3cform.captcha/trunk/quintagroup/z3cform/captcha/validator.py
@@ -1,6 +1,6 @@
 from DateTime import DateTime
 
-from zope.interface import Interface, Invalid
+from zope.interface import Interface
 from zope.component import adapts
 from zope.i18n import MessageFactory
 
@@ -14,6 +14,7 @@ from interfaces import ICaptcha
 
 _ = MessageFactory('quintagroup.z3cform.captcha')
 
+
 class CaptchaValidator(SimpleFieldValidator):
     """Captcha validator"""
 
@@ -23,7 +24,6 @@ class CaptchaValidator(SimpleFieldValidator):
         # Verify the user input against the captcha
         if 'kss_z3cform_inline_validation' in self.request['URL']:
             return
-        errors = ()
         context = self.context
         request = self.request
         value = value or ''
@@ -32,10 +32,10 @@ class CaptchaValidator(SimpleFieldValidator):
             hashkey = request.get('%shashkey' % self.widget.form.prefix, '')
             decrypted_key = decrypt(context.captcha_key, hashkey)
             parsed_key = parseKey(decrypted_key)
-            
+
             index = parsed_key['key']
             date = parsed_key['date']
-            
+
             if captcha_type == 'static':
                 img = getattr(context, '%s.jpg' % index)
                 solution = img.title
@@ -43,7 +43,7 @@ class CaptchaValidator(SimpleFieldValidator):
             else:
                 enc = value
                 solution = getWord(int(index))
-            
+
             captcha_tool = getToolByName(context, 'portal_captchas')
             if (enc != solution) or (captcha_tool.has_key(decrypted_key)) or (DateTime().timeTime() - float(date) > 3600):
                 raise ValueError(_(u'Please re-enter validation code.'))
